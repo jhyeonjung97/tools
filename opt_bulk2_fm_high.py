cf9ba185c79e41870adbd4f8a734aab84ab570c5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-13 17:37:31 -0700

    .

--- opt_bulk2_fm_high.py
+++ opt_bulk2_fm_high.py
@@ -11,7 +11,7 @@ from ase.calculators.vasp import Vasp
 from ase.io.trajectory import Trajectory
 import ase.calculators.vasp as vasp_calculator
 
-name = 'opt_bulk3_afm_high'
+name = 'opt_bulk2_fm_high'
 
 effective_length = 25
 
