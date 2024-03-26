5964fa6f26637c717713aad5534fdc99b9da6aa6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-25 19:13:34 -0700

    .

--- static_bulk_afm.py
+++ static_bulk_afm.py
@@ -93,7 +93,6 @@ def get_kpoints(atoms, effective_length=effective_length, bulk=False):
     return((nkx, nky, nkz))
 
 nbands = get_bands(atoms)
-print('nbands: ', nbands)
 kpoints = get_kpoints(atoms, effective_length=25, bulk=True)
 
 atoms.calc = vasp_calculator.Vasp(
