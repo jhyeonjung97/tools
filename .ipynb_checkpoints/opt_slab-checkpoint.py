91446af0caee4bec353e4579b6bec3bc56e321c8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-22 19:17:36 -0700

    .

--- .ipynb_checkpoints/opt_slab-checkpoint.py
+++ .ipynb_checkpoints/opt_slab-checkpoint.py
@@ -11,7 +11,7 @@ from ase.calculators.vasp import Vasp
 from ase.io.trajectory import Trajectory
 import ase.calculators.vasp as vasp_calculator
 
-name = 'opt_bulk3_fm'
+name = 'opt_slab'
 
 effective_length = 25
 
@@ -23,54 +23,27 @@ spin_states_plus_4 = {'Sc': 1, 'Ti': 2, 'V': 3, 'Cr': 4, 'Mn': 5, 'Fe': 4,
                       'Ir': 3, 'Pt': 2, 'Au': 1, 'Hg': 1, 'Tl': 1, 'Pb': 2,
                       }
 
-ldau_luj = {'Ti':{'L':2,  'U':3.00, 'J':0.0},
+ldau_luj = {'Ti': {'L':2,  'U':3.00, 'J':0.0},
             'V': {'L':2,  'U':3.25, 'J':0.0},
-            'Cr':{'L':2,  'U':3.5,  'J':0.0},
-            'Mn':{'L':2,  'U':3.75, 'J':0.0},
-            'Fe':{'L':2,  'U':4.3,  'J':0.0},
-            'Co':{'L':2,  'U':3.32, 'J':0.0},
-            'Ni':{'L':2,  'U':6.45, 'J':0.0},
-            'Cu':{'L':2, 'U':3.0,  'J':0.0},
+            'Cr': {'L':2,  'U':3.5,  'J':0.0},
+            'Mn': {'L':2,  'U':3.75, 'J':0.0},
+            'Fe': {'L':2,  'U':4.3,  'J':0.0},
+            'Co': {'L':2,  'U':3.32, 'J':0.0},
+            'Ni': {'L':2,  'U':6.45, 'J':0.0},
+            'Cu': {'L':2, 'U':3.0,  'J':0.0},
             }
 
-if path.exists('start.traj'):
-    atoms = read('start.traj')
-    i = 1
-    for a in atoms:
-        if a.symbol in spin_states_plus_4:
-            a.magmom = i*spin_states_plus_4.get(a.symbol)
-            # i *= -1 # set AFM
+if path.exists('restart.json'):
+    atoms = read('restart.json')
 else:
-    raise ValueError('Where is start.traj')
-    
+    raise ValueError('Where is restart.json')
+
 lmaxmix = 2
 for a in atoms:
     if a.symbol in ldau_luj:
         lmaxmix = 4
     else:
         ldau_luj[a.symbol] = {'L': -1, 'U': 0.0, 'J': 0.0}
-
-def get_bands(atoms):
-    """
-    returns the extact number of bands desired by lobster for the pCOHP calculations
-    """
-    nbands = 0
-    for sym in atoms.get_chemical_symbols():
-        if sym == 'H': # H is bugged
-            nbands += 1
-            continue
-        config = element(sym).ec.get_valence().to_str()
-        config = config.split()
-        for c in config:
-            if 's' in c:
-                nbands += 1
-            elif 'p' in c:
-                nbands += 3
-            elif 'd' in c:
-                nbands += 5
-            elif 'f' in c:
-                nbands += 7
-    return nbands
 
 def get_kpoints(atoms, effective_length=effective_length, bulk=False):
     """
@@ -92,8 +65,8 @@ def get_kpoints(atoms, effective_length=effective_length, bulk=False):
         nkz = 1
     return((nkx, nky, nkz))
 
-nbands = get_bands(atoms)
-kpoints = get_kpoints(atoms, effective_length=25, bulk=True)
+# nbands = get_bands(atoms)
+kpoints = get_kpoints(atoms, effective_length=25, bulk=False)
 
 atoms.calc = vasp_calculator.Vasp(
                     istart=0,
@@ -114,11 +87,11 @@ atoms.calc = vasp_calculator.Vasp(
                     sigma=0.05,
                     algo='normal',
                     ibrion=2,
-                    isif=3,
+                    isif=2,
                     ediffg=-0.02,
                     ediff=1e-6,
                     prec='Normal',
-                    nsw=600,
+                    nsw=200,
                     lvtot=False,
                     # nbands=nbands,
                     ispin=2,
@@ -135,15 +108,15 @@ atoms.calc = vasp_calculator.Vasp(
                     # isym=0, 
                     nedos=3000,
                     lorbit=11,
-                    # idipol=3,
+                    idipol=3,
                     # dipol=(0, 0, 0.5),
                     # ldipol=True
-                    # nupdown=0
+                    nupdown=0
                     )
 
 eng = atoms.get_potential_energy()
 print ('Calculation Complete, storing the run + calculator to traj file')
 
 Trajectory(f'final_{name}.traj','w').write(atoms)
-subprocess.call(f'ase convert -f final_{name}.traj restart.json', shell=True)
+# subprocess.call(f'ase convert -f final_{name}.traj restart.json', shell=True)
 subprocess.call(f'cp OUTCAR OUTCAR_{name}', shell=True)
