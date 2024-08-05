cb72444f62c6c1e0f25a9f133f621695ccda9d0b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-05 13:33:20 -0700

    .

---
+++ tetra/sub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+metals=('2_Ti' '3_V' '4_Cr' '5_Mn' '6_Fe' '7_Co' '8_Ni' '9_Cu')
+spins=('1_LS' '2_IS' '2_HS' '3_HS')
+dzs=('1_' '2_' '3_' '4_' '5_' '6_')
+
+for metal in ${metals[@]}; do
+    for spin in ${spins[@]}; do
+        for dz in ${dzs[@]}; do
+            path="/scratch/x2755a09/3_MNC/3d/${metal}/${spin}/${dz}"
+            if [ -d $path ]; then
+                cd $path
+                echo $PWD
+                # qsub submit.sh
+            fi
+        done
+    done
+done
