3c5c88bfc29841a040e318c9677e0698514ba22d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-06 02:07:34 -0700

    .

--- tetra/conti.sh
+++ tetra/conti.sh
@@ -1,6 +1,9 @@
 for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*d/*_*/; do
     cd $dir
     if [[ -n $(grep CONTCAR vasp.out) ]]; then
-        grep CONTCAR vasp.out
+        if [[ -d conti_2 ]]; then
+            ls
+        fi
+        # grep CONTCAR vasp.out
     fi
 done
