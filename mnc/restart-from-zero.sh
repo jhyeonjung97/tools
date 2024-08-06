a87f78b01dce85a8e86cc67f596da1a8661a9739
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-06 15:14:27 -0700

    .

--- mnc/restart-from-zero.sh
+++ mnc/restart-from-zero.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# Step 1: Run spread.sh on directories matching the first pattern
-for dir in /scratch/x2755a09/3_MNC/*d/*_*/*_*S/; do
-    cd $dir
-    if [[ -s 'DONE' ]] && [[ -s 'restart.json' ]]; then
-        sh ~/bin/verve/spread.sh 0_/restart.json
-        sh ~/bin/verve/spread.sh 0_/WAVECAR
-    else
-        echo "Calculation in $dir is not completed"
-    fi
-done
+# metals=('2_Ti' '3_V' '4_Cr' '5_Mn' '6_Fe' '7_Co' '8_Ni' '9_Cu')
+# spins=('1_LS' '2_IS' '2_HS' '3_HS')
+# dzs=('1_' '2_' '3_' '4_' '5_' '6_')
 
-# # Step 2: Submit jobs for specific subdirectories
-# for subdir in /scratch/x2755a09/3_MNC/*d/*_*/*_*S/*_/; do
-#     cd $subdir
-#     IFS='/' read -r -a path_components <<< $PWD
-#     dz=$(echo ${path_components[-1]} | cut -d'_' -f1)
-#     if [[ $dz != 0 ]]; then
-#         qsub submit.sh
-#     fi
+# for metal in ${metals[@]}; do
+#     for spin in ${spins[@]}; do
+#         for dz in ${dzs[@]}; do
+#             path="/scratch/x2755a09/3_MNC/3d/$metal/$spin/$dz"
+#             if [ -d $path ]; then
+#                 cd $path
+#                 echo $PWD
+#                 # if [ -
+#                 # qsub submit.sh
+#             fi
+#         done
+#     done
 # done
+
+# mkdir 1_ 2_ 3_ 4_ 5_ 6_
+# sh ~/bin/verve/spread.sh 0_/restart.json
+# sh ~/bin/verve/spread.sh 0_/WAVECAR
+
+dzs=(1 2 3 4 5 6)
+dir_now=$PWD
+for dz in ${dzs[@]}; do
+    cd "$dz"_/
+    cp /scratch/x2755a09/3_MNC/3d/submit.sh .
+    sed -i -e "/#PBS -N/c\#PBS -N $1$dz" submit.sh
+    sed -i -e "/#PBS -N/c\#PBS -N $1$dz" submit.sh
+    python ~/bin/tools/mnc/dz.py $dz
+    qsub submit.sh
+    cd $dir_now
+done
