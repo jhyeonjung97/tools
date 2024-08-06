c0c83b1e65d63c9f58abb2c056581c339bd7197f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-06 02:33:43 -0700

    .

--- tetra/conti.sh
+++ tetra/conti.sh
@@ -1,4 +1,6 @@
-for dir in /scratch/x2755a09/5_V_bulk/*_*_*/fm/*_*/; do
+#!/bin/bash/
+
+for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
     cd $dir
     if [[ -n $(grep CONTCAR vasp.out) ]]; then
         echo $PWD
@@ -8,6 +10,15 @@ for dir in /scratch/x2755a09/5_V_bulk/*_*_*/fm/*_*/; do
             sed -i -e 's/fm.py/fm_ediff.py' submit.sh
         fi
         sh ~/bin/verve/conti.sh
-        # grep CONTCAR vasp.out
+    elif [[ -s DONE ]]; then
+        mkdir opt
+        mv * opt
+        if [[ -d conti_1 ]]; then
+            cp conti_1/start.traj .
+        fi
+        cp opt/restart.json .
+        cp opt/submit.sh .
+        cp opt/WAVECAR .
+        
     fi
 done
