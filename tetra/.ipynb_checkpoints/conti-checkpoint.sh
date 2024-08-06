21ce809acfc7767551552996be25d33ba1e54aef
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-06 02:05:39 -0700

    .

--- tetra/.ipynb_checkpoints/conti-checkpoint.sh
+++ tetra/.ipynb_checkpoints/conti-checkpoint.sh
@@ -1,4 +1,4 @@
-for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*d/*_*/; do
+for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*d/*_/; do
     cd $dir
     if [[ -n $(grep CONTCAR vasp.out) ]]; then
         grep CONTCAR vasp.out
