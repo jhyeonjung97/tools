532f02bda60f7e983eae85efd1e56398507577a0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-05 21:09:35 -0700

    .

--- heo/.ipynb_checkpoints/heo_relative_energy-checkpoint.py
+++ heo/.ipynb_checkpoints/heo_relative_energy-checkpoint.py
@@ -89,6 +89,7 @@ def main():
             charges = atoms.get_initial_charges()
             for metal in prvs:
                 df_chg.at[i, metal] = mean([abs(charges[atom.index]) for atom in atoms if atom.symbol == metal])
+            df.at[i, 'charge'] = sum(numb[m] * df_chg.at[i, metal] for m, metal in enumerate(prvs)) / 8
         if os.path.exists(gap_path):
             with open(gap_path, 'r') as file:
                 lines = file.read()
@@ -112,6 +113,7 @@ def main():
                     df_occ.at[i, metal] = mean(tmp)*4
                 else:
                     df_occ.at[i, metal] = np.nan  # Handle case where tmp is empty
+            df.at[i, 'eg_occ'] = sum(numb[m] * df_occ.at[i, metal] for m, metal in enumerate(prvs)) / 8
                             
     saving(df, filename)
     saving(df_chg, chg_filename)
@@ -122,22 +124,29 @@ def main():
     for i in range(5):
         df_ref.at[i, 'energy'] = 0
         
-    plotting(pattern='energy', xlabel='Relative energy (eV)', filename=filename, figsize=(6, 6), min=-0.4, max=+0.4, tick=0.04) 
-    plotting(pattern='bandgap', xlabel='Band gap (eV)', filename=gap_filename, figsize=(10, 6), min=+0.0, max=+2.8, tick=0.04)
-    plotting(pattern='Md2Op', xlabel='M3d - O2p (eV)', filename=dos_filename, figsize=(8, 6), min=+0.0, max=+2.0, tick=0.04)
+    plotting(pattern='energy', xlabel='Relative energy (eV)', filename=filename, figsize=(6, 6), 
+             bins=np.arange(-0.4, +0.4, 0.05), width=0.05*0.9, xticks=np.arange(-0.4, +0.4+0.05, 0.05*2), xmin=-0.4-0.05, xmax=+0.4+0.05) 
+    plotting(pattern='bandgap', xlabel='Band gap (eV)', filename=gap_filename, figsize=(10, 6), 
+             bins=np.arange(+0.0, +3.0, 0.05), width=0.05*0.9, xticks=np.arange(+0.0, +3.0+0.10, 0.10*2), xmin=+0.0-0.10, xmax=+3.0+0.10) 
+    plotting(pattern='Md2Op', xlabel='M3d - O2p (eV)', filename=dos_filename, figsize=(8, 6), 
+             bins=np.arange(+0.0, +2.0, 0.05), width=0.05*0.9, xticks=np.arange(+0.0, +2.0+0.10, 0.10*2), xmin=+0.0-0.10, xmax=+2.0+0.10) 
     
-    plotting_adv(df=df_mag, df_ref=df_ref, pattern='magmom', xlabel='Magnetic moments (uB)', filename=mag_filename, min=0.0, max=5.0, tick=0.1)
-    plotting_adv(df=df_chg, df_ref=df_ref, pattern='charge', xlabel='Bader charge (e-)', filename=chg_filename, min=1.0, max=2.1, tick=0.02)
-    plotting_adv(df=df_occ, df_ref=df_ref, pattern='eg_occ', xlabel='e_g occupancy (e-)', filename=occ_filename, min=2.2, max=4.0, tick=0.04)
+    plotting_adv(df=df_mag, df_ref=df_ref, pattern='magmom', xlabel='Magnetic moments (uB)', filename=mag_filename,
+                 bins1=np.arange(0.0, 5.0, 0.1*1), width1=0.1*0.9, xticks1=np.arange(0.0, 5.0+0.1*10, 0.1*10), xmin1=0.0-0.1*5, xmax1=5.0+0.1*5, 
+                 bins2=np.arange(0.0, 5.0, 0.1*2), width2=0.1*2.0, xticks2=np.arange(0.0, 5.0+0.1*10, 0.1*10), xmin2=0.0-0.1*5, xmax2=5.0+0.1*5)
+    plotting_adv(df=df_chg, df_ref=df_ref, pattern='charge', xlabel='Bader charge (e-)', filename=chg_filename, 
+                 bins1=np.arange(1.0, 2.0, 0.02*1), width1=0.02*0.9, xticks1=np.arange(1.0, 2.0+0.02*10, 0.02*10), xmin1=1.0-0.02*5, xmax1=2.0+0.02*5, 
+                 bins2=np.arange(1.0, 2.0, 0.02*2), width2=0.02*2.0, xticks2=np.arange(1.0, 2.0+0.02*10, 0.02*10), xmin2=1.0-0.02*5, xmax2=2.0+0.02*5)
+    plotting_adv(df=df_occ, df_ref=df_ref, pattern='eg_occ', xlabel='e_g occupancy (e-)', filename=occ_filename, 
+                 bins1=np.arange(2.0, 4.0, 0.04*1), width1=0.04*0.9, xticks1=np.arange(2.0, 4.0+0.04*10, 0.04*10), xmin1=2.0-0.04*5, xmax1=4.0+0.04*5, 
+                 bins2=np.arange(2.0, 4.0, 0.04*2), width2=0.04*2.0, xticks2=np.arange(2.0, 4.0+0.04*10, 0.04*10), xmin2=2.0-0.04*5, xmax2=4.0+0.04*5)
     
 def saving(df, filename):
     df.to_csv(f'{filename}.tsv', sep='\t', float_format='%.2f')
     print(f"Data saved to {filename}.tsv")
 
-def plotting(pattern, xlabel, filename, figsize, min, max, tick):
-    
-    bins=np.arange(min, max, tick*1); width=tick*0.9; xticks=np.arange(min, max, tick*2); xmin=min-tick; xmax=max+tick
-    
+def plotting(pattern, xlabel, filename, figsize, bins, width, xticks, xmin, xmax):
+        
     plt.figure(figsize=figsize)
     plt.hist(df[pattern].dropna(), bins=bins, alpha=0.5, width=width)
     for i in range(5):
@@ -151,15 +160,13 @@ def plotting(pattern, xlabel, filename, figsize, min, max, tick):
     print(f"Figure saved as {filename}.png")
     plt.close()
 
-def plotting_adv(df, df_ref, pattern, xlabel, filename, min, max, tick,
+def plotting_adv(df, df_ref, pattern, xlabel, filename,
+                 bins1, width1, xticks1, xmin1, xmax1, 
+                 bins2, width2, xticks2, xmin2, xmax2,
                  figsize1=(8, 6), figsize2=(12, 6)):
-
-    bins1=np.arange(min, max, tick*1); width1=tick*0.9; xticks1=np.arange(min, max, tick*15); xmin1=min-tick*5; xmax1=max+tick*5
-    bins2=np.arange(min, max, tick*2); width2=tick*2.0; xticks2=np.arange(min, max, tick*15); xmin2=min-tick*5; xmax2=max+tick*5
-
-    for i, column in enumerate(df_chg.columns):
+    for i, column in enumerate(df.columns):
         plt.figure(figsize=figsize1)
-        plt.hist(df_mag[column].dropna(), bins=bins1, alpha=0.5, color=clrs[i], label=str(column), width=width1)
+        plt.hist(df[column].dropna(), bins=bins1, alpha=0.5, color=clrs[i], label=str(column), width=width1)
         plt.axvline(x=df_ref.at[i, pattern], color=clrs[i], linestyle='--')
         plt.xlabel(xlabel)
         plt.ylabel('Frequency')
@@ -169,11 +176,12 @@ def plotting_adv(df, df_ref, pattern, xlabel, filename, min, max, tick,
         plt.savefig(f'{filename}_{column}.png', bbox_inches="tight")
         print(f"Figure saved as {filename}_{column}.png")
         plt.close()
-
+    if pattern == 'charge' or pattern == 'eg_occ':
+        plotting(pattern=pattern, xlabel=xlabel, filename=filename+'_avg', figsize=(8, 6), 
+                 bins=bins1, width=width1, xticks=xticks1, xmin=xmin1, xmax=xmax1) 
     plt.figure(figsize=figsize2)
     for i in range(5):
         plt.axvline(x=df_ref.at[i, pattern], color=clrs[i], linestyle='--')
-    bins = bins2
     bin_width = width2 / (len(df.columns) + 1)  # Calculate new width for each bar
     for idx, column in enumerate(df.columns):
         plt.hist(df[column].dropna(), bins=bins2 + idx * bin_width, alpha=0.5, label=str(column), width=bin_width)
@@ -186,5 +194,6 @@ def plotting_adv(df, df_ref, pattern, xlabel, filename, min, max, tick,
     print(f"Figure saved as {filename}.png")
     plt.close()
 
+    
 if __name__ == '__main__':
     main()
