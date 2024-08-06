86af46a5ea45303de4811fb27cc3af412c6fdb3d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-06 02:20:10 -0700

    .

--- tetra/.ipynb_checkpoints/conti-checkpoint.sh
+++ tetra/.ipynb_checkpoints/conti-checkpoint.sh
@@ -2,8 +2,8 @@ for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*d/*_*/; do
     cd $dir
     if [[ -d conti_3 ]] && [[ ! -s vasp.out ]]; then
         echo $PWD
-        # rm -r conti_3 conti_4 conti_5 conti_6
-        # cp conti_2/restart.json .
+        rm -r conti_3 conti_4 conti_5 conti_6
+        cp conti_2/restart.json .
         # sed -i -e 's/fm.py/fm_ediff.py/' submit.sh
     fi
     # sh ~/bin/verve/conti.sh
