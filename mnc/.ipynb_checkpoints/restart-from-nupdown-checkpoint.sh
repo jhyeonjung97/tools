c83a480949d785d17a8cbfc9955bca3e6553badd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-07 16:53:07 -0700

    .

--- mnc/.ipynb_checkpoints/restart-from-nupdown-checkpoint.sh
+++ mnc/.ipynb_checkpoints/restart-from-nupdown-checkpoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 IFS='/' read -r -a path_components <<< $PWD
-metal=$(echo "${path_components[-2]}" | cut -d'_' -f2)
-spin=$(echo "${path_components[-1]}" | cut -d'_' -f2)
+metal=$(echo "${path_components[-3]}" | cut -d'_' -f2)
+spin=$(echo "${path_components[-2]}" | cut -d'_' -f2)
 
 cp $1/restart.json .
+python ~/bin/tools/mnc/dz.py 0
+
 cp $1/WAVECAR .
 ls -l WAVECAR
+
 cp /pscratch/sd/j/jiuy97/6_MNC/scripts/submit.sh .
-sed -i -e "/#SBATCH -J/c\#SBATCH -J $metal$spin" submit.sh
+sed -i -e "/#SBATCH -J/c\#SBATCH -J ${metal}${spin}r" submit.sh
