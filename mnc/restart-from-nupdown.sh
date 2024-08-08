6f00a6783afad8cce5eaab341de0beeb6f1289ae
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-08 12:44:54 -0700

    .

--- mnc/restart-from-nupdown.sh
+++ mnc/restart-from-nupdown.sh
@@ -17,7 +17,9 @@ ls -l WAVECAR
 if [[ ${here} == 'nersc' ]]; then
     cp /pscratch/sd/j/jiuy97/6_MNC/scripts/submit.sh .
     sed -i -e "/#SBATCH -J/c\#SBATCH -J ${metal}${spin}r" submit.sh
+    sbatch submit.sh
 elif [[ ${here} == 'kisti' ]]; then
     cp /scratch/x2755a09/3_MNC/3d/submit.sh .
     sed -i -e "/#PBS -N/c\#PBS -N ${metal}${spin}${dz}n" submit.sh
+    qsub submit.sh
 fi
