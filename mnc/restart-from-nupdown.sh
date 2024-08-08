d1a479b5b073227d54a5937c34fa708aa6046840
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-08 12:39:55 -0700

    .

--- mnc/restart-from-nupdown.sh
+++ mnc/restart-from-nupdown.sh
@@ -3,12 +3,17 @@
 IFS='/' read -r -a path_components <<< $PWD
 metal=$(echo "${path_components[-3]}" | cut -d'_' -f2)
 spin=$(echo "${path_components[-2]}" | cut -d'_' -f2)
+dz=$(echo "${path_components[-1]}" | cut -d'_' -f1)
 
 cp $1/restart.json .
-python ~/bin/tools/mnc/dz.py 0
+# python ~/bin/tools/mnc/dz.py 0
 
 cp $1/WAVECAR .
 ls -l WAVECAR
 
-cp /pscratch/sd/j/jiuy97/6_MNC/scripts/submit.sh .
-sed -i -e "/#SBATCH -J/c\#SBATCH -J ${metal}${spin}r" submit.sh
+if [[ ${here} == 'nersc' ]]; then
+    cp /pscratch/sd/j/jiuy97/6_MNC/scripts/submit.sh .
+    sed -i -e "/#SBATCH -J/c\#SBATCH -J ${metal}${spin}r" submit.sh
+elif [[ ${here} == 'kisti' ]]; then
+    cp /scratch/x2755a09/3_MNC/3d/submit.sh .
+    sed -i -e "/#PBS -N/c\#PBS -N ${metal}${spin}${dz}n" submit.sh
