f86b0ce79c1f12315efde1a9a3bfb162fc20bab7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-08 15:06:20 -0700

    .

--- mnc/restart-from-nupdown.sh
+++ mnc/restart-from-nupdown.sh
@@ -6,7 +6,7 @@ spin=$(echo "${path[-2]}" | cut -d'_' -f2)
 dz=$(echo "${path[-1]}" | cut -d'_' -f1)
 
 new_path="/scratch/x2755a09/3_MNC/pre/${path[-4]}/${path[-3]}/${path[-2]}/${path[-1]}"
-echo $new_path
+ls $new_path
 
 # mkdir nupdown
 # mv * nupdown
