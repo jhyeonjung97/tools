887c02ee4269fda22e3b12a45f93f31d129ab955
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-11 17:38:12 -0700

    .

--- mnc/restart-with-ediff.sh
+++ mnc/restart-with-ediff.sh
@@ -13,11 +13,8 @@ python ~/bin/tools/mnc/dz.py $dz
 cp $1/WAVECAR $1/CHGCAR .
 ls -l WAVECAR CHGCAR
 
+cp /scratch/x2755a09/3_MNC/3d/submit.sh .
 sed -i -e 's/mnc-sol.py/mnc-sol-ediff.py/' submit.sh
-sed -i -e 's/mnc-sol-ns.py/mnc-sol-ediff.py/' submit.sh
-sed -i -e 's/mnc-sol-ls.py/mnc-sol-ediff.py/' submit.sh
-sed -i -e 's/mnc-sol-is.py/mnc-sol-ediff.py/' submit.sh
-sed -i -e 's/mnc-sol-hs.py/mnc-sol-ediff.py/' submit.sh
 
 if [[ ${here} == 'nersc' ]]; then
     cp /pscratch/sd/j/jiuy97/6_MNC/scripts/submit.sh .
