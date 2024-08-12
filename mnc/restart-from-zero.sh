7343b8a3bfeeab796f9ef918b7fa5cc8323afe84
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-11 20:19:30 -0700

    .

--- mnc/restart-from-zero.sh
+++ mnc/restart-from-zero.sh
@@ -4,23 +4,25 @@
 # spins=('1_LS' '2_IS' '2_HS' '3_HS')
 # dzs=('1_' '2_' '3_' '4_' '5_' '6_')
 
-mkdir 0_
-mv * 0_
+mkdir "$1"_
+mv * "$1"_
 
 IFS='/' read -r -a path_components <<< $PWD
 metal=$(echo "${path_components[-2]}" | cut -d'_' -f2)
 spin=$(echo "${path_components[-1]}" | cut -d'_' -f2)
 
-dzs=(1 2 3 4 5 6)
+dzs=(0 1 2 3 4 5 6)
 dir_now=$PWD
 for dz in ${dzs[@]}; do
-    mkdir "$dz"_/
-    cp 0_/restart.json "$dz"_/
-    cp 0_/WAVECAR "$dz"_/
-    cd "$dz"_/
-    cp /scratch/x2755a09/3_MNC/3d/submit.sh .
-    sed -i -e "/#PBS -N/c\#PBS -N $metal$spin$dz" submit.sh
-    python ~/bin/tools/mnc/dz.py $dz
-    qsub submit.sh
-    cd $dir_now
+    if [[ $dz != $1 ]]; then
+        mkdir "$dz"_/
+        cp "$1"_/restart.json "$dz"_/
+        cp "$1"_/WAVECAR "$dz"_/
+        cd "$dz"_/
+        cp /scratch/x2755a09/3_MNC/3d/submit.sh .
+        sed -i -e "/#PBS -N/c\#PBS -N $metal$spin$dz" submit.sh
+        python ~/bin/tools/mnc/dz.py $dz
+        qsub submit.sh
+        cd $dir_now
+    fi
 done
