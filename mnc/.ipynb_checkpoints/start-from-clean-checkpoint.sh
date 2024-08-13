71d39e2aac865b9db87aa3cd9fe231108cfb6c6f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 16:22:48 -0700

    .

--- mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
+++ mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
@@ -3,7 +3,6 @@
 for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S
 do
     cd $dir
-    
     IFS='/' read -r -a path <<< $PWD
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     spin=$(echo "${path[-1]}" | cut -d'_' -f2)
@@ -14,10 +13,8 @@ do
     # elif [[ -s $clean_path/CONTCAR ]]; then
     #     cp $clean_path/CONTCAR POSCAR
     # fi
-
     # sed -i -e "1c\C N $metal" POSCAR
     # sed -i -e "6c\C N $metal" POSCAR
-
     # python ~/bin/tools/mnc/add-o.py
 
     sed -i -e "s/jobname/$metal$spin/" submit.sh
