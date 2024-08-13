7ce1cf592899d23552f6eff19dbed3b19f939d85
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 12:33:17 -0700

    .

--- mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
+++ mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
@@ -5,11 +5,15 @@ do
     cd $dir
     
     IFS='/' read -r -a path <<< $PWD
-    # metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+    echo $metal
     # spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     
-    clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
-    cp $clean_path/re_/CONTCAR POSCAR
-    cp $clean_path/CONTCAR POSCAR
+    # clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
+    # cp $clean_path/re_/CONTCAR POSCAR
+    # cp $clean_path/CONTCAR POSCAR
     # ase converf -f $clean_path/CONTCAR start.traj
+
+    # sed -i -e "1c\C N $metal" POSCAR
+    # sed -i -e "6c\C N $metal" POSCAR
 done
