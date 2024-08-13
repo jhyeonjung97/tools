19946d149a6e6f73769f73d8264e1f3561340b4b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 12:25:00 -0700

    .

--- mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
+++ mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
@@ -3,13 +3,13 @@
 for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S
 do
     cd $dir
-    pwd
     
     IFS='/' read -r -a path <<< $PWD
     # metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     # spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     
     clean_path="/scratch/x2755a09/3_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
+    echo $clean_path
     # cp $clean_path/CONTCAR POSCAR
     # ase converf -f $clean_path/CONTCAR start.traj
 done
