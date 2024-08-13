2b2d35ca6119c1a4e8e4e18892788a807a1d1e83
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 16:25:41 -0700

    .

--- mnc/start-from-clean.sh
+++ mnc/start-from-clean.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S
+for dir in /pscratch/sd/j/jiuy97/6_MNC/2_OH/*d/*_*/*_*S
 do
     cd $dir
     IFS='/' read -r -a path <<< $PWD
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     
-    # clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
-    # if [[ -s $clean_path/re_/CONTCAR ]]; then
-    #     cp $clean_path/re_/CONTCAR POSCAR
-    # elif [[ -s $clean_path/CONTCAR ]]; then
-    #     cp $clean_path/CONTCAR POSCAR
-    # fi
-    # sed -i -e "1c\C N $metal" POSCAR
-    # sed -i -e "6c\C N $metal" POSCAR
-    # python ~/bin/tools/mnc/add-o.py
+    clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
+    if [[ -s $clean_path/re_/CONTCAR ]]; then
+        cp $clean_path/re_/CONTCAR POSCAR
+    elif [[ -s $clean_path/CONTCAR ]]; then
+        cp $clean_path/CONTCAR POSCAR
+    fi
+    sed -i -e "1c\C N $metal" POSCAR
+    sed -i -e "6c\C N $metal" POSCAR
+    python ~/bin/tools/mnc/add-oh.py
 
     cp ~/bin/tools/mnc/submit.sh .
-    sed -i -e "s/jobname/${metal}${spin}_O/" submit.sh
+    sed -i -e "s/jobname/${metal}${spin}_OH/" submit.sh
     if [[ $spin == 'LS' ]]; then
         sed -i -e "s/mnc-sol.py/mnc-sol-ls-nupdown.py/" submit.sh
     elif [[ $spin == 'IS' ]]; then
