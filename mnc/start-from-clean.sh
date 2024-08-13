497d5fb928fc2e19b25ff12b098774d99ae82825
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 12:44:09 -0700

    .

--- mnc/start-from-clean.sh
+++ mnc/start-from-clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S
+for dir in /pscratch/sd/j/jiuy97/6_MNC/2_OH/*d/*_*/*_*S
 do
     cd $dir
     
@@ -9,13 +9,13 @@ do
     # echo $metal
     # spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     
-    # clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
-    # cp $clean_path/re_/CONTCAR POSCAR
-    # cp $clean_path/CONTCAR POSCAR
+    clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
+    cp $clean_path/re_/CONTCAR POSCAR
+    cp $clean_path/CONTCAR POSCAR
     # ase converf -f $clean_path/CONTCAR start.traj
 
-    # sed -i -e "1c\C N $metal" POSCAR
-    # sed -i -e "6c\C N $metal" POSCAR
+    sed -i -e "1c\C N $metal" POSCAR
+    sed -i -e "6c\C N $metal" POSCAR
 
-    python ~/bin/tools/mnc/add-o.py
+    python ~/bin/tools/mnc/add-oh.py
 done
