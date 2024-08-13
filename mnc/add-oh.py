02526b81586cf38f507acd16753914e57f3e287a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 12:41:12 -0700

    .

--- mnc/add-oh.py
+++ mnc/add-oh.py
@@ -2,6 +2,7 @@ from ase.io import read, write
 from ase import Atoms
 
 atoms = read('POSCAR')
-atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, 2.0)])
-atoms += Atoms('H', positions=[atoms[-1].position + (0.8, 0.0, 0.6)])
-write('start.traj', atoms)
+if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, 2.0)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.8, 0.0, 0.6)])
+    write('start.traj', atoms)
