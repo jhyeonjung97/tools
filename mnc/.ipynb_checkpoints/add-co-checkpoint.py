54372d49bf792a3e6f2202ca55188952bc2826b8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 16:43:47 -0700

    .

--- mnc/.ipynb_checkpoints/add-co-checkpoint.py
+++ mnc/.ipynb_checkpoints/add-co-checkpoint.py
@@ -3,6 +3,6 @@ from ase import Atoms
 
 atoms = read('POSCAR')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
-    atoms += Atoms('C', positions=[atoms[-1].position + (0.0, 0.0, 2.0)])
-    atoms += Atoms('O', positions=[atoms[-1].position + (0.8, 0.0, 0.6)])
+    atoms += Atoms('C', positions=[atoms[-1].position + (-0.6, 0.0, 2.0)])
+    atoms += Atoms('O', positions=[atoms[-2].position + (+0.6, 0.0, 2.0)])
     write('start.traj', atoms)
--- mnc/add-h.py
+++ mnc/add-h.py
@@ -3,6 +3,5 @@ from ase import Atoms
 
 atoms = read('POSCAR')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
-    atoms += Atoms('C', positions=[atoms[-1].position + (-0.6, 0.0, 2.0)])
-    atoms += Atoms('O', positions=[atoms[-2].position + (+0.6, 0.0, 2.0)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, 2.0)])
     write('start.traj', atoms)
