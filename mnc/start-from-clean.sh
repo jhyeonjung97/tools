01d02895fd6109df8ec129b2364e35d6f37b084e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 15:55:55 -0700

    .

--- mnc/start-from-clean.sh
+++ mnc/start-from-clean.sh
@@ -8,8 +8,11 @@ do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
     clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
-    cp $clean_path/re_/CONTCAR POSCAR
-    cp $clean_path/CONTCAR POSCAR
+    if [[ -s $clean_path/re_/CONTCAR ]]; then
+        cp $clean_path/re_/CONTCAR POSCAR
+    elif [[ -s $clean_path/CONTCAR ]]; then
+        cp $clean_path/CONTCAR POSCAR
+    fi
 
     sed -i -e "1c\C N $metal" POSCAR
     sed -i -e "6c\C N $metal" POSCAR
