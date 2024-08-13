6db5732163a6e51960a8fe9acae68db974354100
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-13 16:21:11 -0700

    .

--- mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
+++ mnc/.ipynb_checkpoints/start-from-clean-checkpoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/2_OH/*d/*_*/*_*S
+for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S
 do
     cd $dir
     
@@ -8,17 +8,17 @@ do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     
-    clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
-    if [[ -s $clean_path/re_/CONTCAR ]]; then
-        cp $clean_path/re_/CONTCAR POSCAR
-    elif [[ -s $clean_path/CONTCAR ]]; then
-        cp $clean_path/CONTCAR POSCAR
-    fi
+    # clean_path="/pscratch/sd/j/jiuy97/6_MNC/0_clean/${path[-3]}/${path[-2]}/1_LS"
+    # if [[ -s $clean_path/re_/CONTCAR ]]; then
+    #     cp $clean_path/re_/CONTCAR POSCAR
+    # elif [[ -s $clean_path/CONTCAR ]]; then
+    #     cp $clean_path/CONTCAR POSCAR
+    # fi
 
-    sed -i -e "1c\C N $metal" POSCAR
-    sed -i -e "6c\C N $metal" POSCAR
+    # sed -i -e "1c\C N $metal" POSCAR
+    # sed -i -e "6c\C N $metal" POSCAR
 
-    python ~/bin/tools/mnc/add-oh.py
+    # python ~/bin/tools/mnc/add-o.py
 
     sed -i -e "s/jobname/$metal$spin/" submit.sh
     if [[ $spin == 'LS' ]]; then
