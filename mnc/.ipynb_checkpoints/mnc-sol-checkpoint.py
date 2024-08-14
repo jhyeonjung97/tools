02d362f3caf04b3eb1d62450242d3e9c9cebeb10
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-14 13:17:10 -0700

    .

--- mnc/.ipynb_checkpoints/mnc-sol-checkpoint.py
+++ mnc/.ipynb_checkpoints/mnc-sol-checkpoint.py
@@ -31,10 +31,10 @@ else:
         
 lmaxmix = 2
 for atom in atoms:
-    if atom.symbol not in ['C', 'N', 'O', 'H']:
-        ldau_luj[atom.symbol] = {'L': -1, 'U': 0.0, 'J': 0.0}
-    elif atom.symbol in ldau_luj:
+    if atom.symbol in ldau_luj:
         lmaxmix = 4
+    elif atom.symbol not in ['C', 'N', 'O', 'H']:
+        ldau_luj[atom.symbol] = {'L': -1, 'U': 0.0, 'J': 0.0}
     else:
         ldau_luj[atom.symbol] = {'L': 2, 'U': 0.0, 'J': 0.0}   
 
