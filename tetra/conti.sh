d5843af80cc509c88c0fa6a1a9c2edc684df4abf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-15 12:49:31 -0700

    .

--- tetra/conti.sh
+++ tetra/conti.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-for dir in /scratch/x2755a09/5_V_bulk/7_Pyramidal_LT/*/*_*/; do
+for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
     cd $dir
     IFS='/' read -r -a path <<< $PWD
     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
@@ -14,13 +14,17 @@ for dir in /scratch/x2755a09/5_V_bulk/7_Pyramidal_LT/*/*_*/; do
             if [[ -d conti_1 ]]; then
                 rm -r conti*/
             fi
-            cp ~/bin/tools/tetra/lobsterin .
-            sed -i -e "s/X/$metal/g" lobsterin
-            cp ~/bin/tools/tetra/lobster.sh .
-            sed -i -e "s/jobname/${coord}${row}${numb}lob/g" lobster.sh
-            pwd; #qsub lobster.sh
+            if [[ -s icohp.txt ]]; then
+                :
+            else
+                cp ~/bin/tools/tetra/lobsterin .
+                sed -i -e "s/X/$metal/g" lobsterin
+                cp ~/bin/tools/tetra/lobster.sh .
+                sed -i -e "s/jobname/${coord}${row}${numb}lob/g" lobster.sh
+                pwd; qsub lobster.sh
+            fi
         else
-            echo -e "\e[31m$PWD\e[0m"
+            echo -e "\e[32m$PWD\e[0m err"
         fi
     elif [[ ! -d opt ]] && [[ -s DONE ]] && [[ -s vasp.out ]]; then
         if [[ -d conti_1 ]]; then
@@ -34,16 +38,16 @@ for dir in /scratch/x2755a09/5_V_bulk/7_Pyramidal_LT/*/*_*/; do
             sed -i -e 's/m.py/m_fast.py/' submit.sh
             pwd; qsub submit.sh
         elif [[ -n $(grep WARNING vasp.out) ]]; then
-            echo -e "\e[31m$PWD\e[0m"
+            echo -e "\e[35m$PWD\e[0m err"
         else
             mkdir opt; find . -maxdepth 1 -mindepth 1 ! -name opt -exec mv {} opt/ \;
             cp opt/restart.json .
             cp opt/WAVECAR .
             cp ~/bin/tools/tetra/static.sh .
             sed -i -e "s/jobname/${coord}${row}${numb}stc/g" static.sh
-            pwd; #qsub static.sh
+            pwd; qsub static.sh
         fi
     else
-        echo -e "\e[31m$PWD\e[0m"
+        echo -e "\e[36m$PWD\e[0m err"
     fi
 done
