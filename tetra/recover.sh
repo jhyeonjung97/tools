ce4ee02e4409494df72393ef839a46e814619a99
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-16 15:42:29 -0700

    .

--- tetra/recover.sh
+++ tetra/recover.sh
@@ -1,3 +1,21 @@
+# for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
+#     cd $dir
+#     IFS='/' read -r -a path <<< $PWD
+#     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
+#     row=${path[-2]}
+#     numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+#     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
+
+#     if [[ -n $(qstat -u x2755a09 | grep "${coord}${row}${numb} ") ]]; then
+#         if [[ -n $(qstat -u x2755a09 | grep "${coord}${row}${numb}stc") ]]; then
+#             pwd
+#             mv opt/* .
+#             rm -r opt
+#             qstat -u x2755a09 | grep --color=auto "${coord}${row}${numb}stc"
+#         fi
+#     fi
+# done
+
 for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
     cd $dir
     IFS='/' read -r -a path <<< $PWD
@@ -6,12 +24,15 @@ for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
     numb=$(echo "${path[-1]}" | cut -d'_' -f1)
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
 
-    if [[ -n $(qstat -u x2755a09 | grep "${coord}${row}${numb} ") ]]; then
-        if [[ -n $(qstat -u x2755a09 | grep "${coord}${row}${numb}stc") ]]; then
-            pwd
-            mv opt/* .
-            rm -r opt
-            qstat -u x2755a09 | grep --color=auto "${coord}${row}${numb}stc"
-        fi
+    if [[ ! -s lobsterin ]] && [[ -s lobsterout ]]; then
+        cp ~/bin/tools/tetra/lobsterin .
+        sed -i -e "s/X/$metal/g" lobsterin
+        cp ~/bin/tools/tetra/lobster.sh .
+        sed -i -e "s/jobname/${coord}${row}${numb}lob/" lobster.sh
+        qsub lobster.sh
+        pwd
+        exit
+    elif [[ ! -s icohp.txt ]] && [[ -s lobsterout ]]; then
+        echo -e "\e[36m$PWD\e[0m err"
     fi
 done
