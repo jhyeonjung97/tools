a559f9a38ee051e0ddf7f81c037c7f7d79dfab89
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-17 14:26:27 -0700

    .

--- tetra/.ipynb_checkpoints/static-checkpoint.sh
+++ tetra/.ipynb_checkpoints/static-checkpoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #PBS -N jobname
-#PBS -l select=1:ncpus=40:mpiprocs=10:ompthreads=4
-#PBS -l walltime=48:00:00
-#PBS -q norm_skl
+#PBS -l select=1:ncpus=64:mpiprocs=16:ompthreads=4
+#PBS -l walltime=12:00:00
+#PBS -q debug
 #PBS -A vasp
 #PBS -V
 
@@ -13,7 +13,7 @@ export OMP_NUM_THREADS=4
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-export VASP_SCRIPT=/home01/x2755a09/bin/tools/kisti/run_vasp10.py
+export VASP_SCRIPT=/home01/x2755a09/bin/tools/kisti/run_vasp16.py
 export VASP_PP_PATH=/home01/x2755a09/POTCAR/
 
 python /scratch/x2755a09/5_V_bulk/scripts/static_bulk2.py
