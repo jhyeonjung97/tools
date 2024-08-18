bc8a7d7e7c46fb4211a170a08719be212c4af79a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-18 05:52:40 -0700

    .

--- tetra/clean-up-log-files.sh
+++ tetra/clean-up-log-files.sh
@@ -22,6 +22,6 @@ for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/opt/; do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
     pwd
-    ls -t ${coord}${row}${numb}.o* | tail -n +2 | xargs rm -f
-    ls -t ${coord}${row}${numb}.e* | tail -n +2 | xargs rm -f
+    ls -t ${coord}${row}${numb}*.o* | tail -n +2 | xargs rm -f
+    ls -t ${coord}${row}${numb}*.e* | tail -n +2 | xargs rm -f
 done
