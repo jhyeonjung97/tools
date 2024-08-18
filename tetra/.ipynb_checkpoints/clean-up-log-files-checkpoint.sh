6a22f4db04d05d741199d50ddf38b1a1cd5d7e67
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-17 23:44:22 -0700

    .

--- tetra/.ipynb_checkpoints/clean-up-log-files-checkpoint.sh
+++ tetra/.ipynb_checkpoints/clean-up-log-files-checkpoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/opt/; do
+for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/; do
     cd $dir
     IFS='/' read -r -a path <<< "$PWD"
     coord=$(echo "${path[-4]}" | cut -d'_' -f3)
@@ -9,6 +9,6 @@ for dir in /scratch/x2755a09/5_V_bulk/*_*_*/*/*_*/opt/; do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
     pwd
-    ls -t ${coord}${row}${numb}*.o* | tail -n +2 | xargs rm -f
-    ls -t ${coord}${row}${numb}*.e* | tail -n +2 | xargs rm -f
+    ls -t ${coord}${row}${numb}.o* | tail -n +2 | xargs rm -f
+    ls -t ${coord}${row}${numb}.e* | tail -n +2 | xargs rm -f
 done
