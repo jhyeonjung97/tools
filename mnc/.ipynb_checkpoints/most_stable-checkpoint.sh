4ddb6745d89eae2031083b7a620aaad1ce662f02
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-20 03:31:05 -0700

    .

--- mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
+++ mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
@@ -27,9 +27,9 @@ for dz in "${dzs[@]}"; do
     done
     
     if [ -n "$lowest_dir" ]; then
-        echo "$lowest_dir/restart.json" "most_stable/${dz}_/"
-        cp "$lowest_dir/restart.json" "most_stable/${dz}_/"
-        cp "$lowest_dir/WAVECAR" "most_stable/${dz}_/"
+        echo "${lowest_dir}restart.json" "most_stable/${dz}_/"
+        cp "${lowest_dir}restart.json" "most_stable/${dz}_/"
+        cp "${lowest_dir}WAVECAR" "most_stable/${dz}_/"
         # cp ~/bin/tools/mnc/submit.sh "most_stable/${dz}_/"
     else
         echo "No valid directory found for dz=${dz}"
