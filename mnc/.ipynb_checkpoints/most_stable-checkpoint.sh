5fa943cba0505e8c906da1ae5cb7d9c9fabc24f7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-20 03:09:03 -0700

    .

--- mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
+++ mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-
 dir_now=$PWD
 dzs=(0 1 2 3 4 5 6)
 
-mkdir most_stable
+mkdir -p most_stable
 cd most_stable
-mkdir 0_ 1_ 2_ 3_ 4_ 5_ 6_
-cd $dir_now
+for dz in "${dzs[@]}"; do
+    mkdir -p "${dz}_"
+done
+cd "$dir_now"
 
-for dz in dzs
-do
+for dz in "${dzs[@]}"; do
     lowest_dir=""
-    lowest_energy=0
+    lowest_energy=999999 # Initialize to a very high value
+    
     for dir in *_*S/ ; do
         if [ -d "${dir}${dz}_/" ]; then
             if [ -f "${dir}${dz}_/DONE" ]; then
                 energy=$(grep -oP 'ENERGY\s+-\K[0-9]*\.?[0-9]+' "${dir}${dz}_/DONE")
                 if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
                     lowest_energy=$energy
-                    lowest_dir=$dir${dz}_/
+                    lowest_dir="${dir}${dz}_/"
                 fi
             fi
         fi
     done
-    cp $lowest_dir/restart.json most_stable/${dz}_/
-    cp $lowest_dir/WAVECAR most_stable/${dz}_/
-    cp ~/bin/tools/mnc/submit.sh most_stable/${dz}_/
+    
+    if [ -n "$lowest_dir" ]; then
+        cp "$lowest_dir/restart.json" "most_stable/${dz}_/"
+        cp "$lowest_dir/WAVECAR" "most_stable/${dz}_/"
+        cp ~/bin/tools/mnc/submit.sh "most_stable/${dz}_/"
+    else
+        echo "No valid directory found for dz=${dz}"
+    fi
 done
