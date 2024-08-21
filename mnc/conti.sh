1a8713bb1a06401e0ff9eb5caac4f5f326afe2cc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-22 06:08:36 +0900

    .

--- mnc/conti.sh
+++ mnc/conti.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-for dir in /scratch/x2755a09/3_MNC/0_clean/*d/*_*/*_*S
-do
-    cd $dir; pwd
-    IFS='/' read -r -a path <<< $PWD
-    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
-    spin=$(echo "${path[-1]}" | cut -d'_' -f2)
-    if [[ -d nupdown ]]; then
-        mkdir 1_ 2_ 3_ 4_ 5_ 6_
-        sh ~/bin/verve/spread.sh /scratch/x2755a09/3_MNC/0_clean/submit.sh
-        sh ~/bin/verve/spread.sh nupdown/restart.json
-        sh ~/bin/verve/spread.sh nupdown/WAVECAR
-        # mv nupdown 0_
-    else
-        echo -e "\e[36m$PWD\e[0m"
-    fi
-done
+# for dir in /scratch/x2755a09/3_MNC/0_clean/*d/*_*/*_*S
+# do
+#     cd $dir; pwd
+#     IFS='/' read -r -a path <<< $PWD
+#     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+#     spin=$(echo "${path[-1]}" | cut -d'_' -f2)
+#     if [[ -d nupdown ]]; then
+#         mkdir 1_ 2_ 3_ 4_ 5_ 6_
+#         sh ~/bin/verve/spread.sh /scratch/x2755a09/3_MNC/0_clean/submit.sh
+#         sh ~/bin/verve/spread.sh nupdown/restart.json
+#         sh ~/bin/verve/spread.sh nupdown/WAVECAR
+#         # mv nupdown 0_
+#     else
+#         echo -e "\e[36m$PWD\e[0m"
+#     fi
+# done
 
 for dir in /scratch/x2755a09/3_MNC/0_clean/*d/*_*/*_*S/*_
 do
@@ -25,7 +25,7 @@ do
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
     python ~/bin/tools/mnc/dz.py $dz
-    sed -i "/#PBE -N/c\#PBS -N $metal$spin$dz" submit.sh
+    sed -i "/#PBS -N/c\#PBS -N $metal$spin$dz" submit.sh
     if [[ $spin == 'LS' ]]; then
         sed -i 's/mnc-sol.py/mnc-sol-ls-nupdown.py/' submit.sh
     elif [[ $spin == 'IS' ]]; then
@@ -33,5 +33,5 @@ do
     elif [[ $spin == 'HS' ]]; then
         sed -i 's/mnc-sol.py/mnc-sol-hs-nupdown.py/' submit.sh
     fi
-    # qsub submit.sh
+    qsub submit.sh
 done
