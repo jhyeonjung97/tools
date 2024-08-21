7d8948cc3877f6b57bc3d6475b341e5d9b6f2147
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-21 02:00:43 -0700

    .

--- mnc/conti.sh
+++ mnc/conti.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/*_*S
-do
-    cd $dir; pwd
-    IFS='/' read -r -a path <<< $PWD
-    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
-    spin=$(echo "${path[-1]}" | cut -d'_' -f2)
-    if [[ -d nupdown ]]; then
-        mkdir 1_ 2_ 3_ 4_ 5_ 6_
-        sh ~/bin/verve/spread.sh nupdown/submit.sh
-        sh ~/bin/verve/spread.sh nupdown/restart.json
-        sh ~/bin/verve/spread.sh nupdown/WAVECAR
-        mv nupdown 0_
-    else
-        echo -e "\e[36m$PWD\e[0m"
-    fi
-done
-
-# for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/*_*S/*_
+# for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/*_*S
 # do
 #     cd $dir; pwd
 #     IFS='/' read -r -a path <<< $PWD
-#     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
-#     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
-#     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
-#     python ~/bin/tools/mnc/dz.py $dz
-#     sed -i "/#SBATCH -J/c\#SBATCH -J $metal$spin$dz" submit.sh
-#     sbatch submit.sh
+#     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+#     spin=$(echo "${path[-1]}" | cut -d'_' -f2)
+#     if [[ -d nupdown ]]; then
+#         mkdir 1_ 2_ 3_ 4_ 5_ 6_
+#         sh ~/bin/verve/spread.sh nupdown/submit.sh
+#         sh ~/bin/verve/spread.sh nupdown/restart.json
+#         sh ~/bin/verve/spread.sh nupdown/WAVECAR
+#         mv nupdown 0_
+#     else
+#         echo -e "\e[36m$PWD\e[0m"
+#     fi
 # done
+
+for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/*_*S/*_
+do
+    cd $dir; pwd
+    IFS='/' read -r -a path <<< $PWD
+    metal=$(echo "${path[-3]}" | cut -d'_' -f2)
+    spin=$(echo "${path[-2]}" | cut -d'_' -f2)
+    dz=$(echo "${path[-1]}" | cut -d'_' -f1)
+    python ~/bin/tools/mnc/dz.py $dz
+    sed -i "/#SBATCH -J/c\#SBATCH -J $metal$spin$dz" submit.sh
+    sed -i "s/s.py/s-nupdown.py/" submit.sh
+    sbatch submit.sh
+done
