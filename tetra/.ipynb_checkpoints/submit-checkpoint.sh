4d88da6bda337ea9193a7c1da4f8d20841a9deaa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-02 04:25:53 -0700

    .

---
+++ tetra/.ipynb_checkpoints/submit-checkpoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH -G 4
+#SBATCH -q regular
+#SBATCH -J jobname
+#SBATCH -t 12:00:00
+#SBATCH -A m2997
+#SBATCH -e err.%j.log
+#SBATCH -o out.%j.log
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+module load vasp-tpc/6.3.2-gpu
+
+export VASP_SCRIPT=/global/homes/j/jiuy97/bin/run_vasp.py
+export VASP_PP_PATH=/global/cfs/cdirs/m2997/vasp-psp/pseudo54
+
+python /pscratch/sd/j/jiuy97/3_V_shape/scripts/opt_slab2_afm.py
+python ~/bin/verve/bader.py
+python ~/bin/get_restart3
--- tetra/slab.sh
+++ tetra/slab.sh
@@ -21,16 +21,16 @@ for dir in /pscratch/sd/j/jiuy97/4_V_slab/kisti/6_V_slab/*_*_*/*/*_*/; do
         :
         # mkdir full_relaxed
         # python ~/bin/get_restart3
-        # cp restart.json full_relaxed
-        # cp /pscratch/sd/j/jiuy97/4_V_slab/kisti/6_V_slab/submit.sh full_relaxed
-        # cd full_relaxed
+        # cp restart.json full_relaxed/
+        # cp /global/homes/j/jiuy97/bin/tools/tetra/submit.sh full_relaxed/
+        # cd full_relaxed/
         # sed -i -e '/constraints/d' restart.json
         # sed -i -e "/#SBATCH -J/c\#SBATCH -J ${coord}${row}${numb}f" submit.sh
         # pwd; sbatch submit.sh
     elif [[ -s vasp.out ]]; then
         if [[ -n $(grep 'WARNING: random wavefunctions but no delay for mixing, default for NELMD' vasp.out) ]] || [[ -n $(grep 'please rerun with smaller EDIFF, or copy CONTCAR' vasp.out) ]] || [[ -n $(grep 'exceeded limit' *.e*) ]]; then
             python ~/bin/get_restart3
-            cp /pscratch/sd/j/jiuy97/4_V_slab/kisti/6_V_slab/submit.sh .
+            cp /global/homes/j/jiuy97/bin/tools/tetra/submit.sh .
             sed -i -e "/#SBATCH -J/c\#SBATCH -J ${coord}${row}${numb}s" submit.sh
             if [[ $row == 'fm' ]]; then
                 sed -i -e "s/opt_slab2_afm.py/opt_slab2_fm.py/" submit.sh
