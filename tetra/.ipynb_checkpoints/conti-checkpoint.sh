8513ec15e15778756199ab656c07e366ef67f1e7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-03 15:07:52 -0700

    .

--- tetra/.ipynb_checkpoints/conti-checkpoint.sh
+++ tetra/.ipynb_checkpoints/conti-checkpoint.sh
@@ -31,18 +31,6 @@ for dir in /pscratch/sd/j/jiuy97/3_V_bulk/*_*_*/*/*_*/; do
         elif [[ -s static.sh ]] && [[ -n $(grep 'Call to ZHEGV failed' vasp.out) ]]; then
             # sed -i -e "s/static_bulk2.py/static_bulk2_fast.py/" static.sh
             pwd; # sbatch static.sh
-        # elif [[ -n $(grep SYMPREC vasp.out) ]]; then
-        #     mv opt/* .; rm -r opt; rm WAVECAR
-        #     sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
-        #     pwd; qsub submit.sh
-        # elif [[ -n $(grep 'plane wave coefficients changed' vasp.out) ]]; then
-        #     mv opt/* .; rm -r opt; rm WAVECAR
-        #     sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
-        #     pwd; qsub submit.sh
-        # elif [[ -n $(grep 'Inconsistent Bravais lattice types' vasp.out) ]]; then
-        #     mv opt/* .; rm -r opt; rm WAVECAR
-        #     sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
-        #     pwd; qsub submit.sh
         else
             cp ~/bin/tools/tetra/lobsterin .
             sed -i -e "s/X/${metal}/g" lobsterin
@@ -55,30 +43,24 @@ for dir in /pscratch/sd/j/jiuy97/3_V_bulk/*_*_*/*/*_*/; do
             sed -i -e 's/m.py/m_ediff.py/' submit.sh
             sed -i -e 's/m_fast.py/m_ediff.py/' submit.sh
             sed -i -e 's/m_symprec.py/m_ediff.py/' submit.sh
-            sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
             echo -e "\e[35m$PWD\e[0m"; # sbatch submit.sh
         elif [[ -n $(grep Sub-Space-Matrix vasp.out) ]] || [[ -n $(grep EDDDAV vasp.out) ]]; then
             sed -i -e 's/m.py/m_fast.py/' submit.sh
             sed -i -e 's/m_ediff.py/m_fast.py/' submit.sh
             sed -i -e 's/m_symprec.py/m_fast.py/' submit.sh
-            sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
             echo -e "\e[35m$PWD\e[0m"; # sbatch submit.sh
-        # elif [[ -n $(grep SYMPREC vasp.out) ]]; then
-        #     sed -i -e 's/m.py/m_symprec.py/' submit.sh
-        #     sed -i -e 's/m_ediff.py/m_symprec.py/' submit.sh
-        #     sed -i -e 's/m_fast.py/m_symprec.py/' submit.sh
-        #     sed -i -e '/walltime/c\#PBS -l walltime=06:00:00' submit.sh
-        #     pwd; qsub submit.sh
         elif [[ -s DONE ]]; then
             mkdir opt; find . -maxdepth 1 -mindepth 1 ! -name opt -exec mv {} opt/ \;
             cp opt/restart.json opt/WAVECAR .
             cp ~/bin/tools/tetra/lobsterin .
             sed -i -e "s/X/$metal/g" lobsterin
             cp ~/bin/tools/tetra/static.sh .
-            sed -i -e "s/jobname/${coord}${row}${numb}t/g" static.sh 
-            echo -e "\e[35m$PWD\e[0m"; # sbatch static.sh
+            sed -i -e "s/jobname/${coord}${row}${numb}t/" static.sh 
+            echo -e "\e[35m$PWD\e[0m"; sbatch static.sh
         else
-            echo -e "\e[35m$PWD\e[0m"
+            cp ~/bin/tools/tetra/submit-bulk.sh submit.sh
+            sed -i -e "s/jobname/${coord}${row}${numb}/" submit.sh 
+            pwd; sbatch submit.sh
         fi
     else
         echo -e "\e[32m$PWD\e[0m"
