9508b8f94ae1db852746d12451e570fb8b0bfbea
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 21:51:23 -0700

    .

--- mnc/.ipynb_checkpoints/tmp-checkpoint.sh
+++ mnc/.ipynb_checkpoints/tmp-checkpoint.sh
@@ -1,6 +1,7 @@
 for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*/*_*/*_*S;
 do
     cd $dir
+    IFS='/' read -r -a path <<< $PWD
     path3=${path[-3]}
     path2=${path[-2]}
     path1=${path[-1]}
