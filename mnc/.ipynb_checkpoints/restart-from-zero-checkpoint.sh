52f4eaf5bd3ff1c85e110efbac3effffd808a8b5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-05 14:55:55 -0700

    .

--- mnc/.ipynb_checkpoints/restart-from-zero-checkpoint.sh
+++ mnc/.ipynb_checkpoints/restart-from-zero-checkpoint.sh
@@ -27,13 +27,13 @@ do
         cd ${dir}
     done
 done
-for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S/*_/
-do
-    cd $dir; pwd
-    IFS='/' read -r -a path <<< $PWD
-    ads=$(echo "${path[-5]}" | cut -d'_' -f2)
-    metal=$(echo "${path[-3]}" | cut -d'_' -f2)
-    spin=$(echo "${path[-2]}" | cut -d'_' -f2)
-    sed -i "/#SBATCH -J/c\#SBATCH -J ${ads}${metal}${spin}${dz}" submit.sh
-    sbatch submit.sh
-done
+# for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S/*_/
+# do
+#     cd $dir; pwd
+#     IFS='/' read -r -a path <<< $PWD
+#     ads=$(echo "${path[-5]}" | cut -d'_' -f2)
+#     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
+#     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
+#     sed -i "/#SBATCH -J/c\#SBATCH -J ${ads}${metal}${spin}${dz}" submit.sh
+#     sbatch submit.sh
+# done
