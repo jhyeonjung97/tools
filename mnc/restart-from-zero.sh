8b5f8ffa08cf75fdbb91478ff25e788d4c2afa34
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-05 14:52:17 -0700

    .

--- mnc/restart-from-zero.sh
+++ mnc/restart-from-zero.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# metals=('2_Ti' '3_V' '4_Cr' '5_Mn' '6_Fe' '7_Co' '8_Ni' '9_Cu')
-# spins=('1_LS' '2_IS' '2_HS' '3_HS')
-# dzs=('1_' '2_' '3_' '4_' '5_' '6_')
-
-mkdir "$1"_
-mv * "$1"_
-
-IFS='/' read -r -a path_components <<< $PWD
-metal=$(echo "${path_components[-2]}" | cut -d'_' -f2)
-spin=$(echo "${path_components[-1]}" | cut -d'_' -f2)
-
-dzs=(0 1 2 3 4 5 6)
-dir_now=$PWD
-for dz in ${dzs[@]}; do
-    if [[ $dz != $1 ]]; then
-        mkdir "$dz"_/
-        cp "$1"_/restart.json "$dz"_/
-        cp "$1"_/WAVECAR "$dz"_/
-        # cp "$1"_/CHGCAR "$dz"_/
-        cd "$dz"_/
-        cp /scratch/x2755a09/3_MNC/3d/submit.sh .
-        sed -i -e "/#PBS -N/c\#PBS -N $metal$spin$dz" submit.sh
-        python ~/bin/tools/mnc/dz.py $dz
-        qsub submit.sh
-        cd $dir_now
-    fi
+squeue --me > ~/mystat.txt
+for dir in /pscratch/sd/j/jiuy97/6_MNC/1_O/*d/*_*/*_*S/
+do
+    cd ${dir}; pwd
+    IFS='/' read -r -a path <<< $PWD
+    ads=$(echo "${path[-4]}" | cut -d'_' -f2)
+    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+    spin=$(echo "${path[-1]}" | cut -d'_' -f2)
+    for dz in range(0, 6)
+    do
+        mkdir ${dz}_
+        cp ~/bin/tools/mnc/submit.sh ${dz}_
+        cp nupdown/restart.json nupdown/WAVECAR ${dz}_
+        sed -i "/#SBATCH -J/c\#SBATCH -J ${metal}${spin}${ads}${dz}" ${dz}_/submit.sh
+        if [[ ${spin} == 'LS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-ls-nupdown.py/' ${dz}_/submit.sh
+        elif [[ ${spin} == 'IS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-is-nupdown.py/' ${dz}_/submit.sh
+        elif [[ ${spin} == 'HS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-hs-nupdown.py/' ${dz}_/submit.sh
+        fi
+        cd ${dz}_
+        python ~/bin/tools/mnc/dz.py ${dz}
+        # sbatch submit.sh
+        cd ${dir}
+    done
 done
