dbb3c240e5206ae920edf01a86a9e18f3cc677b3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 21:48:17 -0700

    .

--- mnc/most_stable.sh
+++ mnc/most_stable.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 
-dir_now=$PWD
-dzs=(0 1 2 3 4 5 6)
-
-mkdir -p most_stable
-cd most_stable
-for dz in ${dzs[@]}
+for dir in /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/*d/*_*
 do
-    mkdir -p ${dz}_
-done
-cd $dir_now
-
-for dz in ${dzs[@]}
-do
-    lowest_dir=''
-    lowest_energy=0
-    
-    for dir in *_*S/
-    do
-        if [ -d ${dir}${dz}_/ ]; then
-            if [ -f ${dir}${dz}_/DONE ]; then
-                energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${dir}${dz}_/DONE")
-                if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
-                    lowest_energy=$energy
-                    lowest_dir=${dir}${dz}_/
-                fi
-            fi
+    cd $dir
+    dzs=(0 1 2 3 4 5 6)
+    all_done=true
+    for i in {1..6}; do
+        if [[ ! -f "${i}_/DONE" ]]; then
+            echo "DONE file is missing in ${i}_/"
+            all_done=false
         fi
     done
     
-    if [ -n "$lowest_dir" ]; then
-        echo "${lowest_dir}restart.json" "most_stable/${dz}_/"
-        cp "${lowest_dir}restart.json" "most_stable/${dz}_/"
-        cp "${lowest_dir}WAVECAR" "most_stable/${dz}_/"
-        # cp ~/bin/tools/mnc/submit.sh "most_stable/${dz}_/"
+    # Check the flag status
+    if $all_done; then
+        echo "All DONE files are present."
     else
-        echo "No valid directory found for dz=${dz}"
+        echo "Some DONE files are missing."
     fi
-done
+    
+    mkdir -p most_stable
+    cd most_stable
+    for dz in ${dzs[@]}
+    do
+        mkdir -p ${dz}_
+    done
+    cd $dir_now
+    
+    for dz in ${dzs[@]}
+    do
+        lowest_dir=''
+        lowest_energy=0
+        
+        for dir in *_*S/
+        do
+            if [ -d ${dir}${dz}_/ ]; then
+                if [ -f ${dir}${dz}_/DONE ]; then
+                    energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${dir}${dz}_/DONE")
+                    if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
+                        lowest_energy=$energy
+                        lowest_dir=${dir}${dz}_/
+                    fi
+                fi
+            fi
+        done
+        
+        if [ -n "$lowest_dir" ]; then
+            echo "${lowest_dir}restart.json" "most_stable/${dz}_/"
+            cp "${lowest_dir}restart.json" "most_stable/${dz}_/"
+            cp "${lowest_dir}WAVECAR" "most_stable/${dz}_/"
+            # cp ~/bin/tools/mnc/submit.sh "most_stable/${dz}_/"
+        else
+            echo "No valid directory found for dz=${dz}"
+        fi
+    done
--- mnc/tmp.py
+++
@@ -1,7 +0,0 @@
-for metal in [2_Ti  3_V  4_Cr  5_Mn  6_Fe  7_Co  8_Ni  9_Cu]:
-    for spin in [1_LS 2_IS 2_HS 3_HS]:
-        for dz in [1_ 2_ 3_ 4_ 5_ 6_]:
-            path = os.path(f'/scratch/x2755a09/3_MNC/3d/{metal}/{spin}/{dz}')
-            if path.exist():
-                cd path
-                qsub submit.sh
