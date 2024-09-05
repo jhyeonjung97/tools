a4d83851238e7e90633160ed9e9b7cb1abd1fa9a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 22:30:25 -0700

    .

--- mnc/.ipynb_checkpoints/tmp-checkpoint.sh
+++ mnc/.ipynb_checkpoints/tmp-checkpoint.sh
@@ -5,7 +5,5 @@ do
     path3=${path[-3]}
     path2=${path[-2]}
     path1=${path[-1]}
-    echo "mv $dir/nupdown/* /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/$path3/$path2/$path1/0_"
-    echo "cd /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/$path3/$path2/$path1"
-    echo "mv nupdown 0_"
+    mv $dir/nupdown/* /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/$path3/$path2/$path1/0_
 done
