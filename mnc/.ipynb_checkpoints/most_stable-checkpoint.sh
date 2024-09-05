ecbd566c3864719e2afda92a0e878d5810f08d29
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 21:59:55 -0700

    .

--- mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
+++ mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
@@ -2,27 +2,19 @@
 
 for dir in /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/*d/*_*
 do
-    if cd "$dir"; then
-        pwd
-        all_done=true
-        
-        # Loop through the pattern */${i}_/DONE for i from 0 to 6
-        for sub_dir in ./*/*
-        do
-            if [[ ! -f $sub_dir/DONE ]]; then
-                echo "DONE file is missing in $sub_dir/DONE"
-                all_done=false
-            fi
-        done
-        
-        # Report the result
-        if $all_done; then
-            echo "All DONE files are present in $dir."
-        else
-            echo "Some DONE files are missing in $dir."
+    cd $dir; pwd
+    all_done=true
+    for sub_dir in ./*/*
+    do
+        if [[ ! -f $sub_dir/DONE ]]; then
+            echo "DONE file is missing in $sub_dir/DONE"
+            all_done=false
         fi
+    done
+    if $all_done; then
+        echo "All DONE files are present in $dir."
     else
-        echo "Failed to change to directory $dir"
+        echo "Some DONE files are missing in $dir."
     fi
 done
     # mkdir -p most_stable
