f6c4236e255cce42362758492b88388556c00f8e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 22:13:23 -0700

    .

--- mnc/most_stable.sh
+++ mnc/most_stable.sh
@@ -2,7 +2,6 @@
 
 for dir in /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/*d/*_*/
 do
-    cd $dir; pwd
     cd $dir
     IFS='/' read -r -a path <<< $PWD
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
@@ -15,6 +14,7 @@ do
         fi
     done
     if $all_done; then
+        pwd
         mkdir -p most_stable
         cd most_stable
         dzs=(0 1 2 3 4 5 6)
@@ -30,12 +30,13 @@ do
             for sub_dir in *_*S/
             do
                 energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${sub_dir}${dz}_/DONE")
+                echo $energy
                 if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
                     lowest_energy=$energy
                     lowest_dir=${sub_dir}${dz}_/
                 fi
             done
-            if [ -n "$lowest_dir" ]; then
+            if [[ -n "$lowest_dir" ]]; then
                 echo "${lowest_dir}restart.json most_stable/${dz}_/"
                 cp ${lowest_dir}restart.json most_stable/${dz}_/
                 cp ${lowest_dir}WAVECAR most_stable/${dz}_/
