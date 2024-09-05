ab1db6ee5bcb203822d636b4a99f8eafc2931aad
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-04 22:27:09 -0700

    .

--- mnc/most_stable.sh
+++ mnc/most_stable.sh
@@ -6,51 +6,51 @@ do
     cd $dir
     IFS='/' read -r -a path <<< $PWD
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
-    if [[ ! -n $(grep ${metal}MS${dz} ~/mystat.txt) ]]; then
-        all_done=true
-        for sub_sub_dir in ./*_*S/*_/
+    all_done=true
+    for sub_sub_dir in ./*_*S/*_/
+    do
+        if [[ ! -s ${sub_sub_dir}DONE ]]; then
+            # echo "DONE file is missing in $dir$sub_sub_dir/DONE"
+            all_done=false
+        fi
+    done
+    if [[ $all_done == true ]]; then
+        mkdir -p most_stable
+        cd most_stable
+        dzs=(0 1 2 3 4 5 6)
+        for dz in ${dzs[@]}
         do
-            if [[ ! -s ${sub_sub_dir}DONE ]]; then
-                echo "DONE file is missing in $dir$sub_sub_dir/DONE"
-                all_done=false
-            fi
+            mkdir -p ${dz}_
         done
-        if [[ $all_done == true ]]; then
-            mkdir -p most_stable
-            cd most_stable
-            dzs=(0 1 2 3 4 5 6)
-            for dz in ${dzs[@]}
+        cd $dir; pwd
+        for dz in ${dzs[@]}
+        do
+            lowest_dir=''
+            lowest_energy=0
+            for sub_dir in *_*S/
             do
-                mkdir -p ${dz}_
-            done
-            cd $dir; pwd
-            for dz in ${dzs[@]}
-            do
-                lowest_dir=''
-                lowest_energy=0
-                for sub_dir in *_*S/
-                do
-                    energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${sub_dir}${dz}_/DONE")
-                    if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
-                        lowest_energy=$energy
-                        lowest_dir=${sub_dir}${dz}_/
-                    fi
-                done
-                if [[ -n "$lowest_dir" ]]; then
-                    echo "${lowest_dir}restart.json most_stable/${dz}_/"
-                    cp ${lowest_dir}restart.json most_stable/${dz}_/
-                    cp ${lowest_dir}WAVECAR most_stable/${dz}_/
-                    cp ~/bin/tools/mnc/submit.sh most_stable/${dz}_/
-                    sed -i -e "s/jobname/${metal}MS${dz}/" most_stable/${dz}_/submit.sh
-                else
-                    echo "No valid directory found for dz=${dz}"
+                energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${sub_dir}${dz}_/DONE")
+                if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
+                    lowest_energy=$energy
+                    lowest_dir=${sub_dir}${dz}_/
                 fi
             done
-            for sub_sub_dir in ${dir}most_stable/*_/
-            do
-                cd $sub_sub_dir
+            if [[ -n "$lowest_dir" ]]; then
+                echo "${lowest_dir}restart.json most_stable/${dz}_/"
+                cp ${lowest_dir}restart.json most_stable/${dz}_/
+                cp ${lowest_dir}WAVECAR most_stable/${dz}_/
+                cp ~/bin/tools/mnc/submit.sh most_stable/${dz}_/
+                sed -i -e "s/jobname/${metal}MS${dz}/" most_stable/${dz}_/submit.sh
+            else
+                echo "No valid directory found for dz=${dz}"
+            fi
+        done
+        for dz in ${dzs[@]}
+        do
+            if [[ ! -n $(grep ${metal}MS${dz} ~/mystat.txt) ]]; then
+                cd most_stable/${dz}_/
                 pwd; sbatch submit.sh
-            done
-        fi
+            fi
+        done
     fi
 done
