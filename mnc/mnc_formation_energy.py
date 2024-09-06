557bfe2c352487add54b55bb9a108aefc29b9545
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-06 16:54:04 -0700

    .

--- mnc/mnc_formation_energy.py
+++ mnc/mnc_formation_energy.py
@@ -280,8 +280,11 @@ def plot_smooth_line(x, y, color, label):
         else:
             spl = make_interp_spline(x, y, k=2)
         y_smooth = spl(x_new)
-        plt.plot(x_new, y_smooth, color=color, label=label, zorder=1)
-        plt.scatter(x, y, marker='s', color=color, edgecolors=color, facecolors='white', label='_nolegend_', zorder=2)
+        line, = plt.plot(x, y_smooth, color=color, label='Smooth Line', zorder=1)
+        scatter = plt.scatter(x, y, marker='s', color=color, edgecolors=color, facecolors='white', label='Scatter', zorder=2)
+        # plt.plot(x_new, y_smooth, color=color, label=label, zorder=1)
+        # plt.scatter(x, y, marker='s', color=color, edgecolors=color, facecolors='white', label='_nolegend_', zorder=2)
+        plt.legend(handles=[line, scatter], labels=[label])
     except ValueError as e:
         print(f"Error while creating spline: {e}")
 
