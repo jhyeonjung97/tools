22e9f510e3cabb3bb5ac1ccf511ce54c1ac9febb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-06 12:10:32 -0700

    .

--- mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
+++ mnc/.ipynb_checkpoints/most_stable-checkpoint.sh
@@ -42,7 +42,7 @@ do
                 clean_dir=$(echo "$lowest_sub_dir" | sed 's:/$::')
                 spin=$(echo "$clean_dir" | cut -d'_' -f2)
                 echo -e "$product\t$spin" >> lowest.txt
-                if [[ ! -d most_stable/0_/ ]]; then
+                if [[ ! -f most_stable/0_/restart.json ]]; then
                     echo "${lowest_dir}restart.json most_stable/${dz}_/"
                     # cp ${lowest_dir}restart.json most_stable/${dz}_/
                     # cp ${lowest_dir}WAVECAR most_stable/${dz}_/
