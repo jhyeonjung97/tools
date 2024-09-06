32c4b075074e987b6c34b3b96f9b972074f9b79d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-06 15:47:20 -0700

    .

--- mnc/.ipynb_checkpoints/conti-checkpoint.sh
+++ mnc/.ipynb_checkpoints/conti-checkpoint.sh
@@ -3,21 +3,28 @@
 squeue --me > ~/mystat.txt
 # cat ~/kisti.txt ~/nersc.txt > ~/mystat.txt
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/*d/*_*/*_*S/*_
+for dir in /pscratch/sd/j/jiuy97/6_MNC/kisti/3_MNC/0_clean/*d/*_*/*_*S/*
 do
     cd $dir
     IFS='/' read -r -a path <<< $PWD
     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
-    cp ~/bin/tools/mnc/submit.sh .
-    sed -i "/#SBATCH -J/c\#SBATCH -J $metal$spin$dz" submit.sh
-    if [[ $spin == 'LS' ]]; then
-        sed -i 's/mnc-sol.py/mnc-sol-ls-nupdown.py/' submit.sh
-    elif [[ $spin == 'IS' ]]; then
-        sed -i 's/mnc-sol.py/mnc-sol-is-nupdown.py/' submit.sh
-    elif [[ $spin == 'HS' ]]; then
-        sed -i 's/mnc-sol.py/mnc-sol-hs-nupdown.py/' submit.sh
+    if [[ $dz == 'relaxed' ]]; then
+        dz='r'
+    elif [[ $dz == 'x' ]]; then
+        continue
+    fi
+    if [[ ! -f submit.sh ]]; then
+        cp ~/bin/tools/mnc/submit.sh .
+        sed -i "/#SBATCH -J/c\#SBATCH -J $metal$spin$dz" submit.sh
+        if [[ $spin == 'LS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-ls-nupdown.py/' submit.sh
+        elif [[ $spin == 'IS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-is-nupdown.py/' submit.sh
+        elif [[ $spin == 'HS' ]]; then
+            sed -i 's/mnc-sol.py/mnc-sol-hs-nupdown.py/' submit.sh
+        fi
     fi
     if [[ -n $(grep $metal$spin$dz ~/mystat.txt) ]]; then
         :
@@ -26,7 +33,7 @@ do
             :
         else
             sh ~/bin/verve/correct-contcar.sh; python ~/bin/get_restart3
-            ~/bin/shoulder/rm_mv *.e* *.o* *.log
+            # ~/bin/shoulder/rm_mv *.e* *.o* *.log
             pwd; sbatch submit.sh
             # if [[ -n $(grep 'please rerun with smaller EDIFF' vasp.out) ]]; then
             # elif [[ -n $(grep 'Call to ZHEGV failed' vasp.out) ]]; then
@@ -37,7 +44,6 @@ do
         fi
     else
         python ~/bin/tools/mnc/dz.py $dz
-        pwd; sbatch submit.sh
-        # echo -e "\e[35m$PWD\e[0m"
+        echo -e "\e[35m$PWD\e[0m"; sbatch submit.sh
     fi
 done
