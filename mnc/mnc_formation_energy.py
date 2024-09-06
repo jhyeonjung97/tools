aff9cc77f9c79fb11c0cd9e174da1e20ccfe3ee1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-06 11:31:25 -0700

    .

--- mnc/mnc_formation_energy.py
+++ mnc/mnc_formation_energy.py
@@ -280,7 +280,7 @@ def plotting(df, df_relaxed, dzs, spins, ylabel, png_filename, ymin=None, ymax=N
         plt.ylim(ymin, ymax)
     if yticks is not None:
         plt.yticks(yticks)
-    plt.legend(labelspacing=0.4)
+    plt.legend(labelspacing=0.3)
     plt.tight_layout()
     plt.savefig(png_filename, bbox_inches="tight")
     print(f"Figure saved as {png_filename}")
