8fa5fbf793ef7dba0498d94436de32fdaa3f451d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-06 11:02:10 -0700

    .

--- mnc/.ipynb_checkpoints/final_with_calculator-checkpoint.sh
+++ mnc/.ipynb_checkpoints/final_with_calculator-checkpoint.sh
@@ -4,6 +4,6 @@ for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/*_*S/*/
 do
     if [[ ! -f ${dir}final_with_calculator.json ]] && [[ -s ${dir}DONE ]]; then
         cd ${dir}; pwd
-        # ase convert -n -1 OUTCAR final_with_calculator.json
+        ase convert -n -1 OUTCAR final_with_calculator.json
     fi
 done
