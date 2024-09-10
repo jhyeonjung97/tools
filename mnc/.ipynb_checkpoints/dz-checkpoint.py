842e95dcd2ee45aec0437a2875c25e79b742f549
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-09 23:34:29 -0700

    .

--- mnc/.ipynb_checkpoints/dz-checkpoint.py
+++ mnc/.ipynb_checkpoints/dz-checkpoint.py
@@ -12,9 +12,7 @@ for atom in atoms:
     if atom.symbol not in ['C', 'N', 'O', 'H']:
         atom.z = 10.0 + 0.2 * dz
         index = atom.index
-    elif atom.index == index+1 or atom.symbol == 'O':
-        atom.z = 12.0 + 0.2 * dz
-    elif atom.index == index+2 and atom.symbol == 'H':
-        atom.z = 12.6 + 0.2 * dz
+    elif atom.index > index:
+        atom.z += 0.2 * dz
         
 write('restart.json', atoms)
