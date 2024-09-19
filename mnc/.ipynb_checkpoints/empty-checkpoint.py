a04263404a04b72b832dcc318d31a52f6a0f5ffa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-19 11:52:06 -0700

    .

--- mnc/.ipynb_checkpoints/empty-checkpoint.py
+++ mnc/.ipynb_checkpoints/empty-checkpoint.py
@@ -2,7 +2,7 @@ from ase.io import read, write
 from ase import Atoms
 
 metal_index = None
-atoms = read('../1_LS/relaxed/restart.json')
+atoms = read('restart.json')
 
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     metal_index = atoms[-1].index
