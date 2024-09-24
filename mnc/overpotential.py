5080ac9761b895541c5e9401cd26d0756646cc9e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-24 15:41:42 -0700

    .

--- mnc/overpotential.py
+++ mnc/overpotential.py
@@ -9,7 +9,8 @@ from matplotlib.lines import Line2D
 from matplotlib.ticker import FormatStrFormatter
 from scipy.interpolate import make_interp_spline
 
-steps = ['H2O->*OH','*OH->*O', '*O->*OOH', '*OOH->O2']
+OERs = ['H2O->*OH','*OH->*O', '*O->*OOH', '*OOH->O2']
+ORRs = ['O2->*OOH','*OOH->*O', '*O->*OH', '*OH->H2O']
 rows = ['3d', '3d', '3d', '3d', '4d', '5d']
 groups = ['5', '6', '7', '8', '4', '4']
 metals = ['Mn', 'Fe', 'Co', 'Ni', 'Mo', 'W']
@@ -75,30 +76,93 @@ for m, metal in enumerate(metals):
     gibbs_energies['G_OH'] = energies['OH']['energy'] + OH_corr
     gibbs_energies['G_O'] = energies['O']['energy'] + O_corr
     
+    G_ = min(gibbs_energies['G_'])
+    G_OH = min(gibbs_energies['G_OH'])
+    G_O= min(gibbs_energies['G_O'])
+    
     gibbs_energies['dG_OH'] = gibbs_energies['G_OH'] - gibbs_energies['G_'] - hydroxide_G
     gibbs_energies['dG_O'] = gibbs_energies['G_O'] - gibbs_energies['G_'] - oxygen_G
     gibbs_energies['dG_OOH'] = gibbs_energies['dG_OH'] + 3.2
-                                                               
+    
+    dG_OH = G_OH - G_ - hydroxide_G
+    dG_O = G_O - G_ - oxygen_G
+    dG_OOH = dG_OH + 3.2
+    
     gibbs_energies['dG1'] = gibbs_energies['dG_OH']
     gibbs_energies['dG2'] = gibbs_energies['dG_O'] - gibbs_energies['dG_OH']
     gibbs_energies['dG3'] = gibbs_energies['dG_OOH'] - gibbs_energies['dG_O']
     gibbs_energies['dG4'] = 4.92 - gibbs_energies['dG_OOH']
-
+    
+    dG1 = dG_OH
+    dG2 = dG_O - dG_OH
+    dG3 = dG_OOH - dG_O
+    dG4 = 4.92 - dG_OOH
+    
     if gibbs_energies[['dG1', 'dG2', 'dG3', 'dG4']].notna().all().all():
         gibbs_energies['OER'] = gibbs_energies[['dG1', 'dG2', 'dG3', 'dG4']].max(axis=1) - 1.23
+        gibbs_energies['ORR'] = 1.23 - gibbs_energies[['dG1', 'dG2', 'dG3', 'dG4']].min(axis=1)
     else:
         gibbs_energies['OER'] = None
+        gibbs_energies['ORR'] = None
         
-    gibbs_energies = gibbs_energies.round(2)
+    if all([dG1 is not None, dG2 is not None, dG3 is not None, dG4 is not None]):
+        OER = max(dG1, dG2, dG3, dG4) - 1.23
+        ORR = 1.23 - min(dG1, dG2, dG3, dG4)
+    else:
+        OER = None
+        ORR = None
+        
+    # gibbs_energies = gibbs_energies.round(2)
     gibbs_energies = gibbs_energies.set_index(energies['clean'].index)
-
-    for index in energies['clean'].index:
-        spin_cross_over.loc[index, steps[0]] = f"{energies['clean']['spin'].loc[index]}->{energies['OH']['spin'].loc[index]}"
-        spin_cross_over.loc[index, steps[1]] = f"{energies['OH']['spin'].loc[index]}->{energies['O']['spin'].loc[index]}"
         
-    if metal == 'Mn':
-        print(energies['clean'])
-        print(energies['O'])
-        print(energies['OH'])
-        print(gibbs_energies)
-        print(spin_cross_over)
+    for index in energies['clean'].index:
+        spin_cross_over.loc[index, OERs[0]] = f"{energies['clean']['spin'].loc[index]}->{energies['OH']['spin'].loc[index]}"
+        spin_cross_over.loc[index, OERs[1]] = f"{energies['OH']['spin'].loc[index]}->{energies['O']['spin'].loc[index]}"
+        spin_cross_over.loc[index, ORRs[2]] = f"{energies['O']['spin'].loc[index]}->{energies['OH']['spin'].loc[index]}"
+        spin_cross_over.loc[index, ORRs[3]] = f"{energies['OH']['spin'].loc[index]}->{energies['clean']['spin'].loc[index]}"
+
+    
+    gibbs_energies.to_csv(f'{row}_{group}{metal}_gibbs.tsv', sep='\t', float_format='%.2f')
+    spin_cross_over.to_csv(f'{row}_{group}{metal}_spin.tsv', sep='\t')
+    print(f"Data saved to {row}_{group}{metal}_gibbs.tsv and {row}_{group}{metal}_spin.tsv")
+    
+    plotting(gibbs_energies=gibbs_energies, spin_cross_over=spin_cross_over, 
+             rxn=OER, ylabel='Energy (eV)', png_filename=f'{row}_{group}{metal}_spin.png')
+
+def plot_smooth_line(x, y, color):
+    try:
+        x_new = np.linspace(min(x), max(x), 300)
+        if len(x) > 3:
+            spl = make_interp_spline(x, y, k=3)  # Smoothing spline
+        else:
+            spl = make_interp_spline(x, y, k=2)
+        y_smooth = spl(x_new)
+        plt.plot(x_new, y_smooth, color=color, zorder=1)
+        plt.scatter(x, y, marker='s', edgecolors=color, facecolors='white', zorder=2)
+        return y_smooth
+    except ValueError as e:
+        print(f"Error while creating spline: {e}")
+        return None
+
+def plotting(gibbs_energies, spin_cross_over, rxn, ylabel, png_filename):
+    if gibbs_energies.isna().all().all():
+        print("df contains only NaN values, skipping plot.")
+        return
+    
+    plt.figure(figsize=(4, 3))
+    filtered_df = gibbs_energies['OER'].dropna()
+    if not filtered_df.empty:
+        x = filtered_df.index
+        y = filtered_df.values
+        plt.scatter(x, y)
+    if rxn:
+        plt.axhline(y=rxn, color='blue', linestyle='--', zorder=0)
+    plt.xlabel('dz (Å)')
+    plt.ylabel(ylabel)
+    plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.1f'))  # Fix to 0.0 format
+    plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%.1f'))  # Fix to 0.0 format
+    # plt.legend(labelspacing=0.3)
+    plt.tight_layout()
+    plt.savefig(png_filename, bbox_inches="tight")
+    print(f"Figure saved as {png_filename}")
+    plt.close()
