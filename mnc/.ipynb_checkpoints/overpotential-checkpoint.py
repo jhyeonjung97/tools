45c16ab3f1de39e152e212a4c834af4e2ec0badd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-24 09:20:20 -0700

    .

--- mnc/.ipynb_checkpoints/overpotential-checkpoint.py
+++ mnc/.ipynb_checkpoints/overpotential-checkpoint.py
@@ -1,13 +1,13 @@
-import matplotlib.pyplot as plt
+import os
+import glob
+import numpy as np
+import pandas as pd
 from ase.io import read
 from statistics import mean
-import glob
-import os
-import pandas as pd
-from scipy.interpolate import make_interp_spline
-import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.ticker import FormatStrFormatter
+from scipy.interpolate import make_interp_spline
 
 steps = ['H2O->*OH','*OH->*O', '*O->*OOH', '*OOH->O2']
 rows = ['3d', '3d', '3d', '3d', '4d', '5d']
@@ -24,7 +24,8 @@ for i in range(6):
     for adsorbate in adsorbates:
         tsv_path = os.path.join(root, f'{row}_{group}{metal}_{adsorbate}.tsv')
         energies[adsorbate] = pd.read_csv(tsv_path, sep='\t', index_col=0)
-             
+        energies[adsorbate] = energies[adsorbate].head(7)
+
         if 'LS' in energies[adsorbate].columns:
             energies[adsorbate] = energies[adsorbate].drop(columns=['LS'])
         if 'IS' in energies[adsorbate].columns:
