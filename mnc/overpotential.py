bb20d9be184d97b0c9faf6069da4d3b2d03d619b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-24 16:07:02 -0700

    .

--- mnc/overpotential.py
+++ mnc/overpotential.py
@@ -172,7 +172,7 @@ def plotting(gibbs_energies, spin_cross_over, row, group, metal,
         plt.axhline(y=overpotential, color='black', linestyle='--', linewidth=1.0, zorder=0)
     plt.xlabel('dz (Å)')
     plt.ylabel(ylabel)
-    plt.ylim(0.4, 1.4)
+    plt.ylim(0.4, 1.2)
     plt.yticks(np.arange(0.4, 1.4, 0.1))
     plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.1f'))  # Fix to 0.0 format
     plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%.1f'))  # Fix to 0.0 format
