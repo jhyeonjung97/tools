6d17a23a0024aeb70fad9cad9a4762805e1a5378
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-24 17:35:04 -0700

    .

--- mnc/.ipynb_checkpoints/overpotential-checkpoint.py
+++ mnc/.ipynb_checkpoints/overpotential-checkpoint.py
@@ -216,7 +216,7 @@ def plotting(gibbs_energies, spin_cross_over, row, group, metal,
                 elif dGmax == 'dG3':
                     plot_two_color_marker(ax, xi, yi, size=marker_size, color1=color_O, color2=color_OOH)
                 elif dGmax == 'dG4':
-                    plot_two_color_marker(ax, xi, yi, size=marker_size, color1=color_OOH, color2=color_, width = width, height = height)
+                    plot_two_color_marker(ax, xi, yi, size=marker_size, color1=color_OOH, color2=color_)
                 # plot_three_color_marker(ax, xi, yi, size=0.05, color0=color0, color1=color1, color2=color2)
                 ax.annotate(dGmax, (xi, yi), textcoords="offset points", xytext=(0, 6), ha='center', color='black')
         except ValueError as e:
