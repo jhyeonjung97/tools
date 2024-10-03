f8ed2d8de74706f20ba500cc799d7e01601cee7f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-02 22:24:55 -0700

    .

--- mnc/.ipynb_checkpoints/formation-energy-checkpoint.py
+++ mnc/.ipynb_checkpoints/formation-energy-checkpoint.py
@@ -86,6 +86,8 @@ metal_df = pd.read_csv(metal_path, delimiter='\t', index_col=0)
 def main():
     for row_key, metals in rows.items():
         for m, metal in enumerate(metals):
+            if not metal == 'Ti':
+                exit()
             df = pd.DataFrame()
             Ef = pd.DataFrame()
             df_dz = pd.DataFrame()
