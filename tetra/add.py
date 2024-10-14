068957d019ad44226398d25f31b9054a26f0f7e1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-14 13:24:36 -0700

    .

--- tetra/add.py
+++ tetra/add.py
@@ -41,6 +41,7 @@ for dir in glob.glob('/pscratch/sd/j/jiuy97/4_V_slab/*_*_*/*d/*_*/'):
     try:
         if '1_Tetrahedral_WZ' in dir:
             bond_vector = atoms[oxygen_indices[-2]].position - atoms[metal_indices[-3]].position
+            atoms.cell[2] += bond_vector[2]
             oxygen_positions = [atoms[metal_indices[-1]].position + bond_vector]
             hydrogen_positions = [oxygen_positions[0] + (0.0, 1.0, 0.0)]
             add_atoms_and_save(atoms, oxygen_positions, hydrogen_positions, 'restart-o.json', 'restart-oh.json')
