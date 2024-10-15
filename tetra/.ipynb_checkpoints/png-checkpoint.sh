af4d5f994ca97ac637d843fc9ae6f41f94d26b57
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-14 21:43:37 -0700

    .

--- tetra/.ipynb_checkpoints/png-checkpoint.sh
+++ tetra/.ipynb_checkpoints/png-checkpoint.sh
@@ -105,8 +105,8 @@ elif [[ ${here} == 'nersc' ]]; then
 #         -o GP_L_M 3d/energy_GP_Loewdin_M.tsv 4d/energy_GP_Loewdin_M.tsv 5d/energy_GP_Loewdin_M.tsv
         python ~/bin/tools/tetra/tsv.py -l 3d 4d 5d -x "Metal (MO)" -y "Madelung energy (Loewdin, eV/MO)" \
         -o norm_Madelung_L 3d/energy_norm_Madelung_Loewdin.tsv 4d/energy_norm_Madelung_Loewdin.tsv 5d/energy_norm_Madelung_Loewdin.tsv
-        python ~/bin/tools/tetra/tsv.py -l 3d 4d 5d -x "Metal (MO)" -y "Formation energy (eV/MO)" \
-        -o norm_formation 3d/energy_norm_formation.tsv 4d/energy_norm_formation.tsv 5d/energy_norm_formation.tsv
+        # python ~/bin/tools/tetra/tsv.py -l 3d 4d 5d -x "Metal (MO)" -y "Formation energy (eV/MO)" \
+        # -o norm_formation 3d/energy_norm_formation.tsv 4d/energy_norm_formation.tsv 5d/energy_norm_formation.tsv
 #         python ~/bin/tools/tetra/tsv.py -l 3d 4d 5d -x "Metal (MO)" -y "Cohesive energy (eV/MO)" \
 #         -o norm_cohesive 3d/energy_norm_cohesive.tsv 4d/energy_norm_cohesive.tsv 5d/energy_norm_cohesive.tsv
         
