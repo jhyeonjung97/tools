4e4ef7c23aee8c9baac7dce3600d80e58be798ad
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-21 00:28:33 -0700

    .

--- mnc/add-co.py
+++ mnc/add-co.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 from ase import Atoms
 
-atoms = read('restart.json')
+atoms = read('final_with_calculator.json')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('C', positions=[atoms[-1].position + (-0.6, 0.0, 2.0)])
     atoms += Atoms('O', positions=[atoms[-2].position + (+0.6, 0.0, 2.0)])
--- mnc/add-ooh.py
+++ mnc/add-ooh.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 from ase import Atoms
 
-atoms = read('restart.json')
+atoms = read('final_with_calculator.json')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('O', positions=[atoms[-1].position + (0.2, -0.1, 2.2)])
     atoms += Atoms('O', positions=[atoms[-1].position + (-1.3, 0.1, 0.7)])
