febe8cc05b3b86d7757df1d1aab900f974ddca4f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-23 00:21:22 -0700

    .

--- mnc/most_stable.sh
+++ mnc/most_stable.sh
@@ -60,60 +60,60 @@ do
 done
 
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/
-do
-    cd $dir
-    IFS='/' read -r -a path <<< $PWD
-    ads=$(echo "${path[-2]}" | cut -d'_' -f2)
-    metal=$(echo "${path[-1]}" | cut -d'_' -f2)
-    echo -e "dz\tspin_state" > lowest.tsv
-    all_done=true
-    for sub_sub_dir in ./*_*S/*_/
-    do
-        if [[ ! -s ${sub_sub_dir}DONE ]]; then
-            # echo "DONE file is missing in $dir$sub_sub_dir/DONE"
-            all_done=false
-        fi
-    done
-    if [[ $all_done == true ]]; then
-        mkdir -p most_stable
-        cd most_stable
-        dzs=(0 1 2 3 4 5 6)
-        for dz in ${dzs[@]}
-        do
-            mkdir -p ${dz}_
-        done
-        cd $dir; pwd
-        for dz in ${dzs[@]}
-        do
-            lowest_dir=''
-            lowest_energy=0
-            for sub_dir in *_*S/
-            do
-                energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${sub_dir}${dz}_/DONE")
-                if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
-                    lowest_energy=$energy
-                    lowest_dir=${sub_dir}${dz}_/
-                    lowest_sub_dir=${sub_dir}
-                fi
-            done
-            if [[ -n "$lowest_dir" ]]; then
-                product=$(echo "scale=2; $dz * 0.2" | bc)
-                product=$(echo "$product" | awk '{printf "%.2f", $0}')
-                clean_dir=$(echo "$lowest_sub_dir" | sed 's:/$::')
-                spin=$(echo "$clean_dir" | cut -d'_' -f2)
-                echo -e "$product\t$spin" >> lowest.tsv
-                if [[ ! -f most_stable/6_/restart.json ]]; then
-                    echo "${lowest_dir}restart.json most_stable/${dz}_/"
-                    cp ${lowest_dir}restart.json most_stable/${dz}_/
-                    cp ${lowest_dir}WAVECAR most_stable/${dz}_/
-                    cp ~/bin/tools/mnc/submit.sh most_stable/${dz}_/
-                    sed -i -e "/#SBATCH -t/c\#SBATCH -t 00:30:00" most_stable/${dz}_/submit.sh
-                    sed -i -e "/#SBATCH -J/c\#SBATCH -J ${ads}${metal}MS${dz}" most_stable/${dz}_/submit.sh
-                fi
-            else
-                echo "No valid directory found for dz=${dz}"
-            fi
-        done
-    fi
-done
+# for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/
+# do
+#     cd $dir
+#     IFS='/' read -r -a path <<< $PWD
+#     ads=$(echo "${path[-2]}" | cut -d'_' -f2)
+#     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
+#     echo -e "dz\tspin_state" > lowest.tsv
+#     all_done=true
+#     for sub_sub_dir in ./*_*S/*_/
+#     do
+#         if [[ ! -s ${sub_sub_dir}DONE ]]; then
+#             # echo "DONE file is missing in $dir$sub_sub_dir/DONE"
+#             all_done=false
+#         fi
+#     done
+#     if [[ $all_done == true ]]; then
+#         mkdir -p most_stable
+#         cd most_stable
+#         dzs=(0 1 2 3 4 5 6)
+#         for dz in ${dzs[@]}
+#         do
+#             mkdir -p ${dz}_
+#         done
+#         cd $dir; pwd
+#         for dz in ${dzs[@]}
+#         do
+#             lowest_dir=''
+#             lowest_energy=0
+#             for sub_dir in *_*S/
+#             do
+#                 energy=$(grep -oP 'ENERGY\s+\K[-+]?[0-9]*\.?[0-9]+' "${sub_dir}${dz}_/DONE")
+#                 if [[ $(echo "$energy < $lowest_energy" | bc) -eq 1 ]]; then
+#                     lowest_energy=$energy
+#                     lowest_dir=${sub_dir}${dz}_/
+#                     lowest_sub_dir=${sub_dir}
+#                 fi
+#             done
+#             if [[ -n "$lowest_dir" ]]; then
+#                 product=$(echo "scale=2; $dz * 0.2" | bc)
+#                 product=$(echo "$product" | awk '{printf "%.2f", $0}')
+#                 clean_dir=$(echo "$lowest_sub_dir" | sed 's:/$::')
+#                 spin=$(echo "$clean_dir" | cut -d'_' -f2)
+#                 echo -e "$product\t$spin" >> lowest.tsv
+#                 if [[ ! -f most_stable/6_/restart.json ]]; then
+#                     echo "${lowest_dir}restart.json most_stable/${dz}_/"
+#                     cp ${lowest_dir}restart.json most_stable/${dz}_/
+#                     cp ${lowest_dir}WAVECAR most_stable/${dz}_/
+#                     cp ~/bin/tools/mnc/submit.sh most_stable/${dz}_/
+#                     sed -i -e "/#SBATCH -t/c\#SBATCH -t 00:30:00" most_stable/${dz}_/submit.sh
+#                     sed -i -e "/#SBATCH -J/c\#SBATCH -J ${ads}${metal}MS${dz}" most_stable/${dz}_/submit.sh
+#                 fi
+#             else
+#                 echo "No valid directory found for dz=${dz}"
+#             fi
+#         done
+#     fi
+# done
