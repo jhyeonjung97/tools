5a8d5efc694b59c2cfacd4eaefa232565ab0fe33
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-24 23:35:22 -0700

    .

--- tetra/lr.py
+++ tetra/lr.py
@@ -192,14 +192,14 @@ def main():
              ]
     light_colors_indices = [1, 2, 5, 6, 11, 12, 13, 14, 15, 16, 17, 18, 25, 26, 27, 29, 30]
     bold_indices = [i for i, color in enumerate(colors) if color != 'grey' and i not in light_colors_indices]
-    for i, (tick_label, color) in enumerate(zip(ax.get_yticklabels(), colors)):
-        tick_label.set_color(color)
-        if i in bold_indices:
-            tick_label.set_fontproperties(FontProperties(weight='bold'))
     ax.set_xticks(np.arange(M.shape[1]) + 0.5)
     ax.set_xticklabels(M.columns, rotation=90, ha='right', fontsize=6)
     ax.set_yticks(np.arange(M.shape[1]) + 0.5)
     ax.set_yticklabels(M.columns, rotation=0, va='center', fontsize=6)
+    for i, (tick_label, color) in enumerate(zip(ax.get_yticklabels(), colors)):
+        tick_label.set_color(color)
+        if i in bold_indices:
+            tick_label.set_fontproperties(FontProperties(weight='bold'))
     cbar = plt.colorbar(ax.collections[0], ax=ax, pad=0.02)
     cbar.ax.tick_params(labelsize=6)
     cbar.outline.set_visible(False)
