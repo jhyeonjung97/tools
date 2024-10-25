854b537c70268ec334834fb1e1bc640731adc0f1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-24 23:23:07 -0700

    .

--- tetra/.ipynb_checkpoints/lr-checkpoint.py
+++ tetra/.ipynb_checkpoints/lr-checkpoint.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from matplotlib.font_manager import FontProperties
 
 def main():
     parser = argparse.ArgumentParser(description='Perform linear regression using aggregated columns from multiple TSV files excluding the first column, calculate MAE, MSE, plot results, and save output.')
