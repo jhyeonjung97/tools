2cece2377efddf2fcf4dcc06f908e6bb93d64a0f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-28 11:24:29 -0700

    .

--- mnc/untitled.py
+++ mnc/untitled.py
@@ -2,8 +2,6 @@ from ase.io import read, write
 atoms=read('final_with_calculator.json')
 for atom in atoms:
     if atom.symbol not in ['N', 'C', 'O', 'H']:
-        dz = atom.z - 10.0
-        mag = magmoms[atom.index]
         if atom.index < len(atoms) - 1:
             next_atom = atoms[atom.index + 1]
             print(atom.symbol, atom.position)
