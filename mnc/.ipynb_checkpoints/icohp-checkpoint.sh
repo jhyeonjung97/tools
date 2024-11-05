c756a3f8adf68837501ea37c34b81f2fd1106210
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-05 14:46:44 -0800

    .

--- mnc/.ipynb_checkpoints/icohp-checkpoint.sh
+++ mnc/.ipynb_checkpoints/icohp-checkpoint.sh
@@ -5,17 +5,24 @@ do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     for ads in o oh
     do
-        if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
-            cd $ads
-            mkdir -p icohp
-            cp WAVECAR restart.json icohp/
-            cp ~/bin/tools/mnc/lobsterin icohp/
-            sed -i -e "s/X/${metal}/g" icohp/lobsterin
-            cp ~/bin/tools/mnc/static.sh icohp/submit.sh
-            sed -i -e "s/jobname/${metal}${ads}COHP/g" icohp/submit.sh
-            cd $dir
-        else
+        if [[ $metal == 'Ti' ]]; then
             continue
+        else      
+            cd $ads/icohp/
+            sbatch submit.sh
+            cd $dir
         fi
+        # if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
+        #     cd $ads
+        #     mkdir -p icohp
+        #     cp WAVECAR restart.json icohp/
+        #     cp ~/bin/tools/mnc/lobsterin icohp/
+        #     sed -i -e "s/X/${metal}/g" icohp/lobsterin
+        #     cp ~/bin/tools/mnc/static.sh icohp/submit.sh
+        #     sed -i -e "s/jobname/${metal}${ads}COHP/g" icohp/submit.sh
+        #     cd $dir
+        # else
+        #     continue
+        # fi
     done
 done
