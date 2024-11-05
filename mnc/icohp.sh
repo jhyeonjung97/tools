51f12b1d5a3019e348cbabbec3135a54fefe0bfe
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-05 14:47:29 -0800

    .

--- mnc/icohp.sh
+++ mnc/icohp.sh
@@ -5,24 +5,19 @@ do
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     for ads in o oh
     do
-        if [[ $metal == 'Ti' ]]; then
-            continue
-        else      
-            cd $ads/icohp/
+        if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
+            cd $ads
+            mkdir -p icohp
+            cp WAVECAR restart.json icohp/
+            cp ~/bin/tools/mnc/lobsterin icohp/
+            sed -i -e "s/X/${metal}/g" icohp/lobsterin
+            cp ~/bin/tools/mnc/static.sh icohp/submit.sh
+            sed -i -e "s/jobname/${metal}${ads}COHP/g" icohp/submit.sh
+            cd icohp/
             sbatch submit.sh
             cd $dir
+        else
+            continue
         fi
-        # if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
-        #     cd $ads
-        #     mkdir -p icohp
-        #     cp WAVECAR restart.json icohp/
-        #     cp ~/bin/tools/mnc/lobsterin icohp/
-        #     sed -i -e "s/X/${metal}/g" icohp/lobsterin
-        #     cp ~/bin/tools/mnc/static.sh icohp/submit.sh
-        #     sed -i -e "s/jobname/${metal}${ads}COHP/g" icohp/submit.sh
-        #     cd $dir
-        # else
-        #     continue
-        # fi
     done
 done
