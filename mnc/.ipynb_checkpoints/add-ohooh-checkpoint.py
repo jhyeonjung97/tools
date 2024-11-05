01a055c137f48067c55accfdfaeea8579577a137
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-05 14:16:12 -0800

    .

--- mnc/.ipynb_checkpoints/add-ohooh-checkpoint.py
+++ mnc/.ipynb_checkpoints/add-ohooh-checkpoint.py
@@ -6,6 +6,6 @@ if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('O', positions=[atoms[-1].position + (0.0, -1.2, 1.8)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.2, 0.8, 0.6)])
     atoms += Atoms('O', positions=[atoms[-3].position + (0.4, 1.2, 1.8)])
-    atoms += Atoms('O', positions=[atoms[-1].position + (-0.4, 0.0, 1.4)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (-0.6, 0.0, 1.3)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 1.0, 0.0)])
     write('restart-ohooh.json', atoms)
