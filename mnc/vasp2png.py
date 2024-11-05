01dfe352aaa5db86af2f436f099cb25496093e8b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-04 16:22:25 -0800

    .

--- mnc/vasp2png.py
+++ mnc/vasp2png.py
@@ -52,11 +52,11 @@ for root_dir in root_dirs:
                 print(f"No valid CONTCAR file found in {main_dir}")
                 continue  # Skip to the next iteration if invalid
 
-        vasp_output_file = os.path.join(root_dir, main_dir, f'{numb}{metal}{adsorbate}.vasp')
+        vasp_output_file = os.path.join('/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix', f'{numb}{metal}{adsorbate}.vasp')
         write(vasp_output_file, atoms)
         print(f"Saved as {vasp_output_file}")
 
-        png_file = os.path.join(root_dir, main_dir, f'{numb}{metal}{adsorbate}.png')
+        png_file = os.path.join('/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix', f'{numb}{metal}{adsorbate}.png')
         write(png_file, atoms, rotation='-90x, -90y, 0z', show_unit_cell=0)
 
         print(f"Saved as {png_file}")
