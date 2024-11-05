5a6254e37554c5107c6fac27acfb7abdd69c347d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-05 14:14:12 -0800

    .

--- mnc/.ipynb_checkpoints/add-ohooh-checkpoint.py
+++ mnc/.ipynb_checkpoints/add-ohooh-checkpoint.py
@@ -3,9 +3,9 @@ from ase import Atoms
 
 atoms = read('restart.json')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
-    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, -1.0, 2.0)])
-    atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 0.8, 0.6)])
-    atoms += Atoms('O', positions=[atoms[-3].position + (0.0, 1.0, 2.0)])
-    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, 1.4)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, -1.2, 1.8)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.2, 0.8, 0.6)])
+    atoms += Atoms('O', positions=[atoms[-3].position + (0.2, 1.2, 1.8)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (-0.2, 0.0, 1.4)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 1.0, 0.0)])
     write('restart-ohooh.json', atoms)
