735e496fe2f732ec60d54f7367b7ec08c8f72a0f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-05 14:20:51 -0800

    .

--- mnc/add-oohoh.py
+++ mnc/add-oohoh.py
@@ -3,8 +3,8 @@ from ase import Atoms
 
 atoms = read('restart.json')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
-    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, -1.0, 2.0)])
-    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, 1.4)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.4, -1.0, 2.0)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (-0.6, 0.4, 1.3)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 0.8, 0.6)])
     atoms += Atoms('O', positions=[atoms[-4].position + (0.0, 1.0, 2.0)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 1.0, 0.0)])
