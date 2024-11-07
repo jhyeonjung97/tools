bdd732a97fc22b2c13e982e552f37a7e065735db
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-07 12:35:49 -0800

    .

---
+++ mnc/.ipynb_checkpoints/add-ooh-ooh-checkpoint.py
@@ -0,0 +1,12 @@
+from ase.io import read, write
+from ase import Atoms
+
+atoms = read('restart.json')
+if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.1, 0.2, 2.2)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (-0.1, -1.3, 0.7)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.8, 0.1, 0.5)])
+    atoms += Atoms('O', positions=[atoms[-3].position + (0.0, 0.0, -2.0)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (-0.1, -1.3, -0.7)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.8, 0.1, -0.5)])
+    write('restart-ooh-ooh.json', atoms)
