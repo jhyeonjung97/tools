8e3ccc8a109795459a9c5f00df2075d90893d0ea
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-07 12:38:01 -0800

    .

--- mnc/.ipynb_checkpoints/pourbaix-checkpoint.sh
+++ mnc/.ipynb_checkpoints/pourbaix-checkpoint.sh
@@ -44,10 +44,11 @@ for row in "${ordered_rows[@]}"; do
                         if [ -d "$target_dir" ]; then
                             cd "$target_dir" || continue
 
-                            # Execute add*.py scripts
-                            for file in ~/bin/tools/mnc/add*.py; do
-                                python "$file"
-                            done
+                            # # Execute add*.py scripts
+                            # for file in ~/bin/tools/mnc/add*.py; do
+                            #     python "$file"
+                            # done
+                            python ~/bin/tools/mnc/add-ooh-ooh.py
                         else
                             echo "Target directory does not exist: $target_dir"
                             continue
