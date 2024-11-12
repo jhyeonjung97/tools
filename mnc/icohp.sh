4c227cc84d1361343d01f8ce369d8e23c0433fcf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-11 16:05:06 -0800

    .

--- mnc/icohp.sh
+++ mnc/icohp.sh
@@ -1,11 +1,12 @@
-for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*d/*_*/most_stable/
+for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/3d/*_*/*_*S/
 do
     cd $dir; pwd
     IFS='/' read -r -a path <<< "$dir"
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+    spin=$(echo "${path[-1]}" | cut -d'_' -f2)
     for ads in o oh
     do
-        if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
+        if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]] && [[ ! $spin == 'IS' ]]; then
             cd $ads
             mkdir -p icohp
             cp WAVECAR restart.json icohp/
