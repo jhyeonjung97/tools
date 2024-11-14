8f892f4c35538b7f88e5f2fbec5fc80ca57813db
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-14 15:30:35 -0800

    .

--- mnc/add-oh-oh.py
+++ mnc/add-oh-oh.py
@@ -7,4 +7,4 @@ if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 0.8, 0.6)])
     atoms += Atoms('O', positions=[atoms[-3].position + (0.0, 0.0, -2.0)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, -0.8, -0.6)])
-write('restart-oh-oh.json', atoms)
+    write('restart-oh-oh.json', atoms)
