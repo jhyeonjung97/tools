40b7ec017ebd68d722a0caf3a72600813284511a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-15 11:15:55 -0800

    .

---
+++ tetra/isif8.sh
@@ -0,0 +1,14 @@
+for dir in /pscratch/sd/j/jiuy97/3_V_bulk/*_*_*/*d/*_*
+do
+    IFS='/' read -r -a path <<< $dir
+    path1=${path[-1]}
+    path2=${path[-2]}
+    path3=${path[-3]}
+    numb=$(echo $path1 | cut -d'_' -f1 | cut -c1,2)
+    tag=$(echo $path1 | cut -d'_' -f1 | cut -c3)
+    metal=$(echo $path1 | cut -d'_' -f2)
+    coord=$(echo $path2 | cut -d'_' -f3)
+    
+    new_dir="/pscratch/sd/j/jiuy97/3_V_bulk/isif8/${path3}/${path2}/${numb}_${metal}"
+    ls $new_dir    
+done
