123440b4dd12d5cbd50f8917ac18c57a60198f99
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-18 18:52:45 -0800

    .

--- mnc/overpotential.py
+++ mnc/overpotential.py
@@ -177,15 +177,15 @@ def main():
             spin_cross_over.loc[index, 'O'] = energies['O']['spin'].loc[index]
             spin_cross_over.loc[index, 'OOH'] = energies['OOH']['spin'].loc[index]
         
-        # gibbs_energies.to_csv(f'/pscratch/sd/j/jiuy97/6_MNC/figures/{row}_{group}{metal}_gibbs.tsv', sep='\t', float_format='%.2f')
-        # spin_cross_over.to_csv(f'/pscratch/sd/j/jiuy97/6_MNC/figures/{row}_{group}{metal}_spin.tsv', sep='\t')
-        # print(f"Data saved to {row}_{group}{metal}_gibbs.tsv and {row}_{group}{metal}_spin.tsv")
+        gibbs_energies.to_csv(f'/pscratch/sd/j/jiuy97/6_MNC/figures/{row}_{group}{metal}_gibbs.tsv', sep='\t', float_format='%.2f')
+        spin_cross_over.to_csv(f'/pscratch/sd/j/jiuy97/6_MNC/figures/{row}_{group}{metal}_spin.tsv', sep='\t')
+        print(f"Data saved to {row}_{group}{metal}_gibbs.tsv and {row}_{group}{metal}_spin.tsv")
         
-        # plotting(gibbs_energies=gibbs_energies, spin_cross_over=spin_cross_over, row=row, group=group, metal=metal,
-        #          rxn='OER', rds='dGmax', overpotential=OER, ymin=0.2, ymax=1.4)
-        # plotting(gibbs_energies=gibbs_energies, spin_cross_over=spin_cross_over, row=row, group=group, metal=metal,
-        #          rxn='ORR', rds='dGmin', overpotential=ORR, ymin=0.2, ymax=1.4)
-        # print(f"Figures saved as {row}_{group}{metal}_OER.png and {row}_{group}{metal}_ORR.png")
+        plotting(gibbs_energies=gibbs_energies, spin_cross_over=spin_cross_over, row=row, group=group, metal=metal,
+                 rxn='OER', rds='dGmax', overpotential=OER, ymin=0.2, ymax=1.4)
+        plotting(gibbs_energies=gibbs_energies, spin_cross_over=spin_cross_over, row=row, group=group, metal=metal,
+                 rxn='ORR', rds='dGmin', overpotential=ORR, ymin=0.2, ymax=1.4)
+        print(f"Figures saved as {row}_{group}{metal}_OER.png and {row}_{group}{metal}_ORR.png")
     
     scaling_relationship['G_OH'] = scaling_relationship['G_OH'] + OH_corr
     scaling_relationship['G_O'] = scaling_relationship['G_O'] + O_corr
