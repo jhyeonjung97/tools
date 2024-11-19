f668e6cd9874743b1187834c268b85a4c19bc50a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-19 11:28:48 -0800

    .

--- mnc/vshape.py
+++ mnc/vshape.py
@@ -101,7 +101,7 @@ def dual_scatter_by_row(elems, x1, x2, y1, y2, label1, label2, xlabel, ylabel, p
     r2_1 = 1 - (ss_res1 / ss_tot1)
     plt.plot(x1_valid, p1(x1_valid), color='orange', linestyle="--")
     equation_text1 = f"y = {z1[0]:.2f}x + {z1[1]:.2f}\n$R^2$ = {r2_1:.2f}"
-    plt.text(0.7, 0.8, equation_text1, fontsize=10, color='orange', 
+    plt.text(0.75, 0.85, equation_text1, fontsize=10, color='orange', 
              ha='left', va='top', transform=plt.gca().transAxes)
     valid_mask2 = np.isfinite(x2) & np.isfinite(y2)
     x2_valid, y2_valid = x2[valid_mask2], y2[valid_mask2]
@@ -113,7 +113,7 @@ def dual_scatter_by_row(elems, x1, x2, y1, y2, label1, label2, xlabel, ylabel, p
     r2_2 = 1 - (ss_res2 / ss_tot2)
     plt.plot(x2_valid, p2(x2_valid), color='dodgerblue', linestyle="--")
     equation_text2 = f"y = {z2[0]:.2f}x + {z2[1]:.2f}\n$R^2$ = {r2_2:.2f}"
-    plt.text(0.7, 0.7, equation_text2, fontsize=10, color='dodgerblue', 
+    plt.text(0.75, 0.75, equation_text2, fontsize=10, color='dodgerblue', 
              ha='left', va='top', transform=plt.gca().transAxes)
     for xi, yi, elem in zip(x1_valid, y1_valid, elems):
         plt.annotate(f'{elem}', (float(xi), float(yi)), textcoords="offset points", xytext=(0, 5), ha='center', color='orange')
@@ -256,8 +256,8 @@ def dual_plot_by_ads(X, Y, xlabel, ylabel, pngname):
     
 def dual_scatter_by_ads(X, Y, xlabel, ylabel, pngname):
     plt.figure(figsize=(6.0, 4.5), dpi=300)
-    eq_x=[0.10, 0.30, 0.50, 0.50]
-    eq_y=[0.20, 0.50, 0.75, 0.85]
+    eq_x=[0.05, 0.05, 0.50, 0.50]
+    eq_y=[0.15, 0.25, 0.85, 0.95]
     for j, row in enumerate(elements.keys()):
         xx = np.array(X[row][adsorbate])
         yy = np.array(Y[row][adsorbate])
