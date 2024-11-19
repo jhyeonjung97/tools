854090076b76f02834b3b8150a4b1ba71de52c5c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-18 18:17:48 -0800

    .

--- mnc/scaling.py
+++ mnc/scaling.py
@@ -17,17 +17,16 @@ rows = {
     '4d': ['Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd'],
     '5d': ['Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt']
 }
-adsorbates = ['clean', 'oh', 'o', 'ooh']
+dirs = ['relaxed', 'oh', 'o', 'ooh']
+adsorbates = ['clean', 'OH', 'O', 'OOH']
 df = pd.DataFrame()
 
 def main():
     for row_key, metals in rows.items():
         for m, metal in enumerate(metals):
-            for ads in adsorbates:
-                if ads == 'clean':
-                    path = f'/pscratch/sd/j/jiuy97/6_MNC/0_clean/{row_key}/{m+2}_{metal}/most_stable/relaxed'
-                else:
-                    path = f'/pscratch/sd/j/jiuy97/6_MNC/0_clean/{row_key}/{m+2}_{metal}/most_stable/{ads}'
+            for d, dir in enumerate(dirs):
+                ads = adsorbates[d]
+                path = f'/pscratch/sd/j/jiuy97/6_MNC/0_clean/{row_key}/{m+2}_{metal}/most_stable/{dir}'
                 path_DONE = os.path.join(path, 'DONE')
                 path_json = os.path.join(path, 'final_with_calculator.json')
                 if os.path.exists(path_DONE) and os.path.exists(path_json):
