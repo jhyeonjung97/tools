7e3246ba3f28c86fcb41432ef584d2fe2a1b5e4d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-02 23:37:18 -0800

    .

--- mnc/dos.sh
+++ mnc/dos.sh
@@ -7,11 +7,9 @@ do
     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
-    if [[ $metal == 'Ti' ]] && [[ $spin == 'HS' ]] && [[ $dz == '2' ]]; then
-        sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
-        cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
-        sed -i -e "s/XX/$metal/g" sumo.sh
-        sh sumo.sh
-        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${i}${metal}_${spin}${dz}.png
-    fi
+    sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
+    cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
+    sed -i -e "s/XX/$metal/g" sumo.sh
+    sh sumo.sh
+    python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${i}${metal}_${spin}${dz}.png
 done
