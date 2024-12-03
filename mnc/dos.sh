5bb9c42fd0a04fd5a878b35ba0eb7b56f5663b93
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-03 14:23:35 -0800

    .

--- mnc/dos.sh
+++ mnc/dos.sh
@@ -7,20 +7,22 @@ do
     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
-    if [[ $spin == 'stable' ]]; then
-        spin='MS'
-    fi
-    if [[ $dz == 'relaxed' ]]; then
-        dz='r'
-    fi
-    if [[ ! -s sumo.sh ]]; then
-        sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
-        cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
-        sed -i -e "s/XX/$metal/g" sumo.sh
-    fi
-    if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
-        sh sumo.sh
-        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}.png
+    if [[ ($metal == 'Mn' || $metal == 'Fe' || $metal == 'Co' || $metal == 'Ni') && ($dz == 0 || $dz == 5) ]]; then
+        if [[ $spin == 'stable' ]]; then
+            spin='MS'
+        fi
+        if [[ $dz == 'relaxed' ]]; then
+            dz='r'
+        fi
+        if [[ ! -s sumo.sh ]]; then
+            sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
+            cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
+            sed -i -e "s/XX/$metal/g" sumo.sh
+        fi
+        if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
+            sh sumo.sh
+            python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}.png
+        fi
     fi
 done
 
