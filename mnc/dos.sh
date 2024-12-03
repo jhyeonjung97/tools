9510abb2ea971c9faea2172fd7ba0861d9e009a7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-03 12:16:42 -0800

    .

--- mnc/dos.sh
+++ mnc/dos.sh
@@ -1,6 +1,6 @@
 for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*/*_*/*_*/*
 do
-    cd $dir
+    cd $dir; pwd
     IFS='/' read -r -a path <<< $dir
     row=${path[-4]}
     m=$(echo "${path[-3]}" | cut -d'_' -f1)
@@ -13,7 +13,7 @@ do
     if [[ $dz == 'relaxed' ]]; then
         dz='r'
     fi
-    if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/pdos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
+    if [[ -s DONE ]] && [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/pdos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
         sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
         cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
         sed -i -e "s/XX/$metal/g" sumo.sh
@@ -24,7 +24,7 @@ done
 
 for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/*_*/*
 do
-    cd $dir
+    cd $dir; pwd
     IFS='/' read -r -a path <<< $dir
     ads=$(echo "${path[-4]}" | cut -d'_' -f2)
     m=$(echo "${path[-3]}" | cut -d'_' -f1)
@@ -37,7 +37,7 @@ do
     if [[ $dz == 'relaxed' ]]; then
         dz='r'
     fi
-    if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/pdos/pdos_${ads}_${m}${metal}_${spin}${dz}.png" ]]; then
+    if [[ -s DONE ]] && [ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/pdos/pdos_${ads}_${m}${metal}_${spin}${dz}.png" ]]; then
         sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
         cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
         sed -i -e "s/XX/$metal/g" sumo.sh
