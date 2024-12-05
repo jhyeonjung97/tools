a8b80a81b4cd43623b4755f102ac00dd3e87f236
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-05 11:07:02 -0800

    .

--- mnc/.ipynb_checkpoints/dos-checkpoint.sh
+++ mnc/.ipynb_checkpoints/dos-checkpoint.sh
@@ -8,25 +8,25 @@ do
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
     # if [[ ($metal == 'Mn' || $metal == 'Fe' || $metal == 'Co' || $metal == 'Ni') && ($spin == 'IS' || $spin == 'LS') && ($dz == 0 || $dz == 5) ]]; then
-    if [[ $metal == 'Fe' && $spin == 'IS' ]]; then
-        if [[ $spin == 'stable' ]]; then
-            spin='MS'
-        fi
-        if [[ $dz == 'relaxed' ]]; then
-            dz='r'
-        fi
-        if [[ ! -s sumo.sh ]]; then
-            sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
-            cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
-            sed -i -e "s/XX/$metal/g" sumo.sh
-        fi
-        if [[ ! -s sumo_${metal}_dos.dat ]]; then
-            sh sumo.sh
-        fi
-        # if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
-        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}.png
-        # fi
+    # if [[ $metal == 'Fe' && $spin == 'IS' ]]; then
+    if [[ $spin == 'stable' ]]; then
+        spin='MS'
     fi
+    if [[ $dz == 'relaxed' ]]; then
+        dz='r'
+    fi
+    if [[ ! -s sumo.sh ]]; then
+        sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
+        cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
+        sed -i -e "s/XX/$metal/g" sumo.sh
+    fi
+    if [[ ! -s sumo_${metal}_dos.dat ]]; then
+        sh sumo.sh
+    fi
+    if [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
+        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}.png
+    fi
+    # fi
 done
 
 for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/*_*/*
