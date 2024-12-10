0b4b94d5cea31d58c8212572c18c61fb78d8afcf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-10 13:25:47 -0800

    .

--- mnc/scaling.py
+++ mnc/scaling.py
@@ -16,6 +16,7 @@ rows = {
     '4d': ['Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd'],
     '5d': ['Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt']
 }
+specific_metals = ['Mn', 'Fe', 'Co', 'Ni', 'Mo', 'W']
 colors = {'3d': 'blue', '4d': 'green', '5d': 'red'}
 dirs = ['relaxed', 'oh', 'o', 'ooh']
 adsorbates = ['clean', 'OH', 'O', 'OOH']
@@ -23,12 +24,12 @@ adsorbates = ['clean', 'OH', 'O', 'OOH']
 def main():
     df = pd.DataFrame()
     for row_key, metals in rows.items():
-        for m, metal in enumerate(metals):
+        for m, metal in enumerate(metals, start=2):
             for d, dir in enumerate(dirs):
                 ads = adsorbates[d]
                 df.at[metal, 'row'] = row_key
                 df.at[metal, 'color'] = colors[row_key]
-                path = f'/pscratch/sd/j/jiuy97/6_MNC/0_clean/{row_key}/{m+2}_{metal}/most_stable/{dir}'
+                path = f'/pscratch/sd/j/jiuy97/6_MNC/0_clean/{row_key}/{m}_{metal}/most_stable/{dir}'
                 path_DONE = os.path.join(path, 'DONE')
                 path_json = os.path.join(path, 'final_with_calculator.json')
                 if os.path.exists(path_DONE) and os.path.exists(path_json):
@@ -37,7 +38,17 @@ def main():
                     df.at[metal, ads] = energy
                 else:
                     df.at[metal, ads] = np.nan
-
+    for m, metal in enumerate(specific_metals, start=1):
+        for i, ads in enumerate(['O', 'OH', 'OOH'], start=1):
+            path = f'/pscratch/sd/j/jiuy97/6_MNC/{i}_{ads}/{m}_{metal}/most_stable/relaxed'
+            path_DONE = os.path.join(path, 'DONE')
+            path_json = os.path.join(path, 'final_with_calculator.json')
+            if os.path.exists(path_DONE) and os.path.exists(path_json):
+                atoms = read(path_json)
+                energy = atoms.get_total_energy()
+                if df.loc[metal, ads] < energy:
+                    df.at[metal, ads] = energy
+                
     df['G_'] = df['clean']
     df['G_OH'] = df['OH'] + OH_corr
     df['G_O'] = df['O'] + O_corr
@@ -83,7 +94,7 @@ def scaling(dG1, dG2, ads1, ads2, df): #, xmin, xmax, ymin, ymax):
     # plt.xlim(xmin, xmax)
     # plt.ylim(ymin, ymax)
     plt.tight_layout()
-    plt.savefig(f'/pscratch/sd/j/jiuy97/6_MNC/figures/scaling_relationship_full_{ads1}_{ads2}.png')
+    plt.savefig(f'/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix/scaling_relationship_full_{ads1}_{ads2}.png')
     print(f"Figure saved as scaling_relationship_full_{ads1}_{ads2}.png")
     plt.close()
     
