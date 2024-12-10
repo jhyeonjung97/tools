76c4d72cc36529b9b1b68bd9686281f573ebee19
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-09 21:39:28 -0800

    .

--- mnc/.ipynb_checkpoints/formation-energy-checkpoint.py
+++ mnc/.ipynb_checkpoints/formation-energy-checkpoint.py
@@ -1108,8 +1108,8 @@ def plotting(df, df_relaxed, dzs, spins, ylabel, png_filename, ymin=None, ymax=N
         return    
     plt.figure(figsize=(4, 3), dpi=300)
     df_smooth_y = pd.DataFrame()
-    start_points = {}
-    end_points = {}
+    start_points = []
+    end_points = []
     for column in df.columns:
         filtered_df = df[column].dropna()
         if not filtered_df.empty:
