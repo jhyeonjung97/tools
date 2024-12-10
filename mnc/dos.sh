92129384940173f37afcac1c8c69253c714acaae
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-10 14:06:47 -0800

    .

--- mnc/dos.sh
+++ mnc/dos.sh
@@ -7,13 +7,14 @@ do
     metal=$(echo "${path[-3]}" | cut -d'_' -f2)
     spin=$(echo "${path[-2]}" | cut -d'_' -f2)
     dz=$(echo "${path[-1]}" | cut -d'_' -f1)
+    # if [[ ($metal == 'Mn' || $metal == 'Fe' || $metal == 'Co' || $metal == 'Ni') && ($spin == 'IS' || $spin == 'LS') && ($dz == 0 || $dz == 5) ]]; then
+    # if [[ $metal == 'Fe' && $spin == 'IS' ]]; then
     if [[ $spin == 'stable' ]]; then
         spin='MS'
     fi
     if [[ $dz == 'relaxed' ]]; then
         dz='r'
     fi
-    if [[ ($metal == 'Mn' || $metal == 'Fe' || $metal == 'Co' || $metal == 'Ni') && ($spin == 'MS') && ($dz == 0 || $dz == 6) ]]; then
     # if [[ ! -s sumo.sh ]]; then
     #     sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
     #     cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
@@ -22,9 +23,10 @@ do
     # if [[ ! -s sumo_${metal}_dos.dat ]]; then
     #     sh sumo.sh
     # fi
-    # if [[ -s sumo_${metal}_dos.dat ]] && [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
-        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}
+    if [[ -s sumo_${metal}_dos.dat ]] && [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${row}_${m}${metal}_${spin}${dz}.png" ]]; then
+        python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}.png
     fi
+    # fi
 done
 
 for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/*_*/*
@@ -42,7 +44,6 @@ do
     if [[ $dz == 'relaxed' ]]; then
         dz='r'
     fi
-    if [[ ($metal == 'Mn' || $metal == 'Fe' || $metal == 'Co' || $metal == 'Ni') && ($spin == 'MS') && ($dz == 0 || $dz == 6) ]]; then
     # if [[ ! -s sumo.sh ]]; then
     #     sed -i 's/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/0.0/g' vasprun.xml
     #     cp /pscratch/sd/j/jiuy97/6_MNC/scripts/sumo.sh .
@@ -51,7 +52,7 @@ do
     # if [[ ! -s sumo_${metal}_dos.dat ]]; then
     #     sh sumo.sh
     # fi
-    # if [[ -s sumo_${metal}_dos.dat ]] && [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${ads}_${m}${metal}_${spin}${dz}.png" ]]; then
+    if [[ -s sumo_${metal}_dos.dat ]] && [[ ! -s "/pscratch/sd/j/jiuy97/6_MNC/figures/dos/pdos_${ads}_${m}${metal}_${spin}${dz}.png" ]]; then
         python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${ads}_${m}${metal}_${spin}${dz}.png
     fi
 done
