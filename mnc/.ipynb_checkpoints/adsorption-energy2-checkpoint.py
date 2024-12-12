1f986db6054698476b0b3694856b2abca8508b8c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-11 23:21:27 -0800

    .

--- mnc/.ipynb_checkpoints/adsorption-energy2-checkpoint.py
+++ mnc/.ipynb_checkpoints/adsorption-energy2-checkpoint.py
@@ -7,13 +7,46 @@ import pandas as pd
 from scipy.interpolate import make_interp_spline
 import numpy as np
 
-rows = ['3d', '3d', '3d', '3d', '4d', '5d']
-numbs = [5, 6, 7, 8, 4, 4]
-metals = ['Mn', 'Fe', 'Co', 'Ni', 'Mo', 'W']
-adsorbates = ['clean', 'O', 'OH']
-save_path = '/pscratch/sd/j/jiuy97/6_MNC/figures/adsorption_energy'
-
 def main():
+    rows = ['3d', '3d', '3d', '3d', '4d', '5d']
+    numbs = [5, 6, 7, 8, 4, 4]
+    metals = ['Mn', 'Fe', 'Co', 'Ni', 'Mo', 'W']
+    adsorbates = ['clean', 'O', 'OH']
+    save_path = '/pscratch/sd/j/jiuy97/6_MNC/figures/adsorption_energy'
+
+    # Water and hydrogen properties
+    water_E = -14.23797429
+    water_Cv = 0.103
+    water_TS = 0.675
+    water_ZPE = 0.558
+    water_G = water_E + water_Cv - water_TS + water_ZPE
+
+    hydrogen2_E = -6.77412273
+    hydrogen2_Cv = 0.0905
+    hydrogen2_TS = 0.408
+    hydrogen2_ZPE = 0.273
+    hydrogen2_G = hydrogen2_E + hydrogen2_Cv - hydrogen2_TS + hydrogen2_ZPE
+    hydrogen_G = hydrogen2_G / 2
+
+    oxygen_G = water_G - hydrogen2_G
+    hydroxide_G = water_G - hydrogen_G
+
+    # Correction values
+    OH_Cv = 0.042
+    OH_TS = 0.066
+    OH_ZPE = 0.376
+    OH_corr = OH_Cv - OH_TS + OH_ZPE
+
+    O_Cv = 0.034
+    O_TS = 0.060
+    O_ZPE = 0.064
+    O_corr = O_Cv - O_TS + O_ZPE
+
+    OOH_Cv = 0.077
+    OOH_TS = 0.134
+    OOH_ZPE = 0.471
+    OOH_corr = OOH_Cv - OOH_TS + OOH_ZPE
+    
     for m, metal in enumerate(metals):
         row = rows[m]
         numb = numbs[m]
@@ -48,38 +81,4 @@ def main():
         plt.close()        
         
 if __name__ == '__main__':
-    
-    # Water and hydrogen properties
-    water_E = -14.23797429
-    water_Cv = 0.103
-    water_TS = 0.675
-    water_ZPE = 0.558
-    water_G = water_E + water_Cv - water_TS + water_ZPE
-
-    hydrogen2_E = -6.77412273
-    hydrogen2_Cv = 0.0905
-    hydrogen2_TS = 0.408
-    hydrogen2_ZPE = 0.273
-    hydrogen2_G = hydrogen2_E + hydrogen2_Cv - hydrogen2_TS + hydrogen2_ZPE
-    hydrogen_G = hydrogen2_G / 2
-
-    oxygen_G = water_G - hydrogen2_G
-    hydroxide_G = water_G - hydrogen_G
-
-    # Correction values
-    OH_Cv = 0.042
-    OH_TS = 0.066
-    OH_ZPE = 0.376
-    OH_corr = OH_Cv - OH_TS + OH_ZPE
-
-    O_Cv = 0.034
-    O_TS = 0.060
-    O_ZPE = 0.064
-    O_corr = O_Cv - O_TS + O_ZPE
-
-    OOH_Cv = 0.077
-    OOH_TS = 0.134
-    OOH_ZPE = 0.471
-    OOH_corr = OOH_Cv - OOH_TS + OOH_ZPE
-    
     main()
