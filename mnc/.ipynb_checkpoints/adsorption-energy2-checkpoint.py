2df96e931fb28346200dbdcdc404d6cba162baa1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-12 01:15:27 -0800

    .

--- mnc/.ipynb_checkpoints/adsorption-energy2-checkpoint.py
+++ mnc/.ipynb_checkpoints/adsorption-energy2-checkpoint.py
@@ -13,8 +13,8 @@ def main():
     numbs = [5, 6, 7, 8, 4, 4]
     metals = ['Mn', 'Fe', 'Co', 'Ni', 'Mo', 'W']
     adsorbates = ['clean', 'O', 'OH', 'OOH']
-    ymin = [-1, -1, -1, -1, -7, -7]
-    ymax = [5, 5, 5, 5, 0, 0]
+    ymin = [-1, -1, -1, -1, -4, -4]
+    ymax = [5, 5, 5, 5, 3, 3]
     colors = {'LS': '#ff7f0e', 'IS': '#279ff2', 'HS': '#9467bd'}
     save_path = '/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix'
     
@@ -89,7 +89,7 @@ def main():
         df_energy.to_csv(tsv_filename, sep='\t', float_format='%.2f')
         print(f"Data saved as adsorption_energy_{m+1}{metal}.tsv")
         
-        fig, ax = plt.subplots(figsize=(4, 6), dpi=300)
+        fig, ax = plt.subplots(figsize=(4, 4), dpi=300)
         for ads in ['OH', 'O', 'OOH']:
             if row != '3d' and ads == 'OOH':
                 continue
