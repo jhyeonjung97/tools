040f9fbef80b1905afdb81d42fca6b10c8855199
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-05 02:18:33 -0800

    .

--- mnc/.ipynb_checkpoints/icohp2-checkpoint.sh
+++ mnc/.ipynb_checkpoints/icohp2-checkpoint.sh
@@ -1,26 +1,11 @@
-for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/3d/*_*/*_*S/
+for dir in /pscratch/sd/j/jiuy97/6_MNC/icohp/*_*/*_/
 do
     cd $dir; pwd
     IFS='/' read -r -a path <<< "$dir"
-    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
-    spin=$(echo "${path[-1]}" | cut -d'_' -f2)
-    [[ $spin == 'IS' ]] && continue
-
-    for ads in o oh
-    do
-        if [[ -f $ads/DONE ]] && [[ ! -d $ads/icohp ]]; then
-            cd $ads
-            mkdir -p icohp
-            cp WAVECAR restart.json icohp/
-            cp ~/bin/tools/mnc/lobsterin icohp/
-            sed -i -e "s/X/${metal}/g" icohp/lobsterin
-            cp ~/bin/tools/mnc/static.sh icohp/submit.sh
-            sed -i -e "s/jobname/${metal}${ads}COHP/g" icohp/submit.sh
-            cd icohp/
-            sbatch submit.sh
-            cd $dir
-        else
-            continue
-        fi
-    done
+    ads=$(echo "${path[-2]}" | cut -d'_' -f2)
+    dz=$(echo "${path[-1]}" | cut -d'_' -f1)
+    cp ~/bin/tools/mnc/lobsterin2 ./lobsterin    
+    cp ~/bin/tools/mnc/static.sh ./submit.sh
+    sed -i -e "s/jobname/ICOHP_${ads}${dz}/" icohp/submit.sh
+    sbatch submit.sh
 done
