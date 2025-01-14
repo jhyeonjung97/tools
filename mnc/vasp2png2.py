764aa296e127d3de5749c47cbd53f89953e28b93
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-13 16:27:31 -0800

    .

---
+++ mnc/vasp2png2.py
@@ -0,0 +1,14 @@
+import os
+import glob
+import re
+from ase.io import read, write
+
+for i in range(8):
+    contcar_path=f'/pscratch/sd/j/jiuy97/6_MNC/empty/{i}_/CONTCAR/'
+    atoms = read(contcar_path)
+    vasp_output_file = f'/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix/vac{i}.vasp'
+    write(vasp_output_file, atoms)
+    print(f"Saved as vac{i}.vasp")
+    png_file = f'/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix/vac{i}.png'
+    write(png_file, atoms, rotation='-90x, -90y, 0z', show_unit_cell=0)
+    print(f"Saved as vac{i}.png")
