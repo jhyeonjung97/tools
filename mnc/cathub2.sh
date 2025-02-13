983fca0636007938029cfea5d10e819033f879e1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-13 00:23:53 -0800

    .

--- mnc/cathub2.sh
+++ mnc/cathub2.sh
@@ -2,7 +2,7 @@
 
 mnc="/pscratch/sd/j/jiuy97/6_MNC"
 vasp_pbe="VASP-6.3.2/PBE+U+D3+VASPsol"
-cathub="${mnc}/JungTuning2025/${vasp_pbe}"
+cathub="${mnc}/JungTuning2025_dz/${vasp_pbe}"
 # gas="/global/cfs/cdirs/m2997/Delowar/OER/MOF/data_storage_MOF/gas"
 cd "${cathub_path}" || exit 1
 
@@ -10,10 +10,10 @@ site1='M'
 site2='N'
 site3='D'
 
-cp -r "${mnc}/gas" "${cathub}"
 mkdir -p "${cathub}/N4C26/001-${site2}"
 cp "${mnc}/empty/0_/final_with_calculator.json" "${cathub}/N4C26/empty_slab.json"
 cp "${mnc}/empty/2_/final_with_calculator.json" "${cathub}/N4C26/001-${site2}"
+cp -r "${mnc}/gas" "${cathub}"
 
 for dir in ${mnc}/0_clean/*d/*_*/most_stable; do
     IFS='/' read -r -a path <<< "$dir"
@@ -165,7 +165,7 @@ find "${cathub}" -type d -empty -delete
 tree "${cathub}"
 
 cd "${mnc}" || exit 1
-cp ~/bin/tools/mnc/publication.txt JungTuning2025
-cathub folder2db JungTuning2025
+cp ~/bin/tools/mnc/publication.txt JungTuning2025_dz
+cathub folder2db JungTuning2025_dz
 
 
