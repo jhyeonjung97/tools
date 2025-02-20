9584bc3490eda498c25d805ccd4db74ec75a54d1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-20 11:09:12 -0800

    .

--- mnc/michal_toc.py
+++ mnc/michal_toc.py
@@ -147,15 +147,9 @@ for row_num, row in enumerate(df.itertuples(), 1):
                    edgecolor='black')
 ax.scatter([], [], label='relaxed Δz', s=24, marker='X', linewidths=0.5, facecolor='black', edgecolor='black')
 ax.scatter([], [], label='fixed Δz', s=24, marker='o', linewidths=0.5, facecolor='black', edgecolor='black')
-
-
+ax.set_xticks([])
+ax.set_yticks([])
 ax.plot(x, a * x + b, '--', lw=1, dashes=(3, 1), c='black')
-ax.text(1.4, 2.7, r'$\Delta$G$_{\sf OOH}$=', color=(0.8,0.8,0.8), fontsize=10)
-ax.text(1.4, 2.5, rf'{a}*$\Delta$G$_{{\sf OH}}$', color=(0.8,0.8,0.8), fontsize=10)
-ax.text(1.4, 2.3, rf'+{b} eV', color=(0.8,0.8,0.8), fontsize=10)
-ax.legend(bbox_to_anchor=(0.5, 1.1), loc='center', borderaxespad=0.5,
-          ncol=3, columnspacing=1.0, handletextpad=0.4,
-          fancybox=True, shadow=False, fontsize='small', handlelength=2)
 fig.savefig(os.path.join(save_path, 'contour_toc.png'), bbox_inches='tight')
 print("Figure saved as contour_toc.png")
 fig.clf()
