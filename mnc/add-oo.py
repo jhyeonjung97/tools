f5c87a0bcc3d7351b1e0eec8f4fc0d8573e3ab91
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-21 11:27:48 -0800

    .

--- mnc/add-oo.py
+++ mnc/add-oo.py
@@ -4,5 +4,5 @@ from ase import Atoms
 atoms = read('CONTCAR')
 if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, -2.0)])
-    atoms += Atoms('O', positions=[atoms[-2].position + (0.0, 1.0, -1.0)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 1.0, -1.0)])
     write('start-oo.traj', atoms)
