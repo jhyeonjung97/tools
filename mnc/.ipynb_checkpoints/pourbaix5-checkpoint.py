92dc76beb01b4899fec522a411e67146467e471b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-21 15:57:47 -0800

    .

--- mnc/.ipynb_checkpoints/pourbaix5-checkpoint.py
+++ mnc/.ipynb_checkpoints/pourbaix5-checkpoint.py
@@ -508,7 +508,8 @@ for dir in dirs:
     # df['E'] = df['E'].round(2)
     # df['G'] = df['G'].round(2)
     # df['dG'] = df['dG'].round(2)
-    df.to_csv(f'{figure_path}/{A}{B}_energies.tsv', sep=',') #, index=False)
+    df.to_csv(f'{figure_path}/{A}{B}_energies.csv', sep=',') #, index=False)
+    df.to_csv(f'{figure_path}/{A}{B}_energies.tsv', sep='\t', float_format='%.2f'))
     print(f"Data saved as {A}{B}_energies.png")
 
     OER_df = pd.DataFrame(OER)
@@ -518,7 +519,8 @@ for dir in dirs:
     # OER_df['dg41'] = OER_df['dg41'].round(2)
     # OER_df['overP'] = OER_df['overP'].round(2)
     # OER_df['onsetP'] = OER_df['onsetP'].round(2)
-    OER_df.to_csv(f'{figure_path}/{A}{B}_oer.tsv', sep=',') #, index=False)
+    OER_df.to_csv(f'{figure_path}/{A}{B}_oer.csv', sep=',') #, index=False)
+    OER_df.to_csv(f'{figure_path}/{A}{B}_oer.tsv', sep='\t', float_format='%.2f'))
     print(f"Data saved as {A}{B}_oer.png")
 
     ORR_df = pd.DataFrame(ORR)
@@ -528,6 +530,7 @@ for dir in dirs:
     # ORR_df['dg41'] = ORR_df['dg41'].round(2)
     # ORR_df['overP'] = ORR_df['overP'].round(2)
     # ORR_df['onsetP'] = ORR_df['onsetP'].round(2)
-    ORR_df.to_csv(f'{figure_path}/{A}{B}_orr.tsv', sep=',') #, index=False)
+    ORR_df.to_csv(f'{figure_path}/{A}{B}_orr.csv', sep=',') #, index=False)
+    ORR_df.to_csv(f'{figure_path}/{A}{B}_orr.tsv', sep='\t', float_format='%.2f'))
     print(f"Data saved as {A}{B}_orr.png")
     
