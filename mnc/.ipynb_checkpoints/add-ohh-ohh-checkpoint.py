d1a632ab1cdf8b6a6b54af97692d42c86392debe
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:05:29 -0800

    .

---
+++ mnc/.ipynb_checkpoints/add-ohh-ohh-checkpoint.py
@@ -0,0 +1,12 @@
+from ase.io import read, write
+from ase import Atoms
+
+atoms = read('CONTCAR')
+if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, -2.0)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.0, 0.8, -0.6)])
+    atoms += Atoms('H', positions=[atoms[-2].position + (0.8, -0.6, 0.0)])
+    atoms += Atoms('O', positions=[atoms[-4].position + (0.0, 0.0, 2.0)])
+    atoms += Atoms('H', positions=[atoms[-1].position + (0.0, -0.8, 0.6)])
+    atoms += Atoms('H', positions=[atoms[-2].position + (0.8, 0.6, 0.0)])
+    write('start-oo-ohh.traj', atoms)
