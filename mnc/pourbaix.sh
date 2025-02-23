689c3d5e4105a9cfc20ec0d2b6d2ac7eaac8d316
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:43:07 -0800

    .

--- mnc/pourbaix.sh
+++ mnc/pourbaix.sh
@@ -18,6 +18,18 @@ python ~/bin/tools/mnc/add-ohh-ooh.py
 pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/1_Fe'   
 for ads in oo oo-oo oo-ohh oo-oh oo-o oo-ooh ohh ohh-ohh ohh-oo ohh-oh ohh-o ohh-ooh
 do
+    if [[ ${ads} == oo ]] || [[ ${ads} == oo-oo ]] || [[ ${ads} == ohh ]] || [[ ${ads} == ohh-ohh ]]; then
+        oxi=2
+    elif [[ ${ads} == oo-ohh ]] || [[ ${ads} == ohh-oo ]]; then
+        oxi=2
+    elif [[ ${ads} == oo-oh ]] || [[ ${ads} == oo-ooh ]] || [[ ${ads} == ohh-oh ]] || [[ ${ads} == ohh-ooh ]]; then
+        oxi=3
+    elif [[ ${ads} == oo-o ]] || [[ ${ads} == ohh-o ]]; then
+        oxi=4
+    else
+        echo "Warning: spin for ${ads} is not defined"
+        exit
+    fi
     echo "${ads}"
     for spin in LS IS HS
     do
@@ -26,22 +38,10 @@ do
         cd ${dest_dir}
         cp "${dir}/start-${ads}.traj" ./start.traj
         cp ~/bin/tools/mnc/submit.sh .
-        if [[ ${ads} == oo ]] || [[ ${ads} == oo-oo ]] || [[ ${ads} == ohh ]] || [[ ${ads} == ohh-ohh ]]; then
-            oxi=2
-        elif [[ ${ads} == oo-ohh ]] || [[ ${ads} == ohh-oo ]]; then
-            oxi=2
-        elif [[ ${ads} == oo-oh ]] || [[ ${ads} == oo-ooh ]] || [[ ${ads} == ohh-oh ]] || [[ ${ads} == ohh-ooh ]]; then
-            oxi=3
-        elif [[ ${ads} == oo-o ]] || [[ ${ads} == ohh-o ]]; then
-            oxi=4
-        else
-            echo "Warning: spin for ${ads} is not defined"
-            exit
-        fi
         sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown${oxi}.py/g" submit.sh
         sed -i -e "s/jobname/Fe-${ads}-${spin}/g" submit.sh
-        # sbatch submit.sh
-        python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown${oxi}.py"
+        sbatch submit.sh
+        # python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown${oxi}.py"
     done
 done
 
@@ -63,29 +63,32 @@ python ~/bin/tools/mnc/add-ohh-ooh.py
 pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/2_Co'   
 for ads in oo oo-oo oo-ohh oo-oh oo-o oo-ooh ohh ohh-ohh ohh-oo ohh-oh ohh-o ohh-ooh
 do
+    if [[ ${ads} == oo ]] || [[ ${ads} == oo-oo ]] || [[ ${ads} == ohh ]] || [[ ${ads} == ohh-ohh ]]; then
+        oxi=2
+    elif [[ ${ads} == oo-ohh ]] || [[ ${ads} == ohh-oo ]]; then
+        oxi=2
+    elif [[ ${ads} == oo-oh ]] || [[ ${ads} == oo-ooh ]] || [[ ${ads} == ohh-oh ]] || [[ ${ads} == ohh-ooh ]]; then
+        oxi=3
+    elif [[ ${ads} == oo-o ]] || [[ ${ads} == ohh-o ]]; then
+        oxi=4
+    else
+        echo "Warning: spin for ${ads} is not defined"
+        exit
+    fi
     echo "${ads}"
     for spin in LS IS HS
     do
+        if [[ ${oxi} == 2 ]] && [[ ${spin} == 'IS' ]]; then
+            continue
+        fi
         dest_dir="${pb_path}/${ads}/${spin}1"
         mkdir -p ${dest_dir}
         cd ${dest_dir}
         cp "${dir}/start-${ads}.traj" ./start.traj
         cp ~/bin/tools/mnc/submit.sh .
-        if [[ ${ads} == oo ]] || [[ ${ads} == oo-oo ]] || [[ ${ads} == ohh ]] || [[ ${ads} == ohh-ohh ]]; then
-            oxi=2
-        elif [[ ${ads} == oo-ohh ]] || [[ ${ads} == ohh-oo ]]; then
-            oxi=2
-        elif [[ ${ads} == oo-oh ]] || [[ ${ads} == oo-ooh ]] || [[ ${ads} == ohh-oh ]] || [[ ${ads} == ohh-ooh ]]; then
-            oxi=3
-        elif [[ ${ads} == oo-o ]] || [[ ${ads} == ohh-o ]]; then
-            oxi=4
-        else
-            echo "Warning: spin for ${ads} is not defined"
-            exit
-        fi
         sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown${oxi}.py/g" submit.sh
         sed -i -e "s/jobname/Co-${ads}-${spin}/g" submit.sh
-        # sbatch submit.sh
-        python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown${oxi}.py"
+        sbatch submit.sh
+        # python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown${oxi}.py"
     done
 done
