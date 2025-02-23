a15b1247a31f9fadff01acac492c8f5cae8c631c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:10:10 -0800

    .

--- mnc/.ipynb_checkpoints/pourbaix-checkpoint.sh
+++ mnc/.ipynb_checkpoints/pourbaix-checkpoint.sh
@@ -15,19 +15,19 @@ python ~/bin/tools/mnc/add-ohh-oh.py
 python ~/bin/tools/mnc/add-ohh-o.py
 python ~/bin/tools/mnc/add-ohh-ooh.py
 
-pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/2_Co'   
-for ads in oo oo-oh oo-o oo-ooh
-do
-    for spin in LS IS HS
-    do
-        dest_dir="${pb_path}/${ads}/${spin}1"
-        # mkdir -p ${dest_dir}
-        cd ${dest_dir}
-        cp "${dir}/start-${ads}.traj" ./start.traj
-        cp ~/bin/tools/mnc/submit.sh .
-        sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown-oo.py/g" submit.sh
-        sed -i -e "s/jobname/Co-${ads}-${spin}/g" submit.sh
-        # sbatch submit.sh
-        python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown-oo.py"
-    done
-done
+# pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/2_Co'   
+# for ads in oo oo-oh oo-o oo-ooh
+# do
+#     for spin in LS IS HS
+#     do
+#         dest_dir="${pb_path}/${ads}/${spin}1"
+#         # mkdir -p ${dest_dir}
+#         cd ${dest_dir}
+#         cp "${dir}/start-${ads}.traj" ./start.traj
+#         cp ~/bin/tools/mnc/submit.sh .
+#         sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown-oo.py/g" submit.sh
+#         sed -i -e "s/jobname/Co-${ads}-${spin}/g" submit.sh
+#         # sbatch submit.sh
+#         python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown-oo.py"
+#     done
+# done
