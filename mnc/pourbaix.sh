b108a54b020b1ce8bfd9131b1d3eadf32906a4ca
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:14:02 -0800

    .

--- mnc/pourbaix.sh
+++ mnc/pourbaix.sh
@@ -15,19 +15,51 @@ python ~/bin/tools/mnc/add-ohh-oh.py
 python ~/bin/tools/mnc/add-ohh-o.py
 python ~/bin/tools/mnc/add-ohh-ooh.py
 
-# pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/2_Co'   
-# for ads in oo oo-oh oo-o oo-ooh
-# do
-#     for spin in LS IS HS
-#     do
-#         dest_dir="${pb_path}/${ads}/${spin}1"
-#         # mkdir -p ${dest_dir}
-#         cd ${dest_dir}
-#         cp "${dir}/start-${ads}.traj" ./start.traj
-#         cp ~/bin/tools/mnc/submit.sh .
-#         sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown-oo.py/g" submit.sh
-#         sed -i -e "s/jobname/Co-${ads}-${spin}/g" submit.sh
-#         # sbatch submit.sh
-#         python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown-oo.py"
-#     done
-# done
+pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/1_Fe'   
+for ads in oo oo-oo oo-ohh oo-oh oo-o oo-ooh ohh ohh-ohh ohh-oo ohh-oh ohh-o ohh-ooh
+do
+    for spin in LS IS HS
+    do
+        dest_dir="${pb_path}/${ads}/${spin}1"
+        mkdir -p ${dest_dir}
+        cd ${dest_dir}
+        cp "${dir}/start-${ads}.traj" ./start.traj
+        cp ~/bin/tools/mnc/submit.sh .
+        sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown-oo.py/g" submit.sh
+        sed -i -e "s/jobname/Fe-${ads}-${spin}/g" submit.sh
+        # sbatch submit.sh
+        python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown-oo.py"
+    done
+done
+
+dir='/pscratch/sd/j/jiuy97/6_MNC/0_clean/3d/7_Co/most_stable/relaxed'
+cd ${dir}
+python ~/bin/tools/mnc/add-oo.py
+python ~/bin/tools/mnc/add-oo-oo.py
+python ~/bin/tools/mnc/add-oo-ohh.py
+python ~/bin/tools/mnc/add-oo-oh.py
+python ~/bin/tools/mnc/add-oo-o.py
+python ~/bin/tools/mnc/add-oo-ooh.py
+python ~/bin/tools/mnc/add-ohh.py
+python ~/bin/tools/mnc/add-ohh-ohh.py
+python ~/bin/tools/mnc/add-ohh-oo.py
+python ~/bin/tools/mnc/add-ohh-oh.py
+python ~/bin/tools/mnc/add-ohh-o.py
+python ~/bin/tools/mnc/add-ohh-ooh.py
+
+pb_path='/pscratch/sd/j/jiuy97/6_MNC/pourbaix/2_Co'   
+for ads in oo oo-oo oo-ohh oo-oh oo-o oo-ooh ohh ohh-ohh ohh-oo ohh-oh ohh-o ohh-ooh
+do
+    for spin in LS IS HS
+    do
+        dest_dir="${pb_path}/${ads}/${spin}1"
+        mkdir -p ${dest_dir}
+        cd ${dest_dir}
+        cp "${dir}/start-${ads}.traj" ./start.traj
+        cp ~/bin/tools/mnc/submit.sh .
+        sed -i -e "s/mnc-sol.py/mnc-sol-${spin}-nupdown-oo.py/g" submit.sh
+        sed -i -e "s/jobname/Fe-${ads}-${spin}/g" submit.sh
+        # sbatch submit.sh
+        python "/pscratch/sd/j/jiuy97/6_MNC/scripts/mnc-sol-${spin}-nupdown-oo.py"
+    done
+done
