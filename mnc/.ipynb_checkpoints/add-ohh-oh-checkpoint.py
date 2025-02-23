277118d19e6924fddc69b83d0cbb240bfb934871
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:09:55 -0800

    .

--- mnc/.ipynb_checkpoints/add-ohh-oh-checkpoint.py
+++ mnc/.ipynb_checkpoints/add-ohh-oh-checkpoint.py
@@ -8,5 +8,4 @@ if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('H', positions=[atoms[-2].position + (0.8, -0.6, 0.0)])
     atoms += Atoms('O', positions=[atoms[-4].position + (0.0, 0.0, 2.0)])
     atoms += Atoms('H', positions=[atoms[-1].position + (0.0, -0.8, 0.6)])
-    atoms += Atoms('H', positions=[atoms[-2].position + (0.8, 0.6, 0.0)])
-    write('start-ohh-ohh.traj', atoms)
+    write('start-ohh-oh.traj', atoms)
