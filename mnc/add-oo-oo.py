600d14434abab9fa6b530ac0e274f27e0fab81d9
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-22 19:12:01 -0800

    .

--- mnc/add-oo-oo.py
+++ mnc/add-oo-oo.py
@@ -6,5 +6,5 @@ if atoms[-1].symbol not in ['C', 'N', 'O', 'H']:
     atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 0.0, -2.0)])
     atoms += Atoms('O', positions=[atoms[-1].position + (0.0, 1.0, -1.0)])
     atoms += Atoms('O', positions=[atoms[-3].position + (0.0, 0.0, 2.0)])
-    atoms += Atoms('O', positions=[atoms[-1].position + (-0.1, -1.3, 0.7)])
+    atoms += Atoms('O', positions=[atoms[-1].position + (0.0, -1.0, 1.0)])
     write('start-oo-oo.traj', atoms)
