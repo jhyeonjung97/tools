9e7260639330533201690a1f850c2a8d58370065
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-24 21:43:39 -0800

    .

--- mnc/pourbaix6.py
+++ mnc/pourbaix6.py
@@ -255,7 +255,7 @@ def overpotential_orr(int0, int1, int2, int3, int4, df, ORR):
             else:
                 ints[i] = int[1]
         if ints[i] == 'oo' or re.match(r"oo-.*", ints[i]):
-            df.loc[ints[i], 'dG'] -= 2*go
+            df.loc[ints[i], 'dG'] += 2*go
     int0, int1, int2, int3, int4 = ints
     
     dG = np.zeros(5)
