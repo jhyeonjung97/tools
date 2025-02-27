1ebc98ab4894ff1269f22140d3543245e4f40fcb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-27 15:36:39 -0800

    .

--- heo/.ipynb_checkpoints/scaling_relationship-checkpoint.py
+++ heo/.ipynb_checkpoints/scaling_relationship-checkpoint.py
@@ -104,7 +104,7 @@ def scaling_OH_O(df):
         Line2D([0], [0], marker='o', linestyle='', label='Co', color='w', markerfacecolor='red'),
         Line2D([0], [0], marker='o', linestyle='', label='Ni', color='w', markerfacecolor='purple'),
     ]
-    plt.legend(handles=legend_elements, loc="uppder left", bbox_to_anchor=(0.8, 0.5), 
+    plt.legend(handles=legend_elements, loc="upper left", bbox_to_anchor=(0.8, 0.5), 
                fontsize=8, ncol=1, columnspacing=0.5, handletextpad=0.0)
     plt.tight_layout()
     plt.savefig('heo_scaling.png')
