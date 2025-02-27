5162a70cdeeba1df49fbc913008d457caeec57bd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-27 13:40:40 -0800

    .

--- mnc/cathub.sh
+++ mnc/cathub.sh
@@ -18,22 +18,22 @@ for dir in ${mnc}/0_clean/*d/*_*/most_stable; do
     IFS='/' read -r -a path <<< "$dir"
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001"
     mkdir -p "${dest_dir}"
     cp "${dir}/relaxed/final_with_calculator.json" "${dest_dir}/empty_slab.json"
     # echo "Copied relaxed/final_with_calculator.json to ${dest_dir}"
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
     mkdir -p "${dest_dir}"
     cp "${dir}/o/final_with_calculator.json" "${dest_dir}/O.json"
     # echo "Copied o/final_with_calculator.json to ${dest_dir}"
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
     mkdir -p "${dest_dir}"
     cp "${dir}/oh/final_with_calculator.json" "${dest_dir}/OH.json"
     # echo "Copied oh/final_with_calculator.json to ${dest_dir}"
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
     mkdir -p "${dest_dir}"
     if [[ -f "${dir}/ooh/final_with_calculator.json" ]]; then
         cp "${dir}/ooh/final_with_calculator.json" "${dest_dir}/OOH.json"
@@ -43,7 +43,7 @@ for dir in ${mnc}/0_clean/*d/*_*/most_stable; do
     dzs=(0.0 0.2 0.4 0.6 0.8 1.0 1.2)
     for i in {0..6}; do
         dz=${dzs[$i]}
-        dest_dir="${cathub}/${metal}N4C26-Δz:${dz}Å-${site1}/001"
+        dest_dir="${cathub}/${metal}N4C26-Δz=${dz}Å-${site1}/001"
         mkdir -p "${dest_dir}"
         cp "${dir}/${i}_/final_with_calculator.json" "${dest_dir}/empty_slab.json"
         # echo "Copied ${i}_/final_with_calculator.json to ${dest_dir}"
@@ -54,7 +54,7 @@ for dir in ${mnc}/1_O/*_*/most_stable; do
     IFS='/' read -r -a path <<< "$dir"
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
     mkdir -p "${dest_dir}"
     cp "${dir}/relaxed/final_with_calculator.json" "${dest_dir}/O.json"
     # echo "Copied relaxed/final_with_calculator.json to ${dest_dir}"
@@ -62,7 +62,7 @@ for dir in ${mnc}/1_O/*_*/most_stable; do
     dzs=(0.0 0.2 0.4 0.6 0.8 1.0 1.2)
     for i in {0..6}; do
         dz=${dzs[$i]}
-        dest_dir="${cathub}/${metal}N4C26-Δz:${dz}Å-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
+        dest_dir="${cathub}/${metal}N4C26-Δz=${dz}Å-${site1}/001/H2Ogas_-1.0H2gas_star__Ostar"
         mkdir -p "${dest_dir}"
         cp "${dir}/${i}_/final_with_calculator.json" "${dest_dir}/O.json"
         # echo "Copied ${i}_/final_with_calculator.json to ${dest_dir}"
@@ -73,7 +73,7 @@ for dir in ${mnc}/2_OH/*_*/most_stable; do
     IFS='/' read -r -a path <<< "$dir"
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
     mkdir -p "${dest_dir}"
     cp "${dir}/relaxed/final_with_calculator.json" "${dest_dir}/OH.json"
     # echo "Copied relaxed/final_with_calculator.json to ${dest_dir}"
@@ -81,7 +81,7 @@ for dir in ${mnc}/2_OH/*_*/most_stable; do
     dzs=(0.0 0.2 0.4 0.6 0.8 1.0 1.2)
     for i in {0..6}; do
         dz=${dzs[$i]}
-        dest_dir="${cathub}/${metal}N4C26-Δz:${dz}Å-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
+        dest_dir="${cathub}/${metal}N4C26-Δz=${dz}Å-${site1}/001/H2Ogas_-0.5H2gas_star__OHstar"
         mkdir -p "${dest_dir}"
         cp "${dir}/${i}_/final_with_calculator.json" "${dest_dir}/OH.json"
         # echo "Copied ${i}_/final_with_calculator.json to ${dest_dir}"
@@ -92,7 +92,7 @@ for dir in ${mnc}/3_OOH/*_*/most_stable; do
     IFS='/' read -r -a path <<< "$dir"
     metal=$(echo "${path[-2]}" | cut -d'_' -f2)
     
-    dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
+    dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
     mkdir -p "${dest_dir}"
     cp "${dir}/relaxed/final_with_calculator.json" "${dest_dir}/OOH.json"
     # echo "Copied relaxed/final_with_calculator.json to ${dest_dir}"
@@ -100,7 +100,7 @@ for dir in ${mnc}/3_OOH/*_*/most_stable; do
     dzs=(0.0 0.2 0.4 0.6 0.8 1.0 1.2)
     for i in {0..6}; do
         dz=${dzs[$i]}
-        dest_dir="${cathub}/${metal}N4C26-Δz:${dz}Å-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
+        dest_dir="${cathub}/${metal}N4C26-Δz=${dz}Å-${site1}/001/2.0H2Ogas_-1.5H2gas_star__OOHstar"
         mkdir -p "${dest_dir}"
         cp "${dir}/${i}_/final_with_calculator.json" "${dest_dir}/OOH.json"
         # echo "Copied ${i}_/final_with_calculator.json to ${dest_dir}"
@@ -123,39 +123,39 @@ for dir in ${mnc}/pourbaix/*_*/*/most_stable; do
     elif [[ "$ads_upper" == "OOH" ]]; then
         site=${site1}; dual=''; rxn="2.0H2Ogas_-1.5H2gas_star__OOHstar"
     elif [[ "$ads_upper" == "OO" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="2.0H2Ogas_-2.0H2gas_star__OstarOstar"
+        site=${site1}; dual='adjacent'; rxn="2.0H2Ogas_-2.0H2gas_star__OstarOstar"
     elif [[ "$ads_upper" == "OHO" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="2.0H2Ogas_-1.5H2gas_star__OHstarOstar"
+        site=${site1}; dual='adjacent'; rxn="2.0H2Ogas_-1.5H2gas_star__OHstarOstar"
     elif [[ "$ads_upper" == "OHOH" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="2.0H2Ogas_-1.0H2gas_star__OHstarOHstar"
+        site=${site1}; dual='adjacent'; rxn="2.0H2Ogas_-1.0H2gas_star__OHstarOHstar"
     elif [[ "$ads_upper" == "OOHO" ]] || [[ "$ads_upper" == "OOOH" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="3.0H2Ogas_-2.5H2gas_star__OOHstarOstar"
+        site=${site1}; dual='adjacent'; rxn="3.0H2Ogas_-2.5H2gas_star__OOHstarOstar"
     elif [[ "$ads_upper" == "OOHOH" ]] || [[ "$ads_upper" == "OHOOH" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="3.0H2Ogas_-2.0H2gas_star__OOHstarOHstar"
+        site=${site1}; dual='adjacent'; rxn="3.0H2Ogas_-2.0H2gas_star__OOHstarOHstar"
     elif [[ "$ads_upper" == "OOHOOH" ]]; then
-        site=${site1}; dual=':adjacent'; rxn="4.0H2Ogas_-3.0H2gas_star__OOHstarOOHstar"
+        site=${site1}; dual='adjacent'; rxn="4.0H2Ogas_-3.0H2gas_star__OOHstarOOHstar"
     elif [[ "$ads_upper" == "O-O" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="2.0H2Ogas_-2.0H2gas_star__OstarOstar"
+        site=${site1}; dual='antipodal'; rxn="2.0H2Ogas_-2.0H2gas_star__OstarOstar"
     elif [[ "$ads_upper" == "OH-O" ]] || [[ "$ads_upper" == "O-OH" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="2.0H2Ogas_-1.5H2gas_star__OHstarOstar"
+        site=${site1}; dual='antipodal'; rxn="2.0H2Ogas_-1.5H2gas_star__OHstarOstar"
     elif [[ "$ads_upper" == "OH-OH" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="2.0H2Ogas_-1.0H2gas_star__OHstarOHstar"
+        site=${site1}; dual='antipodal'; rxn="2.0H2Ogas_-1.0H2gas_star__OHstarOHstar"
     elif [[ "$ads_upper" == "OOH-O" ]] || [[ "$ads_upper" == "O-OOH" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="3.0H2Ogas_-2.5H2gas_star__OOHstarOstar"
+        site=${site1}; dual='antipodal'; rxn="3.0H2Ogas_-2.5H2gas_star__OOHstarOstar"
     elif [[ "$ads_upper" == "OOH-OH" ]] || [[ "$ads_upper" == "OH-OOH" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="3.0H2Ogas_-2.0H2gas_star__OOHstarOHstar"
+        site=${site1}; dual='antipodal'; rxn="3.0H2Ogas_-2.0H2gas_star__OOHstarOHstar"
     elif [[ "$ads_upper" == "OOH-OOH" ]]; then
-        site=${site1}; dual=':antipodal'; rxn="4.0H2Ogas_-3.0H2gas_star__OOHstarOOHstar"
+        site=${site1}; dual='antipodal'; rxn="4.0H2Ogas_-3.0H2gas_star__OOHstarOOHstar"
     else
         rxn=''
     fi
     if [[ -n "${rxn}" ]] && [[ -f "${dir}/final_with_calculator.json" ]]; then
-        dest_dir="${cathub}/${metal}N4C26-Δz:relaxed-${site}${dual}/001/${rxn}"
+        dest_dir="${cathub}/${metal}N4C26-Δz=relaxed-${site}${dual}/001/${rxn}"
         mkdir -p "${dest_dir}"
         cp "${dir}/final_with_calculator.json" "${dest_dir}"
         # echo "Copied final_with_calculator.json to ${dest_dir}"
         if [[ ! ${site}${dual} == ${site1} ]]; then
-            cp "${cathub}/${metal}N4C26-Δz:relaxed-${site1}/001/empty_slab.json" "${cathub}/${metal}N4C26-Δz:relaxed-${site}${dual}/001"
+            cp "${cathub}/${metal}N4C26-Δz=relaxed-${site1}/001/empty_slab.json" "${cathub}/${metal}N4C26-Δz=relaxed-${site}${dual}/001"
         fi
     fi
 done
