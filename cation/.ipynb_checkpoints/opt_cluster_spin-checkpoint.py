cf3761ac187eca6c9ebd22229c164bd447ba2aea
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-28 12:49:02 -0800

    .

--- cation/.ipynb_checkpoints/opt_cluster_spin-checkpoint.py
+++ cation/.ipynb_checkpoints/opt_cluster_spin-checkpoint.py
@@ -1,13 +1,15 @@
+import os
+import time
 import subprocess
-from os import path
 from ase.io import read, write
 from ase.calculators.vasp import Vasp
 from ase.io.trajectory import Trajectory
 import ase.calculators.vasp as vasp_calculator
 
 name = 'opt_cluster'
+start_time = time.time()
 
-if path.exists('restart.json'):
+if os.path.exists('restart.json'):
     atoms = read('restart.json')
 else:
     atoms = read('start.traj')
@@ -41,3 +43,15 @@ print ('Calculation Complete, storing the run + calculator to traj file')
 
 Trajectory(f'final_{name}.traj','w').write(atoms)
 subprocess.call(f'ase convert -f final_{name}.traj final_{name}.json', shell=True)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+with open('time.log', 'a') as f:
+    f.write(f"Calculation Directory: {current_dir}\n")
+    f.write(f"Start Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}\n")
+    f.write(f"End Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}\n")
+    f.write(f"Elapsed Time: {elapsed_time:.2f} seconds\n")
+    f.write("="*40 + "\n")
+
+print(f"Execution time logged in 'time.log'.")
