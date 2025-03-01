755160fc52bc0f32ee2aef6c58d07b183e88ad8f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-01 00:41:59 -0800

    .

---
+++ cation/.ipynb_checkpoints/submit-checkpoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J pt
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -n 120
+#SBATCH -p roma
+#SBATCH -A suncat
+#SBATCH --mem=480G
+#SBATCH -e err.%j.log
+#SBATCH -o out.%j.log
+
+module load mpi
+
+export OMP_NUM_THREADS=1
+export VASP_SCRIPT=/sdf/home/j/jiuy97/bin/run_vasp_cpu.py
+export VASP_PP_PATH=/fs/ddn/sdf/group/suncat/sw/psp/vasp/potpaw54
+
+python ./opt_slab.py
+python ~/bin/verve/bader.py
+python ~/bin/get_restart3
