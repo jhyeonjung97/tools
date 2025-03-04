88c38ea121c72319d7efb5cbb47597d1f312fe1f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-04 15:57:05 -0800

    .

--- tetra/.ipynb_checkpoints/png2-checkpoint.sh
+++ tetra/.ipynb_checkpoints/png2-checkpoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [[ ${here} == 'slac' ]]; then
-    /usr/bin/rsync -e 'ssh' --ignore-times --size-only -avlzp -K --max-size=50000m \
-    	--include="*/" \
-    	--include="*.png" \
-    	--exclude="*" \
-     jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_bulk/nersc .
-elif [[ ${here} == 'nersc' ]]; then
     
     ### section1
     
