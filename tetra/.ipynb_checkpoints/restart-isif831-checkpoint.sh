7dd3805097aa8dda41c767f985a5eb040b8fe665
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-07 13:03:18 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
@@ -1,5 +1,5 @@
 i=0
-for dir in /pscratch/sd/j/jiuy97/7_V_bulk/6_Octahedral_RS/4d/*_*
+for dir in /pscratch/sd/j/jiuy97/7_V_bulk/6_Octahedral_RS/3d/*_*
 do
     cd "$dir" || continue
     
@@ -11,22 +11,30 @@ do
     jobname=${coord}${row}${numb}
     
     if [[ -n $(squeue --me | grep $jobname) ]]; then
-        pwd
+        continue
+    elif [[ -d "isif3" ]]; then
+        if [[ -f "DONE" ]]; then
+            continue
+        else
+            pwd; echo -e "\e[31mCheck this directory!\e[0m"
+        fi
+    elif [[ -d "isif8" ]]; then
+        if [[ -f "DONE" ]]; then
+            mkdir isif3; mv * isif3; cd isif3
+            mv isif8 $dir; cp CHGCAR WAVECAR restart.json submit.sh $dir; cd $dir
+            sed -i -e 's/opt_bulk8/opt_bulk3/' submit.sh
+            pwd; echo -e "\e[32mSubmit?\e[0m"; # sbatch submit.sh; ((i+=1))
+        else
+            pwd; echo -e "\e[31mCheck this directory!\e[0m"
+        fi
+    else
+        if [[ -f "DONE" ]]; then
+            mkdir isif8; mv * isif8; cd isif8
+            cp CHGCAR WAVECAR restart.json submit.sh $dir; cd $dir
+            sed -i -e 's/opt_bulk8/opt_bulk3/' submit.sh
+            pwd; echo -e "\e[32mSubmit?\e[0m"; # sbatch submit.sh; ((i+=1))
+        else
+            pwd; echo -e "\e[31mCheck this directory!\e[0m"
+        fi
     fi
-    # [[ -d "isif3" ]]; then
-    #     if [[ -f "DONE" ]]; then
-    #         continue
-    #     elif 
-    # elif [[ -d "$dir/isif8/" ]]; then
-        
-    # elif (( i < 5 )) && [[ -f "$dir/DONE" ]] && [[ -f "$dir/final_opt_bulk8_afm.traj" ]]; then  # Correct integer comparison
-    #     pwd
-    #     mkdir isif8
-    #     #sbatch submit.sh
-    #     ((i+=1))  # Correct increment syntax
-    # elif (( i < 5 )) && [[ -f "$dir/DONE" ]] && [[ -f "$dir/final_opt_bulk8_afm.traj" ]]; then  # Correct integer comparison
-    #     pwd
-    #     #sbatch submit.sh
-    #     ((i+=1))  # Correct increment syntax
-    # fi
 done
