763aaaebe84994d314b2d6fa7003d40f81f5bc67
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-07 14:58:39 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
@@ -28,21 +28,21 @@ do
         pwd; sbatch submit.sh; ((i+=1))
     elif [[ ! -f "DONE" ]]; then
         pwd; echo -e "\e[31mCheck this directory!\e[0m"
-    # elif [[ -d "isif2" ]]; then
-    #     continue
-    elif [[ -d "isif3" ]]; then
-        # mkdir isif2; find . -maxdepth 1 -type f -exec mv {} isif2/ \;
-        # cd isif2; cp WAVECAR restart.json submit.sh $dir
-        # cd $dir; sed -i -e 's/opt_bulk2_afm/static_bulk/' submit.sh
-        # sed -i -e '/bader/d' submit.sh
-        # echo '~/bin/lobster-5.0.0/lobster-5.0.0' >> submit.sh
-        # echo 'python ~/bin/aloha/cohp.py > icohp.txt' >> submit.sh
-        # echo 'python ~/bin/aloha/cobi.py > icobi.txt' >> submit.sh
-        # echo 'python ~/bin/tools/tetra/spilling.py' >> submit.sh
-        # cp ~/bin/tools/tetra/lobsterin .
-        # sed -i -e "s/X/${metal}/" lobsterin
-        # pwd; sbatch submit.sh; ((i+=1))
+    elif [[ -d "isif2" ]]; then
         continue
+    elif [[ -d "isif3" ]]; then
+        mkdir isif2; find . -maxdepth 1 -type f -exec mv {} isif2/ \;
+        cd isif2; cp WAVECAR restart.json submit.sh $dir
+        cd $dir; sed -i -e 's/opt_bulk2_afm/static_bulk/' submit.sh
+        sed -i -e '/bader/d' submit.sh
+        echo '~/bin/lobster-5.0.0/lobster-5.0.0' >> submit.sh
+        echo 'python ~/bin/aloha/cohp.py > icohp.txt' >> submit.sh
+        echo 'python ~/bin/aloha/cobi.py > icobi.txt' >> submit.sh
+        echo 'python ~/bin/aloha/coop.py > icoop.txt' >> submit.sh
+        echo 'python ~/bin/tools/tetra/spilling.py' >> submit.sh
+        cp ~/bin/tools/tetra/lobsterin .
+        sed -i -e "s/X/${metal}/" lobsterin
+        pwd; sbatch submit.sh; ((i+=1))
     elif [[ -d "isif8" ]]; then
         mkdir isif3; find . -maxdepth 1 -type f -exec mv {} isif3/ \;
         cd isif3; cp WAVECAR restart.json submit.sh $dir
