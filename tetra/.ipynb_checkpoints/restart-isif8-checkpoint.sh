d227dd54cb8e9f58a08ba4c730745c81bd33466f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-07 16:18:15 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
@@ -8,7 +8,7 @@ do
         exit
     fi
     
-    cd "$dir"; pwd
+    cd "$dir"
     IFS='/' read -r -a path <<< $dir
     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
     row=$(echo "${path[-2]}" | cut -d'_' -f1)
@@ -19,14 +19,6 @@ do
     if [[ -n $(squeue --me | grep $jobname) ]]; then
         continue
     elif [[ -z "$(find . -maxdepth 1 -type f ! -name 'start.traj' ! -name 'submit.sh' ! -name '.*')" ]]; then
-        pwd; sbatch submit.sh; ((i+=1))
-    elif [[ ! -f "DONE" ]]; then
-        # continue
-        pwd; echo -e "\e[31mCheck this directory!\e[0m"
-    else
-        mkdir isif8; find . -maxdepth 1 -type f -exec mv {} isif8/ \;
-        cd isif8; cp WAVECAR restart.json submit.sh $dir
-        cd $dir; sed -i -e 's/opt_bulk8/opt_bulk3/' submit.sh
         pwd; sbatch submit.sh; ((i+=1))
     fi
 done
