31c6b8df284f8cfce8ab4db15586185f81932742
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-08 01:09:58 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
@@ -25,22 +25,25 @@
 
 for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*d/*_*
 do    
-    cd $dir
-    IFS='/' read -r -a path <<< $dir
+    cd "$dir" || continue  # Ensure script doesn't fail if cd is unsuccessful
+    IFS='/' read -r -a path <<< "$dir"
+    
     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
     row=$(echo "${path[-2]}" | cut -d'_' -f1)
     numb=$(echo "${path[-1]}" | cut -d'_' -f1)
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
-    jobname=${coord}${row}${numb}
+    jobname="${coord}${row}${numb}"
 
-    if [[ -n $(squeue --me | grep $jobname) ]] || [[ -f 'DONE' ]]; then
+    if [[ -n "$(squeue --me | grep "$jobname")" ]] || [[ -f 'DONE' ]]; then
         continue
-    elif [[ -z "$(find . -maxdepth 1 -type f ! -name 'start.traj' ! -name 'submit.sh' ! -name '.*')" ]]; then
+    else
         pwd; python ~/bin/get_restart3; sbatch submit.sh
     fi
     
-    if [[ $row == '3d' ]] && [[ -f 'DONE' ]] && [[ -f 'restart.json' ]]; then
-        dir_fm = "/pscratch/sd/j/jiuy97/7_V_bulk/${path[-3]}/fm/${path[-1]}"
-        cp ./restart.json ./submit.sh $dir_fm        
+    if [[ "$row" == '3d' ]] && [[ -f 'DONE' ]] && [[ -f 'restart.json' ]]; then
+        dir_fm="/pscratch/sd/j/jiuy97/7_V_bulk/${path[-3]}/fm/${path[-1]}"
+        mkdir -p "$dir_fm"  # Ensure destination directory exists
+        cp ./restart.json ./submit.sh "$dir_fm"
+        sed -i -e 's/3d/fm/' -e 's/afm/fm/' "$dir_fm/submit.sh"
     fi
 done
