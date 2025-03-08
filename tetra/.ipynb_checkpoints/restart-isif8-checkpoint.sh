76c786d0d2304885be82675332a0d04df4dd8543
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-08 00:59:40 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif8-checkpoint.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-i=$(squeue --me | grep 'gpu' | wc -l)
+# i=$(squeue --me | grep 'gpu' | wc -l)
+
+# for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*d/*_*
+# do
+#     if(( i > 4 )); then
+#         exit
+#     fi
+    
+#     cd "$dir"
+#     IFS='/' read -r -a path <<< $dir
+#     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
+#     row=$(echo "${path[-2]}" | cut -d'_' -f1)
+#     numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+#     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
+#     jobname=${coord}${row}${numb}
+
+#     if [[ -n $(squeue --me | grep $jobname) ]]; then
+#         continue
+#     elif [[ -z "$(find . -maxdepth 1 -type f ! -name 'start.traj' ! -name 'submit.sh' ! -name '.*')" ]]; then
+#         pwd; sbatch submit.sh; ((i+=1))
+#     fi
+# done
 
 for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*d/*_*
-do
-    if(( i > 4 )); then
-        exit
-    fi
-    
-    cd "$dir"
+do    
+    cd $dir
     IFS='/' read -r -a path <<< $dir
     coord=$(echo "${path[-3]}" | cut -d'_' -f3)
     row=$(echo "${path[-2]}" | cut -d'_' -f1)
@@ -16,9 +33,9 @@ do
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
     jobname=${coord}${row}${numb}
 
-    if [[ -n $(squeue --me | grep $jobname) ]]; then
+    if [[ -n $(squeue --me | grep $jobname) ]] || [[ -f 'DONE' ]]; then
         continue
     elif [[ -z "$(find . -maxdepth 1 -type f ! -name 'start.traj' ! -name 'submit.sh' ! -name '.*')" ]]; then
-        pwd; sbatch submit.sh; ((i+=1))
+        pwd; python ~/bin/get_restart3; sbatch submit.sh
     fi
 done
