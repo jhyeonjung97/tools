c88de913fb6a7ce22479a2a05c52a6cc53236c4b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-08 10:30:50 -0800

    .

--- tetra/get_restart3.sh
+++ tetra/get_restart3.sh
@@ -1,5 +1,5 @@
 for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*/*_*; do
     if [[ -f $dir/DONE ]] && [[ ! -n $(grep 'PROFILE, used timers' $dir/vasp.out) ]]; then
-        echo $dir
+        echo $dir; rm $dir/DONE
     fi
 done
