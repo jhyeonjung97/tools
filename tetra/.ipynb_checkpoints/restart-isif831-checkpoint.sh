551cab21d3fd5a73c351c2c55d4e937746e4da13
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-08 17:48:07 -0800

    .

--- tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
+++ tetra/.ipynb_checkpoints/restart-isif831-checkpoint.sh
@@ -15,6 +15,10 @@ do
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
     jobname=${coord}${row}${numb}
     
+    if [[ -f "$dir/DONE" ]]; then
+        ~/bin/shoulder/rm_mv "$dir"/*log
+    fi
+    
     if [[ -n $(squeue --me | grep $jobname) ]] || [[ -z $(find . -maxdepth 1 -type f) ]]; then
         continue
     elif [[ -z "$(find . -maxdepth 1 -type f ! -name 'start.traj' ! -name 'submit.sh' ! -name '.*')" ]]; then
