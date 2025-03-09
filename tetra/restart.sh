93c27ed5d354db9f8b1c82e27a785c8bdc0d0f6a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-08 16:50:05 -0800

    .

--- tetra/restart.sh
+++ tetra/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+i=0
 for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*/*_*
 do    
     IFS='/' read -r -a path <<< "$dir"
@@ -9,15 +10,19 @@ do
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
     jobname="${coord}${row}${numb}"
     
-    if [[ -f "$dir/DONE" ]] && [[ ! -n $(grep 'PROFILE, used timers' "$dir/vasp.out") ]]; then
-        cd $dir; rm DONE
-        pwd; python ~/bin/tools/tetra/get_restart3.py; sbatch submit.sh
+    if [[ ! -f "$dir/DONE" && $coord != 'LT' && $coord != 'AU' && $coord != 'AQ' ]]; then
+        ((i+=1))
     fi
+#     if [[ -f "$dir/DONE" ]] && [[ ! -n $(grep 'PROFILE, used timers' "$dir/vasp.out") ]]; then
+#         cd $dir; rm DONE
+#         pwd; python ~/bin/tools/tetra/get_restart3.py; sbatch submit.sh
+#     fi
     
-    if [[ ! -f "$dir/DONE" ]] && [[ -n $(grep 'PROFILE, used timers' "$dir/vasp.out") ]]; then
-        cd $dir; python ~/bin/tools/tetra/get_restart3.py
-        if [[ -f "$dir/DONE" ]]; then
-            pwd; squeue --me | grep --color=auto $jobname
-        fi
-    fi
+#     if [[ ! -f "$dir/DONE" ]] && [[ -n $(grep 'PROFILE, used timers' "$dir/vasp.out") ]]; then
+#         cd $dir; python ~/bin/tools/tetra/get_restart3.py
+#         if [[ -f "$dir/DONE" ]]; then
+#             pwd; squeue --me | grep --color=auto $jobname
+#         fi
+#     fi
 done
+echo $i
