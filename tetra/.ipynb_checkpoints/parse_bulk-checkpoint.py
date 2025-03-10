3bdebc6e052908d96d949bc3ef92d56376fc9538
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-10 16:09:05 -0700

    .

--- tetra/.ipynb_checkpoints/parse_bulk-checkpoint.py
+++ tetra/.ipynb_checkpoints/parse_bulk-checkpoint.py
@@ -63,7 +63,7 @@ int_cols = ['CN', 'ON', 'n_bond']
 float_cols = ['energy', 'form', 'volume', 'cell', 'chg', 'mag', 'l_bond', 'n_bond', '-ICOHPm', 'ICOBIm', '-ICOOPm', '-ICOHPn', 'ICOBIn', '-ICOOPn', 'madelung', 'grosspop']
 
 metal_df = pd.read_csv('~/bin/tools/tetra/metal-data.tsv', sep='\t')
-
+print(metal_df)
 def main():
     global df
     
