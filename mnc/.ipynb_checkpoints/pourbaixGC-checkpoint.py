0b591fbae208dee01971f6aeef71d83ebca448ca
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-11 06:50:18 -0700

    .

--- mnc/.ipynb_checkpoints/pourbaixGC-checkpoint.py
+++ mnc/.ipynb_checkpoints/pourbaixGC-checkpoint.py
@@ -107,7 +107,7 @@ def dg_ion(k, pH, U):
     dg = (
         surfs[k][0]
         - (surfs[0][6]*(U**2) + surfs[0][7]*U + surfs[0][8])
-        + 2 * (dgh -gh + 1 * (U + pH * const))
+        + 2 * (dgh -gh) # + 1 * (U + pH * const))
         + surfs[k][2] * (1 * (U + pH * const))
         + surfs[k][3] * (-2 * (U + pH * const))
         + surfs[k][4] * (-1 * (U + pH * const))
