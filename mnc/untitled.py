9582e437a1faaa6e12ef57ff9d7ac7d19ed3c578
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-14 14:08:48 -0700

    .

--- mnc/untitled.py
+++ mnc/untitled.py
@@ -3,13 +3,14 @@ from ase.io import read, write
 # Read atomic structure from CONTCAR
 atoms = read('CONTCAR')
 
+n = 0
 # Modify z-coordinate for N and Co atoms
 for atom in atoms:
     if atom.symbol == 'N':
         atom.position[2] = 10.0  # Modify z-coordinate
     elif atom.symbol == 'Co':
-        dz = 10.0 - atom.position[2]
-        atom.position[2] = 10.0  # Modify z-coordinate
+        dz = 10.0 + n * 0.2 - atom.position[2]
+        atom.position[2] = 10.0 + n * 0.2  # Modify z-coordinate
     elif atom.symbol in ['O', 'H']:
         atom.position[2] += dz
         
