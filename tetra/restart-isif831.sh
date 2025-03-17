ffe19dad7f9b6594c67a6c0169844ce61cdb631d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-17 12:41:11 -0700

    .

--- tetra/restart-isif831.sh
+++ tetra/restart-isif831.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-for dir in /pscratch/sd/j/jiuy97/7_V_bulk/*_*_*/*/*_*/from_*
+for dir in /pscratch/sd/j/jiuy97/7_V_bulk/7_Pyramidal_LT/*/*_*
 do
-    cd $dir; pwd
+    cd $dir
     IFS='/' read -r -a path <<< $dir
-    coord=$(echo "${path[-4]}" | cut -d'_' -f3)
-    row=$(echo "${path[-3]}" | cut -d'_' -f1)
-    numb=$(echo "${path[-2]}" | cut -d'_' -f1)
-    metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+    coord=$(echo "${path[-3]}" | cut -d'_' -f3)
+    row=$(echo "${path[-2]}" | cut -d'_' -f1)
+    numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+    metal=$(echo "${path[-1]}" | cut -d'_' -f2)
     jobname=${coord}${row}${numb}
+    
+    dir_fm="/pscratch/sd/j/jiuy97/7_V_bulk/${path[-3]}/fm/${path[-1]}"
+    if [[ $row == '3d' ]] && [[ -f "$dir/isif8/DONE" ]] && [[ -f "$dir/isif8/restart.json" ]] && [[ ! -f "$dir_fm/start.traj" ]] && [[ ! -d "$dir_fm/isif8" ]]; then
+        cd $dir_fm; cp $dir/isif8/CONTCAR $dir/isif8/submit.sh .
+        echo -e "\e[32mcp CONTCAR submit.sh $dir_fm\e[0m"
+        ase convert CONTCAR start.traj; rm CONTCAR
+        sed -i -e "s/$jobname/${coord}fm${numb}/" -e 's/afm/fm/' submit.sh
+    fi
     
     if [[ -n $(squeue --me | grep $jobname) ]] || [[ -z $(find . -maxdepth 1 -type f) ]]; then
         continue
@@ -25,13 +33,12 @@ do
         fi
         pwd; sbatch submit.sh #; ((i+=1))
     elif [[ ! -f "DONE" ]]; then
-        pwd; echo -e "\e[31mCheck this directory!\e[0m"
-        # python ~/bin/tools/tetra/get_restart3.py
-        # if [[ -f "$dir/DONE" ]]; then
-        #     pwd; echo -e "\e[31mCheck this directory!\e[0m"
-        # else
-        #     pwd; sbatch submit.sh
-        # fi
+        python ~/bin/tools/tetra/get_restart3.py
+        if [[ -f "$dir/DONE" ]]; then
+            pwd; echo -e "\e[31mCheck this directory!\e[0m"
+        else
+            pwd; sbatch submit.sh
+        fi
     elif [[ -d "isif2" ]]; then
         continue
     elif [[ -d "isif3" ]]; then
@@ -50,13 +57,21 @@ do
         cp ~/bin/tools/tetra/lobsterin .
         sed -i -e "s/X/${metal}/" lobsterin
         pwd; sbatch submit.sh #; ((i+=1))
-    # else
-    #     mkdir isif3; find . -maxdepth 1 -type f -exec mv {} isif3/ \;
-    #     cd isif3; cp WAVECAR restart.json submit.sh $dir; cd $dir
-    #     sed -i -e 's/\.\/opt/~\/bin\/tools\/tetra\/opt/' submit.sh
-    #     sed -i -e 's/_symprec.py/.py/' submit.sh
-    #     sed -i -e 's/opt_bulk3/opt_bulk2/' submit.sh
-    #     echo '\npython ~/bin/verve/bader.py' >> submit.sh
-    #     pwd; sbatch submit.sh #; ((i+=1))
+    elif [[ -d "isif8" ]]; then
+        mkdir isif3; find . -maxdepth 1 -type f -exec mv {} isif3/ \;
+        cd isif3; cp WAVECAR restart.json submit.sh $dir; cd $dir
+        sed -i -e 's/\.\/opt/~\/bin\/tools\/tetra\/opt/' submit.sh
+        sed -i -e 's/_symprec.py/.py/' submit.sh
+        sed -i -e 's/opt_bulk3/opt_bulk2/' submit.sh
+        echo '' >> submit.sh
+        echo 'python ~/bin/verve/bader.py' >> submit.sh
+        pwd; sbatch submit.sh #; ((i+=1))
+    else
+        mkdir isif8; find . -maxdepth 1 -type f -exec mv {} isif8/ \;
+        cd isif8; cp WAVECAR restart.json submit.sh $dir; cd $dir
+        sed -i -e 's/\.\/opt/~\/bin\/tools\/tetra\/opt/' submit.sh
+        sed -i -e 's/_symprec.py/.py/' submit.sh
+        sed -i -e 's/opt_bulk8/opt_bulk3/' submit.sh
+        pwd; sbatch submit.sh #; ((i+=1))
     fi
 done
