d8fb401d3df0854ce836afea32b5cc8a56583e8c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-03-24 23:47:24 -0700

    .

--- tetra/.ipynb_checkpoints/lr-checkpoint.py
+++ tetra/.ipynb_checkpoints/lr-checkpoint.py
@@ -22,8 +22,14 @@ import os
 
 def main():
     parser = argparse.ArgumentParser(description='Linear regression using bulk_data.csv and mendeleev_data.csv')
-    parser.add_argument('--Y', required=True, help='Target column from bulk_data.csv')
-    parser.add_argument('--X', required=True, nargs='+', help='List of feature columns from bulk_data.csv and/or mendeleev_data.csv')
+    parser.add_argument('--Y', default='form', help='Target column from bulk_data.csv (default: form)')
+    parser.add_argument('--X', nargs='+', default=[
+        'CN', 'ON', 'energy', 'volume', 'cell', 'chg', 'mag', 'l_bond', 'n_bond',
+        '-ICOHPm', 'ICOBIm', '-ICOOPm', '-ICOHPn', 'ICOBIn', '-ICOOPn',
+        'madelung', 'grosspop', 'Natom', 'Vatom', 'Tboil', 'Tmelt', 'mass', 'density',
+        'dipole', 'pauling', 'Rcoval', 'Rmetal', 'Rvdw', 'Hevap', 'Hfus', 'Hform',
+        'ion1', 'ion2', 'ion3', 'ion12'
+    ], help='List of feature columns from bulk_data.csv and/or mendeleev_data.csv')
     parser.add_argument('--row', type=str, default=None, help='Filter by row: 3d, 4d, or 5d')
     parser.add_argument('--coord', type=str, default=None, help='Filter by coordination, e.g., ZB, RS')
     parser.add_argument('--output', type=str, default='result', help='Output filename prefix')
