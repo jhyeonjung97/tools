f84dc91f24c8075986166c43d102bdf5d9615efb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-18 22:14:39 -0700

    .

--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -15,6 +15,7 @@ do
     elif [[ -f "submit.sh" ]]; then
         cp ~/bin/tools/tetra/submit_slab.sh ./submit.sh
         sed -i -e "s/jobname/${jobname}/" submit.sh
+        python ~/bin/get_restart3.py
         pwd; #sbatch submit.sh
     fi
 done
