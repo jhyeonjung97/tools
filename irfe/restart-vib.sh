0772086ebc9f2a7aa778108b438eea3e1651ea2d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-20 23:52:18 -0700

    .

--- irfe/restart-vib.sh
+++ irfe/restart-vib.sh
@@ -1,23 +1,23 @@
-for dir in /home/hyeonjung/scratch/4_IrFe3/*_*/*_*/*_*_*
-do
-    cd $dir
-    IFS='/' read -r -a path <<< $dir
-    vib_dir=/home/hyeonjung/scratch/4_IrFe3/${path[-3]}/vib/${path[-2]}/${path[-1]}
-    if [[ -f CONTCAR ]]; then
-        python ~/bin/tools/irfe/vib.py
-        cp vib.vasp $vib_dir/POSCAR
-    fi
-    cd $vib_dir
-    cp ~/bin/tools/irfe/INCAR_vib INCAR
-    cp ~/bin/tools/irfe/KPOINTS .
-    vaspkit -task 107
-    mv POSCAR_REV POSCAR
-    vaspkit -task 103
-    python3 ~/bin/orange/magmom.py
-done
+# for dir in /home/hyeonjung/scratch/4_IrFe3/*_*/*_*/*_*_*
+# do
+#     cd $dir
+#     IFS='/' read -r -a path <<< $dir
+#     vib_dir=/home/hyeonjung/scratch/4_IrFe3/${path[-3]}/vib/${path[-2]}/${path[-1]}
+#     if [[ -f CONTCAR ]]; then
+#         python ~/bin/tools/irfe/vib.py
+#         cp vib.vasp $vib_dir/POSCAR
+#     fi
+#     cd $vib_dir
+#     cp ~/bin/tools/irfe/INCAR_vib INCAR
+#     cp ~/bin/tools/irfe/KPOINTS .
+#     vaspkit -task 107
+#     mv POSCAR_REV POSCAR
+#     vaspkit -task 103
+#     python3 ~/bin/orange/magmom.py
+# done
 
 for dir in /home/hyeonjung/scratch/4_IrFe3/*_*/vib/*_*
 do
     cd $dir
-    cp ~/bin/tools/irfe/run_slurm.sh .
+    cp ~/bin/tools/irfe/run_vib.sh run_slurm.sh
 done
