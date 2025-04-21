e09fcca2c1f0b8bf4f665081abb36993d6ea23b2
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-20 23:58:44 -0700

    .

--- irfe/run_vib.sh
+++ irfe/run_vib.sh
@@ -13,8 +13,7 @@ for dir in 0_Ir 1_Mn 2_Fe 3_Co 4_Ni; do
     for sub in 1_top_layer 2_hol_layer 3_Ir_top 4_Ir_hol; do
         path="${dir}/${sub}"
         cp $path/* .
-	    mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.
-3.2.std.x
+	    mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
         cp * $path/
     done
 done
