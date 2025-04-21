1e09ff6d6765bd5c423d1153c044373e17bf86c8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-20 23:57:41 -0700

    .

--- irfe/run_vib.sh
+++ irfe/run_vib.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --partition=g1
-#SBATCH -J IrFe-O1
+#SBATCH -J IrFe1
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
 #SBATCH -e STDERR.%N.%j.err
@@ -10,18 +9,12 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-cp 1_top_layer/* .
-mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-cp * 1_top_layer/
-
-cp 2_hol_layer/* .
-mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-cp * 2_hol_layer/
-
-cp 3_Ir_top/* .
-mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-cp * 3_Ir_top/
-
-cp 4_Ir_hol/* .
-mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-cp * 4_Ir_hol/
+for dir in 0_Ir 1_Mn 2_Fe 3_Co 4_Ni; do
+    for sub in 1_top_layer 2_hol_layer 3_Ir_top 4_Ir_hol; do
+        path="${dir}/${sub}"
+        cp $path/* .
+	    mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.
+3.2.std.x
+        cp * $path/
+    done
+done
