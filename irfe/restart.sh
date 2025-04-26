bbd55a5ba02377bd1d6adc955e68df7270d7d2c6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-26 06:01:09 -0700

    .

--- irfe/restart.sh
+++ irfe/restart.sh
@@ -61,26 +61,45 @@
 #     fi
 # done
 
-# for dir in /home/hyeonjung/scratch/4_IrFe3/2_OH/*_*/*_*_brg
-# do
-#     cd $dir
-#     if [[ ! -f OUTCAR ]]; then
-#         cp ~/bin/tools/irfe/INCAR .
-#         cp ~/bin/tools/irfe/KPOINTS .
-#         mv *.vasp POSCAR
-#         vaspkit -task 107
-#         mv POSCAR_REV POSCAR
-#         rm POTCAR
-#         vaspkit -task 103
-#         python3 ~/bin/orange/magmom.py
-#     fi
-# done
-
-path=/home/hyeonjung/scratch/4_IrFe3/slab
 for dir in 5_IrMn 6_IrFe 7_IrCo 8_IrNi
 do
-    cd $path/$dir
+    cd /home/hyeonjung/scratch/4_IrFe3/slab/$dir
     cp h-*.vasp /home/hyeonjung/scratch/4_IrFe3/1_H/$dir
     cp o-*.vasp /home/hyeonjung/scratch/4_IrFe3/3_O/$dir
     cp oh-*.vasp /home/hyeonjung/scratch/4_IrFe3/2_OH/$dir
+
+    cd /home/hyeonjung/scratch/4_IrFe3/1_H/$dir
+    mv h-top-layer.vasp 1_layer_top
+    mv h-hol-layer.vasp 2_layer_hol
+    mv h-top-atom.vasp 3_atom_top
+    mv h-hol-atom.vasp 4_atom_hol
+
+    cd /home/hyeonjung/scratch/4_IrFe3/2_OH/$dir
+    mv oh-top-layer.vasp 1_layer_top
+    mv oh-brg-layer.vasp 2_layer_brg
+    mv oh-top-atom1.vasp 3_atom_top1
+    mv oh-top-atom2.vasp 4_atom_top2
+    mv oh-brg-atom1.vasp 5_atom_brg1
+    mv oh-brg-atom2.vasp 6_atom_brg2
+
+    cd /home/hyeonjung/scratch/4_IrFe3/3_O/$dir
+    mv o-top-layer.vasp 1_layer_top
+    mv o-hol-layer.vasp 2_layer_hol
+    mv o-top-atom.vasp 3_atom_top
+    mv o-hol-atom.vasp 4_atom_hol
+
+    for subdir in /home/hyeonjung/scratch/4_IrFe3/*_*/$dir/*_*_*
+    do
+        cd $subdir
+        if [[ ! -f OUTCAR ]]; then
+            cp ~/bin/tools/irfe/INCAR .
+            cp ~/bin/tools/irfe/KPOINTS .
+            mv *.vasp POSCAR
+            vaspkit -task 107
+            mv POSCAR_REV POSCAR
+            rm POTCAR
+            vaspkit -task 103
+            python3 ~/bin/orange/magmom.py
+        fi
+    done
 done
