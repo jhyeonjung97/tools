f5311a08b979099896155cf1b679fb555a661cbf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-26 06:46:40 -0700

    .

--- irfe/add3.py
+++ irfe/add3.py
@@ -12,12 +12,19 @@ atoms = original_atoms.copy()
 atoms += Atoms('H', positions=[atoms[37].position + (0.5, 0.8, 0.0)])
 write('o-oh.vasp', atoms)
 
-atoms = read('o-oh.vasp')
-del atoms[34]
-write('v-oh.vasp', atoms)
-
-del original_atoms[34]
+del original_atoms[36]
 write('v-o.vasp', original_atoms)
 
 del original_atoms[36]
 write('v-v.vasp', original_atoms)
+
+atoms = read('o-oh.vasp')
+del atoms[36]
+write('v-oh.vasp', atoms)
+
+# rm CONTCAR
+# mv o-oh.vasp 5_O_OH
+# mv o-ooh.vasp 6_O_OOH
+# mv v-oh.vasp 3_V_OH
+# mv v-o.vasp 2_V_O
+# mv v-v.vasp 1_V_V
