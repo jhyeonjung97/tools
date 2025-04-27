9e5210cf7d56d855abe3fa99af9391883d52d6f6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-27 15:45:16 -0700

    .

--- irfe/run_slurm.sh
+++ irfe/run_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --partition=g1
-#SBATCH -J IrFe-H
+#SBATCH -J IrFe-vib1
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
 #SBATCH -e STDERR.%N.%j.err
@@ -12,10 +12,12 @@
 
 for dir in 5_IrMn 6_IrFe 7_IrCo 8_IrNi
 do
-    for subdir in 3_atom_top1 5_atom_hol1
+    for subdir in 1_layer_top 2_layer_hol 3_atom_top1 4_atom_top2 5_atom_hol1 6_atom_hol2
     do
-        cp $dir/$subdir/* .
-        mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-        cp * $dir/$subdir/
+        if [[ -d ../$dir/$subdir/vib ]] && [[ ! -f ../$dir/$subdir/vib/OUTCAR ]]; then
+            cp ../$dir/$subdir/vib/* .
+            mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
+            cp * ../$dir/$subdir/vib/
+        fi
     done
 done
