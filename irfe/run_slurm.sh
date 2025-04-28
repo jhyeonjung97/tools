c5b10d8780ea7db3ced806b6203cb7786a1777c0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-27 18:59:36 -0700

    .

--- irfe/run_slurm.sh
+++ irfe/run_slurm.sh
@@ -10,10 +10,5 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-for dir in 1_Ir_top/1_V_V/ 1_Ir_top/6_O_OOH/ 2_Ir_hol/3_V_OH/ 2_Ir_hol/5_O_OH/ 3_IrFe_top1/6_O_OOH/ 4_IrFe_top2/6_O_OOH/ 5_IrFe_top3/6_O_OOH/
-do
-    cp $dir/* .
-    mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-    cp * $dir/
-done
+mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
 
