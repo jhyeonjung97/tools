8859a48772378d9a9c99d790294f7c688c0b6763
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-04-27 18:22:01 -0700

    .

--- irfe/run_slurm.sh
+++ irfe/run_slurm.sh
@@ -10,14 +10,4 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-for dir in 5_IrMn 6_IrFe 7_IrCo 8_IrNi
-do
-    for subdir in 1_layer_top 2_layer_hol 3_atom_top1 4_atom_top2 5_atom_hol1 6_atom_hol2
-    do
-        if [[ -d ../$dir/$subdir/vib ]] && [[ ! -f ../$dir/$subdir/vib/OUTCAR ]]; then
-            cp ../$dir/$subdir/vib/* .
-            mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-            cp * ../$dir/$subdir/vib/
-        fi
-    done
-done
+mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
