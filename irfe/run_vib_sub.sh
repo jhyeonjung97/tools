d7304204e442ee39553ad8266a63568cea72f885
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-01 02:54:59 -0700

    .

--- irfe/run_vib_sub.sh
+++ irfe/run_vib_sub.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --partition=g3
-#SBATCH -J IrFe1
+#SBATCH -J IrFe-O
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
 #SBATCH -e STDERR.%N.%j.err
@@ -10,7 +10,17 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-for dir in 1_layer_top  2_layer_brg  3_Ir_top  4_Ir_brg
+for dir in 5_IrMn 6_IrFe
+do
+    for subdir in 2_layer_hol 3_atom_top1 5_atom_hol1
+    do
+        cp $dir/$subdir/* .
+        mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
+        cp * $dir/$subdir/
+    done
+done
+
+for dir in 7_IrCo 8_IrNi
 do
     for subdir in 3_atom_top1 5_atom_hol1
     do
