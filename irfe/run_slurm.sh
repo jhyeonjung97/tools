ec9fbfec550d4de8959da1ab897c0ab0c141b4ca
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-01 03:32:40 -0700

    .

--- irfe/run_slurm.sh
+++ irfe/run_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --partition=g1
-#SBATCH -J IrFe-vib1
+#SBATCH -J IrFe-R2top
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
 #SBATCH -e STDERR.%N.%j.err
@@ -10,11 +10,4 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-# for dir in 1_Ir_top/1_V_V/ 1_Ir_top/6_O_OOH/ 2_Ir_hol/3_V_OH/ 2_Ir_hol/5_O_OH/ 3_IrFe_top1/6_O_OOH/ 4_IrFe_top2/6_O_OOH/ 5_IrFe_top3/6_O_OOH/
-for dir in 2_Ir_hol/3_V_OH/ 3_IrFe_top1/1_V_V/ 3_IrFe_top1/6_O_OOH/ 4_IrFe_top2/1_V_V/ 4_IrFe_top2/6_O_OOH/ 5_IrFe_top3/6_O_OOH/
-do
-    cp $dir/* .
-    mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-    cp * $dir/
-done
-
+mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
