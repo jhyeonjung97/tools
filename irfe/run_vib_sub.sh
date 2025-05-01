07ca9a6b197171cb9965a01fac4b262dba804d17
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-01 02:57:33 -0700

    .

--- irfe/run_vib_sub.sh
+++ irfe/run_vib_sub.sh
@@ -10,17 +10,7 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-for dir in 5_IrMn 6_IrFe
-do
-    for subdir in 2_layer_hol 3_atom_top1 5_atom_hol1
-    do
-        cp $dir/$subdir/* .
-        mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
-        cp * $dir/$subdir/
-    done
-done
-
-for dir in 7_IrCo 8_IrNi
+for dir in 5_IrMn 6_IrFe 7_IrCo 8_IrNi
 do
     for subdir in 3_atom_top1 5_atom_hol1
     do
