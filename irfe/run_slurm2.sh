6cf7ce829b904c137aab69a2b92c18e5865523c4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-01 03:35:07 -0700

    .

--- irfe/run_slurm2.sh
+++ irfe/run_slurm2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
-#SBATCH --partition=g3
+#SBATCH --ntasks-per-node=32
+#SBATCH --partition=g1
 #SBATCH -J IrFe-R1
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
