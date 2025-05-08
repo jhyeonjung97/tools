07916f3c5d1468821676622e56d18035fe9cfb34
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-08 15:17:15 -0700

    .

--- tetra/restart-ads.sh
+++ tetra/restart-ads.sh
@@ -6,17 +6,19 @@ do
     if [[ ! -f 'unmatched' ]]; then
         for subdir in $dir/o*/
         do
-            cd $subdir
-            IFS='/' read -r -a path <<< $subdir
-            coord=$(echo "${path[-4]}" | cut -d'_' -f3)
-            row=$(echo "${path[-3]}" | cut -d'_' -f1)
-            numb=$(echo "${path[-2]}" | cut -d'_' -f1)
-            metal=$(echo "${path[-2]}" | cut -d'_' -f2)
-            ads=$(echo "${path[-1]}" | cut -d'_' -f1)
-            jobname=${coord}${row}${numb}${ads}
-            if [[ ! -f 'DONE' ]]; then
-                python ~/bin/get_restart3.py
-                pwd; sbatch submit.sh
+            if [[ -d $subdir ]]; then
+                cd $subdir
+                IFS='/' read -r -a path <<< $subdir
+                coord=$(echo "${path[-4]}" | cut -d'_' -f3)
+                row=$(echo "${path[-3]}" | cut -d'_' -f1)
+                numb=$(echo "${path[-2]}" | cut -d'_' -f1)
+                metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+                ads=$(echo "${path[-1]}" | cut -d'_' -f1)
+                jobname=${coord}${row}${numb}${ads}
+                if [[ ! -f 'DONE' ]]; then
+                    python ~/bin/get_restart3.py
+                    pwd; sbatch submit.sh
+                fi
             fi
         done
     fi
