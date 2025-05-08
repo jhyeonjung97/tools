a9d0acae825b3722e0b54f9e4cd01431e9c6481a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-08 15:15:35 -0700

    .

---
+++ tetra/restart-ads.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for dir in /pscratch/sd/j/jiuy97/8_V_slab/*_*_*/*/*_*
+do
+    cd $dir    
+    if [[ ! -f 'unmatched' ]]; then
+        for subdir in $dir/o*/
+        do
+            cd $subdir
+            IFS='/' read -r -a path <<< $subdir
+            coord=$(echo "${path[-4]}" | cut -d'_' -f3)
+            row=$(echo "${path[-3]}" | cut -d'_' -f1)
+            numb=$(echo "${path[-2]}" | cut -d'_' -f1)
+            metal=$(echo "${path[-2]}" | cut -d'_' -f2)
+            ads=$(echo "${path[-1]}" | cut -d'_' -f1)
+            jobname=${coord}${row}${numb}${ads}
+            if [[ ! -f 'DONE' ]]; then
+                python ~/bin/get_restart3.py
+                pwd; sbatch submit.sh
+            fi
+        done
+    fi
+done
--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -10,11 +10,14 @@ do
     metal=$(echo "${path[-1]}" | cut -d'_' -f2)
     jobname=${coord}${row}${numb}
     
-    if [[ -f 'unmatched' ]]; then
-        rm *.*
-    else
-        sed -i -e "s/jobname/${jobname}/" submit.sh
-        mv slab.traj start.traj
-        pwd; sbatch submit.sh
+    if [[ ! -f 'unmatched' ]]; then
+        for subdir in $dir/o*/
+        do
+            cd $subdir
+            if [[ ! -f 'DONE' ]]; then
+                python ~/bin/get_restart3.py
+                pwd; sbatch submit.sh
+            fi
+        done
     fi
 done
