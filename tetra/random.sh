ca03b0331e763a7488a6ea81fc7b497f0af8bcd2
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-09 14:32:45 -0700

    .

--- tetra/random.sh
+++ tetra/random.sh
@@ -1,7 +1,7 @@
-for i in {1..100}; do
+for i in {2..100}; do
     for norm_a in c o n; do
         for norm_b in c o n; do
-            echo "\033[1;31mmodel: ${model}, random_state: ${i}, norm_a: ${norm_a}, norm_b: ${norm_b}\033[0m"
+            echo "\033[1;31mrandom_state: ${i}, norm_a: ${norm_a}, norm_b: ${norm_b}\033[0m"
             name_a=$(echo ${norm_a} | tr '[:lower:]' '[:upper:]')
             name_b=$(echo ${norm_b} | tr '[:lower:]' '[:upper:]')
             python ~/bin/tools/tetra/bulk_pred_cfse.py --model gpr --X OS CN group outer_e Hevap base_cfse ICOHP${norm_a} ionN${norm_b} mag volume l_bond chg --random_state ${i} --output random${name_a}${name_b}${i}
