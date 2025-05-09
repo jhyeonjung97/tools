a4860ee2ba29cc44d8b7a9c8d921f7f61e16f7c4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-09 16:04:37 -0700

    .

--- tetra/random.sh
+++ tetra/random.sh
@@ -1,3 +1,3 @@
-for i in {1..100}; do
+for i in {80..100}; do
     python ~/bin/tools/tetra/bulk_pred_cfse.py --model gpr --X OS CN group outer_e Hevap base_cfse ICOHPo ionNo mag volume l_bond chg --random_state ${i} --output random${i}
 done
