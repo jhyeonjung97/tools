8c63f31c9732ddb7d33b255aa10ad59c51b812c0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-20 15:20:30 -0700

    .

--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -4,22 +4,24 @@ for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 4_SquarePlanar_
 do
     for row in 3d 4d 5d fm
     do
-        dir=/pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}
-        cd ${dir}
-        if [[ -f 'DONE' ]] || [[ -f 'unmatched' ]]; then
-            mkdir -p clean
-            mv * clean
-            if [[ -d 'clean/o' ]]; then
-                mv clean/o/ old_o
-                mv clean/oh/ old_oh
-            elif [[ -d 'clean/o1' ]]; then
-                mv clean/o1/ old_o1
-                mv clean/o2/ old_o2
-                mv clean/oh1 old_oh1
-                mv clean/oh2 old_oh2
+        for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/
+        do
+            cd ${dir}
+            if [[ -f 'DONE' ]] || [[ -f 'unmatched' ]]; then
+                mkdir -p clean
+                mv * clean
+                if [[ -d 'clean/o' ]]; then
+                    mv clean/o/ old_o
+                    mv clean/oh/ old_oh
+                elif [[ -d 'clean/o1' ]]; then
+                    mv clean/o1/ old_o1
+                    mv clean/o2/ old_o2
+                    mv clean/oh1/ old_oh1
+                    mv clean/oh2/ old_oh2
+                fi
+            else
+                pwd
             fi
-        else
-            pwd
-        fi
+        done
     done
 done
