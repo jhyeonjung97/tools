d8c53589935c3049490f9e26dba5305f65c81ab4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-25 19:20:29 -0700

    .

--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+# for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 4_SquarePlanar_PD 5_SquarePlanar_NB 6_Octahedral_RS 7_Pyramidal_LT 8_Tetrahedral_WZ 9_Tetrahedral_ZB
+# do
+#     for row in 3d 4d 5d
+#     do
+#         for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/clean
+#         do
+#             cd ${dir}
+#             if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]]; then
+#                 continue
+#             elif [[ -f 'submit.sh' ]]; then
+#                 sbatch submit.sh
+#             fi
+#         done
+#     done
+# done
+
 for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 4_SquarePlanar_PD 5_SquarePlanar_NB 6_Octahedral_RS 7_Pyramidal_LT 8_Tetrahedral_WZ 9_Tetrahedral_ZB
 do
     for row in 3d 4d 5d
     do
-        for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/clean
+        for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/clean/o
         do
             cd ${dir}
-            if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]]; then
+            IFS='/' read -r -a path <<< $dir
+            coordi=$(echo "${path[-3]}" | cut -d'_' -f3)
+            numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+            jobname=${coordi}${row}${numb}
+            if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]] || [[ -n $(squeue --me | grep $jobname) ]]; then
                 continue
             elif [[ -f 'submit.sh' ]]; then
-                sbatch submit.sh
+                python ~/bin/get_restart3
             fi
         done
     done
