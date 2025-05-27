ba0782073ad71f6e7f4c45e9edd03d16e4737d02
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-27 16:58:24 -0700

    .

--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -16,8 +16,8 @@
 #     done
 # done
 
-# for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 4_SquarePlanar_PD 5_SquarePlanar_NB 6_Octahedral_RS 7_Pyramidal_LT 8_Tetrahedral_WZ 9_Tetrahedral_ZB
-for coord in 5_SquarePlanar_NB 6_Octahedral_RS 9_Tetrahedral_ZB
+for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 4_SquarePlanar_PD 5_SquarePlanar_NB 6_Octahedral_RS 7_Pyramidal_LT 8_Tetrahedral_WZ 9_Tetrahedral_ZB
+# for coord in 5_SquarePlanar_NB 6_Octahedral_RS 7_Pyramidal_LT 9_Tetrahedral_ZB
 do
     for row in 3d 4d 5d
     do
@@ -34,6 +34,46 @@ do
                     continue
                 elif [[ -f 'submit.sh' ]]; then
                     sbatch submit.sh
+                fi
+            done
+        done
+    done
+done
+
+for row in 3d 4d 5d
+do
+    for dir in /pscratch/sd/j/jiuy97/8_V_slab/9_Tetrahedral_ZB/${row}/*_*/clean
+    do
+        cd ${dir}
+        IFS='/' read -r -a path <<< $dir
+        coordi=$(echo "${path[-3]}" | cut -d'_' -f3)
+        numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+        jobname=${coordi}${row}${numb}${ads}
+        if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]]; then
+            continue
+        elif [[ -f 'submit.sh' ]]; then
+            pwd
+        fi
+    done
+done
+
+for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 5_SquarePlanar_NB 6_Octahedral_RS 9_Tetrahedral_ZB
+do
+    for row in 3d 4d 5d
+    do
+        for ads in o oh
+        do
+            for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/${ads}
+            do
+                cd ${dir}
+                IFS='/' read -r -a path <<< $dir
+                coordi=$(echo "${path[-3]}" | cut -d'_' -f3)
+                numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+                jobname=${coordi}${row}${numb}${ads}
+                if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]] || [[ -n $(squeue --me | grep $jobname) ]]; then
+                    continue
+                elif [[ -f 'submit.sh' ]]; then
+                    pwd
                 fi
             done
         done
