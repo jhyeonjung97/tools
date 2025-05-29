94939b7dcc6c98cfd7c3d36626958a1283379556
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-05-29 16:00:24 -0700

    .

--- tetra/restart-slab.sh
+++ tetra/restart-slab.sh
@@ -57,11 +57,11 @@ do
     done
 done
 
-for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 3_SquarePlanar_TN 5_SquarePlanar_NB 6_Octahedral_RS 9_Tetrahedral_ZB
+for coord in 1_Tetrahedral_WZ 2_Tetrahedral_ZB 5_SquarePlanar_NB 6_Octahedral_RS
 do
     for row in 3d 4d 5d
     do
-        for ads in o oh
+        for ads in clean o oh
         do
             for dir in /pscratch/sd/j/jiuy97/8_V_slab/${coord}/${row}/*_*/${ads}
             do
@@ -73,9 +73,26 @@ do
                 if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]] || [[ -n $(squeue --me | grep $jobname) ]]; then
                     continue
                 elif [[ -f 'submit.sh' ]]; then
-                    pwd
+                    python ~/bin/get_restart3
                 fi
             done
         done
+    done
+done
+
+
+for row in 3d 4d 5d
+do
+    for dir in /pscratch/sd/j/jiuy97/8_V_slab/9_Tetrahedral_ZB/${row}/*_*/clean
+    do
+        cd ${dir}
+        IFS='/' read -r -a path <<< $dir
+        numb=$(echo "${path[-1]}" | cut -d'_' -f1)
+        jobname=ZZ${row}${numb}
+        if [[ -f 'unmatched' ]] || [[ -f 'DONE' ]] || [[ -n $(squeue --me | grep $jobname) ]]; then
+            continue
+        elif [[ -f 'submit.sh' ]]; then
+            pwd
+        fi
     done
 done
