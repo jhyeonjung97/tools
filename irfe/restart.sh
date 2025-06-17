da359c1539ca1fc06cfe510d92cc9813c2825b0a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-06-16 21:27:52 -0400

    .

--- irfe/restart.sh
+++ irfe/restart.sh
@@ -162,7 +162,7 @@ do
     if [[ -f unmatched ]]; then
         continue
     elif [[ -f OUTCAR ]] && [[ ! -f DONE ]]; then
-        python ~/bin/restart.py
+        python ~/bin/get_restart.py
         if [[ ! -f DONE ]]; then
             continue
         fi
