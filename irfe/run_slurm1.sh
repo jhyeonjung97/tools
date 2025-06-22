e1ab2b0db61b3e51ace33471bbc7d4a6b6b468a0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-06-21 20:45:41 -0700

    .

--- irfe/run_slurm1.sh
+++ irfe/run_slurm1.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --partition=g1
-#SBATCH -J IrFe-oxide
+#SBATCH -J IrFe-metal
 #SBATCH --time=05-00:00
 #SBATCH -o stdout.%N.%j.out
 #SBATCH -e STDERR.%N.%j.err
@@ -10,7 +10,7 @@
 ## HPC ENVIRONMENT DON'T REMOVE THIS PART
 . /etc/profile.d/TMI.sh
 
-for dir in 1_ 2_ 3_ 4_ 5_ 8_
+for dir in 0_Ir 6_IrFe
 do
     cp $dir/vib/* .
     mpiexec.hydra -genv I_MPI_DEBUG 5 -np $SLURM_NTASKS /TGM/Apps/VASP/VASP_BIN/6.3.2/vasp.6.3.2.std.x
