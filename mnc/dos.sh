5cd36b311bdd0ab7ca3a416dcf34f7ee4c1b6855
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-07-01 10:28:09 -0700

    .

--- mnc/dos.sh
+++ mnc/dos.sh
@@ -1,4 +1,4 @@
-for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*/*_*/*_*/*
+for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/*/*_*/*_*/*/
 do
     cd $dir; pwd
     IFS='/' read -r -a path <<< $dir
@@ -32,7 +32,7 @@ do
     # fi
 done
 
-for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/*_*/*
+for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*_*/*_*/*/
 do
     cd $dir; pwd
     IFS='/' read -r -a path <<< $dir
@@ -63,3 +63,20 @@ do
         python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${ads}_${m}${metal}_${spin}${dz}
     fi
 done
+
+
+IFS='/' read -r -a path <<< $PWD
+row=${path[-4]}
+m=$(echo "${path[-3]}" | cut -d'_' -f1)
+metal=$(echo "${path[-3]}" | cut -d'_' -f2)
+spin=$(echo "${path[-2]}" | cut -d'_' -f2)
+dz=$(echo "${path[-1]}" | cut -d'_' -f1)
+if [[ $spin == 'stable' ]]; then
+    spin='MS'
+fi
+if [[ $dz == 'relaxed' ]]; then
+    dz='r'
+fi
+if [[ -s sumo_${metal}_dos.dat ]]; then
+    python ~/bin/tools/mnc/dos.py --file sumo_${metal}_dos.dat --output pdos_${row}_${m}${metal}_${spin}${dz}
+fi
