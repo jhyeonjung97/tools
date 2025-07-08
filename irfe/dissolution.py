9a21b636cadfd97e567c14954402a053538b0dc3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-07-08 09:41:00 -0700

    .

--- irfe/dissolution.py
+++ irfe/dissolution.py
@@ -2,9 +2,7 @@ from ase.io import read
 import os
 import pandas as pd
 
-root = '/Users/hailey/Desktop/4_IrFe3'
-
-df = pd.DataFrame(columns=['Ads', 'System', 'Site', 'Ir', 'Fe', 'O'])
+results = []
 
 def read_vib_correction(vib_file):
     try:
@@ -19,22 +17,71 @@ def read_vib_correction(vib_file):
     except:
         return 0.0
 
-def get_energy(dir, name):
-    atoms_path = os.path.join(root, 'dissolution', dir, 'final.json')
+def get_energy(dir):
+    atoms_path = os.path.join(dir, 'final.json')
     atoms = read(atoms_path)
-    energy = atoms.info['energy']
-    vib_path = os.path.join(root, 'dissolution', dir, 'vib.txt')
+    energy = atoms.get_potential_energy()
+    vib_path = os.path.join(dir, 'vib-z.txt')
     vib_correction = read_vib_correction(vib_path)
+    return energy, vib_correction
+
+dirs = {'1_Ir': 'slab/0_Ir',
+        '4_IrFe_Ir': 'slab/2_Fe',
+        '5_IrFe_Fe': 'slab/2_Fe',
+        '2_IrO_top': '3_O/0_Ir/1_layer_top',
+        '3_IrO_hol': '3_O/0_Ir/2_layer_hol',
+        '6_IrFeO_topIr': '3_O/2_Fe/1_layer_top',
+        '7_IrFeO_topFe': '3_O/2_Fe/1_layer_top',
+        '8_IrFeO_holIr': '3_O/2_Fe/2_layer_hol',
+        '9_IrFeO_holFe': '3_O/2_Fe/2_layer_hol'}
+
+for dir in dirs.keys():
+    system_name = dir.split('_')[1]
+
+    if 'top' in dir:
+        site = 'top'
+    elif 'hol' in dir:
+        site = 'hol'
+    else:
+        site = '-'
+
+    before_path = os.path.join('/Users/hailey/Desktop/4_IrFe3', dirs[dir])
+    after_path = os.path.join('/Users/hailey/Desktop/4_IrFe3/dissolution', dir)
+    energy_before_dissolution = get_energy(before_path)
+    energy_after_dissolution = get_energy(after_path)
+
+    if dir.endswith('Fe'):
+        metal_name = 'Fe'
+        electron = 2
+        metal_energy = -32.32542329/4
+        standard_potential = -0.44
+    else:
+        metal_name = 'Ir'
+        electron = 3
+        metal_energy = -35.36938889/4
+        standard_potential = 1.156
+    ion_energy = metal_energy + electron * standard_potential
+
+    energy_before_dissolution, vib_before_dissolution = energy_before_dissolution
+    energy_after_dissolution, vib_after_dissolution = energy_after_dissolution
+    gibbs_before = energy_before_dissolution + vib_before_dissolution
+    gibbs_after = energy_after_dissolution + vib_after_dissolution + ion_energy
+    potential = (gibbs_after - gibbs_before) / electron
+
+
+
     results.append({
-        'Ads': adsorbate,
         'System': system_name,
-        'Site': info['site'],
-        **layer_charges,
-        **layer_distances,
-        **layer_z_avg
+        'Site': site,
+        'Metal': metal_name,
+        'Ion_energy': ion_energy,
+        'E_before': energy_before_dissolution,
+        'E_after': energy_after_dissolution,
+        'V_before': vib_before_dissolution,
+        'V_after': vib_after_dissolution,
+        'Potential': potential,
     })
 
-dirs = ['1_Ir', '2_IrO_top', '3_IrO_hol', '4_IrFe_Ir', '5_IrFe_Fe', '6_IrFeO_topIr', '7_IrFeO_topFe', '8_IrFeO_holIr', '9_IrFeO_holFe']
-
-
-print(results)
+df = pd.DataFrame(results)
+print(df)
+# df.to_csv('dissolution.csv', index=False)
---
+++ irfe/vib-o.py
@@ -0,0 +1,19 @@
+from ase.io import read, write
+from ase.constraints import FixAtoms
+import numpy as np
+
+# Read the structure
+atoms = read('CONTCAR')
+
+# Get z-coordinates of all atoms
+z_coords = atoms.get_positions()[:, 2]
+
+# Find indices of the 48 atoms with lowest z-coordinates
+bottom_48_indices = np.argsort(z_coords)[:48]
+
+# Apply constraints to fix the bottom 48 atoms
+constraint = FixAtoms(indices=bottom_48_indices)
+atoms.set_constraint(constraint)
+
+# Write the structure with constraints
+write('vib.vasp', atoms)
