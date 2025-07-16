2664e72b2963318db0a81b419c48ca2a3c23ef30
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-07-16 15:33:52 -0700

    .

--- tetra/random.sh
+++ tetra/random.sh
@@ -1,8 +1,10 @@
-for norm in oo no nn on; do
-    for feature in Hsub Hfus Hform Hevap; do
-        for i in {1..99}; do
-            python ~/bin/tools/tetra/bulk_pred_cfse.py --model gpr --random_state ${i} --output ${feature}-${norm}${i} \
-            --X OS CN group outer_e ${feature} base_cfse ICOHPo ionNo mag volume l_bond chg
+for norm1 in o n; do
+    for norm2 in o n; do
+        for feature in Hsub Hfus Hform Hevap; do
+            for i in {1..99}; do
+                python ~/bin/tools/tetra/bulk_pred_cfse.py --model gpr --random_state ${i} --output ${feature}-${norm1}${norm2}${i} \
+                --X OS CN group outer_e ${feature} base_cfse ICOHP${norm1} ion${norm2} mag volume l_bond chg
+            done
         done
     done
 done
