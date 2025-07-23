38a5e180aeae1e633f82b0b20d45b97507a4f627
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-07-22 19:15:24 -0700

    .

---
+++ ti-sac/combine.py
@@ -0,0 +1,14 @@
+from ase.db import connect
+from ase.io import read
+import glob
+
+# Create or open the database
+db = connect('combined.db')
+
+# Loop over all JSON files
+for filename in sorted(glob.glob('*.json')):
+    atoms = read(filename)
+    # Write to database
+    db.write(atoms, filename=filename)
+
+print(f"All JSON files have been written to combined.db")
