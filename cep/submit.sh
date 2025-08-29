c3312544167058aed4ed005f43ecfb103ac75e63
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-08-29 14:31:23 -0700

    .

--- cep/submit.sh
+++ cep/submit.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1
 #SBATCH -C cpu
 #SBATCH -q regular
-#SBATCH -J jobname
-#SBATCH -t 48:00:00
+#SBATCH -J test
+#SBATCH -t 12:00:00
 #SBATCH -A m2997
 #SBATCH -e err.%j.log
 #SBATCH -o out.%j.log
@@ -18,6 +18,6 @@ module load vasp-tpc/6.4.2-cpu
 export VASP_SCRIPT=/global/homes/j/jiuy97/bin/run_vasp_cpu.py
 export VASP_PP_PATH=/global/cfs/cdirs/m2997/vasp-psp/pseudo54
 
-python ~/bin/tools/cep/mnc.py
-python ~/bin/get_restart3.py
+python ~/bin/tools/cep/mno2-100.py
 python ~/bin/verve/bader.py
+python ~/bin/get_restart3
