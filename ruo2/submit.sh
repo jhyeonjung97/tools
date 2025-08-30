d19008ec03c5a2168798a6c199c32792ea6dc6ee
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-08-30 00:34:07 -0700

    .

---
+++ ruo2/submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -J test
+#SBATCH -t 12:00:00
+#SBATCH -A m2997
+#SBATCH -e err.%j.log
+#SBATCH -o out.%j.log
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+module load vasp-tpc/6.4.2-cpu
+
+export VASP_SCRIPT=/global/homes/j/jiuy97/bin/run_vasp_cpu.py
+export VASP_PP_PATH=/global/cfs/cdirs/m2997/vasp-psp/pseudo54
+
+python ~/bin/tools/ruo2/ruo2-bulk.py
+python ~/bin/verve/bader.py
+python ~/bin/get_restart3
