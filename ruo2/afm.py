4781dc2948250629690539ffce7c2058c7218084
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-08-30 20:52:20 -0700

    .

--- ruo2/afm.py
+++ ruo2/afm.py
@@ -1,6 +1,7 @@
 from ase.io import read, write
 
 atoms = read('restart.json')
+
 mag_moments = [0] * len(atoms)
 mag_moments[0] = mag_moments[1] = mag_moments[2] = mag_moments[3] = 2
 mag_moments[4] = mag_moments[5] = mag_moments[6] = mag_moments[7] = -2
