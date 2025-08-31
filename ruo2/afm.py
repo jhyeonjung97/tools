f98b15337d1575bf66053742ca89821231799acf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-08-30 21:49:43 -0700

    .

--- ruo2/afm.py
+++ ruo2/afm.py
@@ -3,8 +3,8 @@ from ase.io import read, write
 atoms = read('restart.json')
 
 mag_moments = [0] * len(atoms)
-mag_moments[0] = mag_moments[1] = mag_moments[2] = mag_moments[3] = 2
-mag_moments[4] = mag_moments[5] = mag_moments[6] = mag_moments[7] = -2
+mag_moments[0] = mag_moments[1] = mag_moments[2] = mag_moments[3] = 3
+mag_moments[4] = mag_moments[5] = mag_moments[6] = mag_moments[7] = -3
 atoms.set_initial_magnetic_moments(mag_moments)
 
 write('restart.json', atoms)
