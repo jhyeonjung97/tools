41e4b52d73dd379e4bc50e73cf3a208c6da6af2f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-09-03 18:25:21 -0700

    .

--- ruo2/dos.sh
+++ ruo2/dos.sh
@@ -1,4 +1,3 @@
-sumo-dosplot --format png --prefix sumo --elements Ru.d
-
 fermi=$(grep "E-fermi" OUTCAR | tail -n 1 | awk '{print $3}')
-pydos -z $fermi --elem='Ru' --spd='d'
+pydos -z $fermi --elem='Ru' --spd='d' --elem='O' --spd='p'
+sumo-dosplot --format png --prefix sumo --elements Ru.d,O.p
