b546852e3d6be3ee4616afe6b278c4144a020d26
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-09-16 13:28:46 -0700

    .

--- ruo2/pourbaix.sh
+++ ruo2/pourbaix.sh
@@ -1,4 +1,7 @@
 # basic
-mv */*.json .
+sh ~/bin/tools/ruo2/json.sh
+python ~/bin/HybridPB/pourbaix.py --hybrid --legend-out \
+--Umax 2 --Gmin -15 --Gmax 15 --cmap-2d Purples --cmin-2d 0.2 --cmax-2d 0.8 --suffix full
+rm Ru_top.json
 python ~/bin/HybridPB/pourbaix.py --hybrid --legend-out \
 --Umax 2 --Gmin -15 --Gmax 15 --cmap-2d Purples --cmin-2d 0.2 --cmax-2d 0.8 --suffix brg
