b882b71eb77c9f175427ff6dcec1836c67e82489
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-09-16 12:50:51 -0700

    .

--- ruo2/vib.py
+++ ruo2/vib.py
@@ -4,10 +4,10 @@ from ase.constraints import FixAtoms
 
 atoms = read('restart.json')
 z_positions = atoms.get_positions()[:, 2]
-num_to_fix = min(46, len(atoms))
+num_to_fix = min(48, len(atoms))
 fix_mask = np.zeros(len(atoms), dtype=bool)
 fix_mask[np.argsort(z_positions)[:num_to_fix]] = True
 
 fix_indices = np.nonzero(fix_mask)[0].tolist()
 atoms.set_constraint(FixAtoms(indices=fix_indices))
-write('vib.json', atoms)
+write('restart.json', atoms)
