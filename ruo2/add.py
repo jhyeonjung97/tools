05163a6f52017c4296e3b38ce08b062131a225ca
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-09-18 15:14:09 -0700

    .

--- ruo2/add.py
+++ ruo2/add.py
@@ -2,6 +2,6 @@ from ase.io import read, write
 from ase import Atoms
 
 atoms = read('restart.json')
-atoms += Atoms('O', positions=[atoms[48].position + (1.0, 0.0, 1.0)])
+atoms += Atoms('O', positions=[atoms[48].position + (+0.8, 0.0, 1.2)])
 atoms += Atoms('H', positions=[atoms[49].position + (-0.8, 0.0, 0.6)])
 write('restart.json', atoms)
