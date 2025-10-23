b5bdec62823b68f52c4d7593a8c093e91d626e7e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-10-23 12:35:41 -0700

    .

--- ruo2/dos.sh
+++ ruo2/dos.sh
@@ -1,5 +1,5 @@
 fermi=$(grep "E-fermi" OUTCAR | tail -n 1 | awk '{print $3}')
-~/bin/tools/ruo2/pydos-ruo2 -z $fermi -y -12 12 --notot \
+~/bin/tools/ruo2/pydos-ruo2 -z $fermi -y -12 12 --notot -p \
 --elem='Ru' --spd='d' --lc 'C2' \
 --elem='O' --spd='p' --lc 'C1'
 
@@ -9,4 +9,9 @@ fermi=$(grep "E-fermi" OUTCAR | tail -n 1 | awk '{print $3}')
 # --elem='Ru' --spd='d' --lc 'C2' \
 # --elem='O' --spd='p' --lc 'C1'
 
-# sumo-dosplot --format png --prefix sumo --elements Ru.d,O.p
+sumo-dosplot --format png --prefix sumo --orbitals Ru.d --elements Ru.d
+
+~/bin/tools/ruo2/pydos-ruo2 -notot -p "12" -p "12" -p "12" -p "12" -p "12" \
+-l "dxy" -l "dyz" -l "dz2" -l "dxz" -l "dx2" \
+--spd "dxy" --spd "dyz" --spd "dz2" --spd "dxz" --spd "dx2" \
+-o atom12_d_orbitals.png
