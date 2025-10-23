b5d35f11423281d633ff6f204555fc09036da4a1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-10-23 12:19:32 -0700

    .

--- ruo2/dos.sh
+++ ruo2/dos.sh
@@ -4,7 +4,7 @@
 # --elem='O' --spd='p' --lc 'C1'
 
 fermi=$(grep "E-fermi" OUTCAR | tail -n 1 | awk '{print $3}')
-~/bin/tools/ruo2/pydos-reruo2 -z $fermi -y -12 12 --notot \
+~/bin/tools/ruo2/pydos-ruo2 -z $fermi -y -12 12 --notot \
 --elem='Re' --spd='d' --lc 'C0' \
 --elem='Ru' --spd='d' --lc 'C2' \
 --elem='O' --spd='p' --lc 'C1'
