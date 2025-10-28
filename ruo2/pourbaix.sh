11f7d0aaa431e225afa2910d856ab70e7bfe0639
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-10-27 22:04:54 -0700

    .

--- ruo2/pourbaix.sh
+++ ruo2/pourbaix.sh
@@ -4,8 +4,8 @@ rm OH_OO.json
 python ~/bin/HybridPB/pourbaix.py --legend-in --show-transitions \
 --Umin 0.0 --Umax 2.0 --Gmin -5 --Gmax 5 --cmin-1d 0.1 --cmax-1d 0.9 --cgap-1d 0.2
 
-python ~/bin/HybridPB/pourbaix.py --hybrid --show-transitions \
---Umin -0.5 --Umax 2.5 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu --cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0
+python ~/bin/HybridPB/pourbaix.py --hybrid --show-transitions --no-bulk \
+--Umin 0.0 --Umax 2.0 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu --cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0
 
 python ~/bin/HybridPB/pourbaix.py --hybrid --legend-out --no-bulk \
 --Umax 2 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu --cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0 --suffix RdYlBu
