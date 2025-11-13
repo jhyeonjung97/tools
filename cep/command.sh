c3a9321b2bd900b6f6ba5f52ef9c3b0f5ad232c3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-11-13 14:15:40 -0800

    .

--- cep/command.sh
+++ cep/command.sh
@@ -11,39 +11,16 @@
 # python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid \
 # --suffix V --Umax 2 --cmin-2d 0.2 --cmax-2d 0.8 --cgap-2d 0.2
 
-mv */*.json .
-python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmin-2d 0.3 --cmax-2d 0.7 --cgap-2d 0.2
+# mv */*.json .
+# python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk --show-ref \
+# --Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmin-2d 0.3 --cmax-2d 0.7 --cgap-2d 0.2
 
 python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap virdis --suffix virdis
+--Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap-2d RdYlBu --suffix RdYlBu38 --cmin-2d 0.3 --cmax-2d 0.8 
 
 python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap plasma --suffix plasma
+--Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap-2d RdYlBu --suffix RdYlBu39 --cmin-2d 0.3 --cmax-2d 0.9 
 
-python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap RdYlBu --suffix RdYlBu --cmin-2d 0.2 --cmax-2d 0.8 
 
-python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap RdYlGn --suffix RdYlGn --cmin-2d 0.2 --cmax-2d 0.8 
-
-python ~/bin/HybridPB/pourbaix.py --legend-out --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap Spectral --suffix Spectral --cmin-2d 0.2 --cmax-2d 0.8 
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmin-2d 0.3 --cmax-2d 0.7 --cgap-2d 0.2
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap virdis --suffix virdis
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap plasma --suffix plasma
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap RdYlBu --suffix RdYlBu --cmin-2d 0.2 --cmax-2d 0.8 
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap RdYlGn --suffix RdYlGn --cmin-2d 0.2 --cmax-2d 0.8 
-
-python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
---Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap Spectral --suffix Spectral --cmin-2d 0.2 --cmax-2d 0.8 
+python ~/bin/HybridPB/pourbaix.py --legend-up --hybrid --no-bulk \
+--Gmin -40 --Gmax 40 --Umin -2 --Umax 3 --cmap-2d RdYlBu --cmin-2d 0.2 --cmax-2d 0.9
