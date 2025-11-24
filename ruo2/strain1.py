4689bfb54e328c3f690a4a9700490fb6572a50f0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-11-24 11:48:10 -0800

    .

--- ruo2/strain1.py
+++ ruo2/strain1.py
@@ -5,4 +5,4 @@ a = atoms.cell[0][0]*1.01
 b = atoms.cell[1][1]*1.01
 c = atoms.cell[2][2]
 atoms.set_cell([a, b, c], scale_atoms=True)
-atoms.write('strain1.json')
+atoms.write('restart.json')
--- ruo2/strain2.py
+++ ruo2/strain2.py
@@ -5,4 +5,4 @@ a = atoms.cell[0][0]*1.02
 b = atoms.cell[1][1]*1.02
 c = atoms.cell[2][2]
 atoms.set_cell([a, b, c], scale_atoms=True)
-atoms.write('strain2.json')
+atoms.write('restart.json')
