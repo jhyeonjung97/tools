103c275ad8556dfb7fc036d5a5fce7d4f45dedec
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-11-24 12:07:29 -0800

    .

--- ruo2/eos-ac.py
+++ ruo2/eos-ac.py
@@ -9,21 +9,22 @@ base_path = os.path.expanduser(base_path)
 dirs = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
 dirs.sort()
 
-figsize = (6.4/6.4*5.4, 4.8/6.4*5.4)
+figsize = (6.4, 4.0)
 plt.figure(figsize=figsize)
-df = pd.DataFrame(columns=['a', 'b', 'energy'])
+df = pd.DataFrame(columns=['a', 'c', 'energy'])
 for dir in dirs:
     parts = dir.split('_')
     a = float(parts[1])
-    b = float(parts[2])
+    c = float(parts[2])
     atoms = read(os.path.join(base_path, dir, 'final_with_calculator.json'))
     energy = atoms.get_potential_energy()
-    df.loc[len(df)] = [a, b, energy]
+    df.loc[len(df)] = [a, c, energy]
 vmin = df['energy'].min()
 df['energy'] = (df['energy'] - vmin)
 df.to_csv('eos-ac.csv', index=False)
-plt.scatter(df['a'], df['b'], c=df['energy'], edgecolors='black', linewidths=0.5, cmap='RdBu', zorder=10)
-plt.tricontourf(df['a'], df['b'], df['energy'], levels=30, cmap='RdBu', zorder=0)
+print(df)
+plt.scatter(df['a'], df['c'], c=df['energy'], edgecolors='black', linewidths=0.5, cmap='RdBu', zorder=10)
+plt.tricontourf(df['a'], df['c'], df['energy'], levels=30, cmap='RdBu', zorder=0)
 plt.colorbar(label='Relative energy (eV)')
 plt.gca().set_aspect('equal', adjustable='box')
 plt.xlabel('Lattice constant a=b (Å)')
--- ruo2/lattice.py
+++ ruo2/lattice.py
@@ -11,23 +11,23 @@ wavecar_path = parent_dir + '/WAVECAR'
 chgcar_path = parent_dir + '/CHGCAR'
 submit_path = parent_dir + '/submit.sh'
 
-for a in [6.46]:
-    # for b in [6.44, 6.45, 6.46, 6.47, 6.48]:
-    for c in [6.23]:
-        # folder_name = f'rutile_{a:.2f}_{b:.2f}_{c:.2f}'
-        folder_name = f'rutile_{a:.2f}_{c:.2f}'
-        folder_path = os.path.join(parent_dir, 'others', folder_name)
-        done_path = os.path.join(parent_dir, folder_name, 'DONE')
-        if os.path.exists(done_path):
-            continue
-        os.makedirs(folder_path, exist_ok=True)
-        shutil.copy(restart_path, folder_path)
-        shutil.copy(wavecar_path, folder_path)
-        shutil.copy(chgcar_path, folder_path)
-        shutil.copy(submit_path, folder_path)
-        os.chdir(folder_path)
-        atoms = read('restart.json')
-        atoms.set_cell([a, a, c], scale_atoms=True)
-        atoms.write('restart.json')
-        subprocess.call(f'sh ~/bin/verve/jobname.sh {folder_name}', shell=True)
-        os.chdir('../')
+for a in [6.42, 6.43, 6.44]:
+    for b in [6.49]:
+        for c in [6.24, 6.25]:
+            folder_name = f'rutile_{a:.2f}_{b:.2f}_{c:.2f}'
+            # folder_name = f'rutile_{a:.2f}_{c:.2f}'
+            folder_path = os.path.join(parent_dir, 'others', folder_name)
+            done_path = os.path.join(parent_dir, folder_name, 'DONE')
+            if os.path.exists(done_path):
+                continue
+            os.makedirs(folder_path, exist_ok=True)
+            shutil.copy(restart_path, folder_path)
+            shutil.copy(wavecar_path, folder_path)
+            shutil.copy(chgcar_path, folder_path)
+            shutil.copy(submit_path, folder_path)
+            os.chdir(folder_path)
+            atoms = read('restart.json')
+            atoms.set_cell([a, a, c], scale_atoms=True)
+            atoms.write('restart.json')
+            subprocess.call(f'sh ~/bin/verve/jobname.sh {folder_name}', shell=True)
+            os.chdir('../')
