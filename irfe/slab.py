c9f53110cea88e2ea566bf677a5efa318df054d1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-12-07 22:20:29 -0800

    .

---
+++ irfe/slab.py
@@ -0,0 +1,21 @@
+from ase.io import read, write
+from ase import Atoms
+import os
+
+for file in os.listdir('./'):
+    if file.endswith('.json'):
+        filename = file.split('.')[0]
+        atoms = read(file)
+
+        min_z = atoms.positions[:,2].min()
+        max_z = atoms.positions[:,2].max()
+        height = max_z - min_z + 15.0
+
+        l1 = atoms.cell.lengths()[0]
+        l2 = atoms.cell.lengths()[1]
+        a1 = atoms.cell.angles()[0]
+        a2 = atoms.cell.angles()[1]
+        a3 = atoms.cell.angles()[2]
+        atoms.cell = (l1, l2, height, a1, a2, a3)
+
+        atoms.write(f'{filename}.vasp')
