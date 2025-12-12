025be3982b69e74c17122bd4bdcf424d4e73db16
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-12-12 14:28:07 -0800

    .

--- ruo2/pourbaix.sh
+++ ruo2/pourbaix.sh
@@ -10,10 +10,13 @@ python ~/bin/HybridPB/pourbaix.py --hybrid --show-transitions --no-bulk \
 python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
 --Umin -0.5 --Umax 2.5 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu --cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0 --suffix RdYlBu
 
+python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk --legend-out \
+--Umin -0.5 --Umax 2.5 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu \
+--cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0 --colors-bulk white gainsboro whitesmoke white
+
 python ~/bin/HybridPB/pourbaix.py --hybrid --no-bulk \
 --Umin -0.5 --Umax 2.5 --Gmin -15 --Gmax 15 --cmap-2d RdYlBu \
---cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0 --cmin 0.3 --cmax 0.0
-
+--cmin-2d 0.3 --cmax-2d 0.8 --cgap-2d 0.0 --colors-bulk white whitesmoke white
 
 python ~/bin/HybridPB/pourbaix.py --hybrid --Umin -2 --Umax 2 --cmin 0.1 --cmax 0.6
 
