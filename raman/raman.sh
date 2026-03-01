bad9fa6d8bb1fc67f1d169da5c91cf6009092e27
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-03-01 13:53:56 -0800

    .

--- raman/raman.sh
+++ raman/raman.sh
@@ -5,7 +5,8 @@ cp ../POTCAR POTCAR
 cp ../KPOINTS KPOINTS
 cp ../run_slurm.sh run_slurm.sh
 
-sed -i -e 's/IBRION/#IBRION/' \
+sed -i \
+-e 's/IBRION/#IBRION/' \
 -e 's/POTIM/#POTIM/' \
 -e 's/NSW/#NSW/' \
 -e '/LREAL/ a LEPSILON = .TRUE.' \
