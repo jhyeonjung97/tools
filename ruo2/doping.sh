14d98f3b5b1325b6c18d7b4c5e02844866df80de
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-03-17 13:17:57 -0700

    .

---
+++ ruo2/doping.sh
@@ -0,0 +1,5 @@
+sed -i -e "s/X/72/" 1_Hf/restart.json  
+sed -i -e "s/X/73/" 2_Ta/restart.json  
+sed -i -e "s/X/74/" 3_W/restart.json  
+sed -i -e "s/X/75/" 4_Re/restart.json  
+sed -i -e "s/X/76/" 5_Os/restart.json
