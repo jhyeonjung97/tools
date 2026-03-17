a7d700c340943171af938b8699d60b00baa13de8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-03-17 12:39:49 -0700

    .

--- ruo2/lobsterin.sh
+++ ruo2/lobsterin.sh
@@ -1,7 +1,4 @@
 for dir in */; do
-    cd $dir
-    dir=${dir%/}
     IFS='_' read -r A B <<< "$dir"
-    sed -i -e "s/X/$B/g" lobsterin
-    cd $dir_now
+    sed -i -e "s/X/$B/g" $dir/lobsterin
 done
