bb644b8647981a6cc2790d54b57aed23fd14b25f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-03-27 16:05:50 -0700

    .

--- ruo2/m2ru.py
+++ ruo2/m2ru.py
@@ -1,10 +1,10 @@
 from ase.io import read, write
 atoms = read('restart.json')
 
-atoms[19].symbol = 'Ru'
+atoms[45].symbol = 'Ru'
 
 magmoms = atoms.get_initial_magnetic_moments()
-magmoms[19] = magmoms[16]
+magmoms[45] = magmoms[44]
 
 atoms.set_initial_magnetic_moments(magmoms)
 
