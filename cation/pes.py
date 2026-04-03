d3fa04575d4496a338982924402ba6bb5ddc8fd5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-02 20:29:27 -0700

    .

--- cation/pes.py
+++ cation/pes.py
@@ -5,6 +5,6 @@ atoms = read('restart.json')
 z_coords = atoms.get_positions()[:, 2]
 min_z_index = z_coords.argmin()
 
-for i in range(10):
+for i in range(30):
     atoms[min_z_index].z -= 0.1
     write(f'restart_{i+1:02d}.json', atoms)
