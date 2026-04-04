16cd92a3f61c5bfdbbd6a8760051a43e66b4b13e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-03 17:54:11 -0700

    .

--- cation/pes_slab0.py
+++ cation/pes_slab0.py
@@ -1,21 +1,20 @@
 import os
 import time
+import shutil
 import subprocess
-from ase.io import read
+from ase.io import read, write
 from ase.io.trajectory import Trajectory
-import ase.calculators.vasp as vasp_calculator
+from ase.calculators.vasp import Vasp
 
-name = 'opt_slab'
+root = os.getcwd()
+basename = os.path.basename(root)
+
+name = 'pes_slab'
 start_time = time.time()
 
-if os.path.exists('restart.json'):
-    atoms = read('restart.json')
-else:
-    atoms = read('start.traj')
-
-atoms.calc = vasp_calculator.Vasp(
+calc = Vasp(
     istart=1,
-    icharg=1,
+    icharg=2,
     inimix=0,
     amix=0.1,
     bmix=0.0001,
@@ -39,7 +38,7 @@ atoms.calc = vasp_calculator.Vasp(
     ediffg=-0.02,
     ediff=1e-5,
     nsw=0,
-    nelm=250,
+    nelm=800,
     setups='recommended',
     # lasph=True,
     # laechg=True,
@@ -48,12 +47,34 @@ atoms.calc = vasp_calculator.Vasp(
     # lwave=False,
     )
 
-eng = atoms.get_potential_energy()
-print ('Calculation Complete, storing the run + calculator to traj file')
+for j in [1, 2, 3, 29]:
+    i = j - 1
 
-Trajectory(f'final_{name}.traj', 'w').write(atoms)
-subprocess.call(f'ase convert -f final_{name}.traj final_with_calculator.json', shell=True)
-subprocess.call(f'/sdf/home/j/jiuy97/miniconda3/envs/myenv/bin/python ~/bin/get_restart3', shell=True)
+    old_folder_name = f'{i:02d}_'
+    old_folder_path = os.path.join(root, old_folder_name)
+
+    new_folder_name = f'{j:02d}_'
+    new_folder_path = os.path.join(root, new_folder_name)
+    os.makedirs(new_folder_path, exist_ok=True)
+
+    shutil.copy2(os.path.join(old_folder_path, 'restart.json'), new_folder_path)  
+    shutil.copy2(os.path.join(old_folder_path, 'WAVECAR'), new_folder_path)
+    shutil.copy2(os.path.join(old_folder_path, 'CHGCAR'), new_folder_path)
+
+    os.chdir(new_folder_path)
+    atoms = read('restart.json')
+    z_coords = atoms.get_positions()[:, 2]
+    min_z_index = z_coords.argmin()
+    atoms[min_z_index].z -= 0.1
+    write('restart.json', atoms)
+
+    atoms.calc = calc
+    atoms.get_potential_energy()
+    atoms.get_forces()
+
+    Trajectory(f'final_{name}.traj', 'w').write(atoms)
+    subprocess.call(f'ase convert -f final_{name}.traj final_with_calculator.json', shell=True)
+    subprocess.call(f'/sdf/home/j/jiuy97/miniforge3/bin/python ~/bin/get_restart3', shell=True)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
