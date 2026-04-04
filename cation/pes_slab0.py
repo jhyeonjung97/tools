e2c0931b4e913d255cfb7cce475e2b6508b5b3d3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-03 17:46:58 -0700

    .

---
+++ cation/pes_slab0.py
@@ -0,0 +1,67 @@
+import os
+import time
+import subprocess
+from ase.io import read
+from ase.io.trajectory import Trajectory
+import ase.calculators.vasp as vasp_calculator
+
+name = 'opt_slab'
+start_time = time.time()
+
+if os.path.exists('restart.json'):
+    atoms = read('restart.json')
+else:
+    atoms = read('start.traj')
+
+atoms.calc = vasp_calculator.Vasp(
+    istart=1,
+    icharg=1,
+    inimix=0,
+    amix=0.1,
+    bmix=0.0001,
+    amix_mag=0.1,
+    bmix_mag=0.0001,
+    encut=500,
+    xc='PBE',
+    gga='RP',
+    ivdw=12,
+    kpts=(3, 3, 1),
+    kpar=5,
+    ncore=6,
+    gamma=True,
+    ismear=0,
+    sigma=0.05,
+    algo='normal',
+    lreal='auto',
+    ibrion=-1,
+    isif=2,
+    ispin=2,
+    ediffg=-0.02,
+    ediff=1e-5,
+    nsw=0,
+    nelm=250,
+    setups='recommended',
+    # lasph=True,
+    # laechg=True,
+    # isym=0,
+    lorbit=11,
+    # lwave=False,
+    )
+
+eng = atoms.get_potential_energy()
+print ('Calculation Complete, storing the run + calculator to traj file')
+
+Trajectory(f'final_{name}.traj', 'w').write(atoms)
+subprocess.call(f'ase convert -f final_{name}.traj final_with_calculator.json', shell=True)
+subprocess.call(f'/sdf/home/j/jiuy97/miniconda3/envs/myenv/bin/python ~/bin/get_restart3', shell=True)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+with open('time.log', 'a') as f:
+    f.write(f"Start Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}\n")
+    f.write(f"End Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}\n")
+    f.write(f"Elapsed Time: {elapsed_time:.2f} seconds\n")
+    f.write("="*40 + "\n")
+
+print(f"Execution time logged in 'time.log'.")
