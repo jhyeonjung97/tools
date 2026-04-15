94a75d300deb5ad304026e99bfafa91473564ba6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-15 15:55:48 -0700

    .

--- cation/cell4xyz.py
+++ cation/cell4xyz.py
@@ -2,6 +2,6 @@ from ase.io import read, write
 
 atoms = read('xtbopt.xyz')
 
-atoms.cell = (30, 30, 30, 90, 90, 90)
+atoms.cell = (30, 30, 40, 90, 90, 90)
 atoms.center()
 write('xtbopt.json', atoms)
