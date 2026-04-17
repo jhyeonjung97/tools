b6fb41eafece3f290d93899c1cefe1daa54d43bd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-17 11:55:11 -0700

    .

---
+++ cation/del.py
@@ -0,0 +1,9 @@
+from ase.io import read, write
+
+atoms = read('restart.json')
+z_coords = atoms.get_positions()[:, 2]
+indices_to_delete = z_coords.argsort()[:6]
+
+for index in indices_to_delete:
+    del atoms[index]
+write('restart_del.json', atoms)
