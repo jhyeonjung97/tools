3af34c71f54e78a3f3d6fce58b95ab9425f2bb8d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-04-24 22:16:08 -0700

    .

--- ruo2/m2ru.py
+++ ruo2/m2ru.py
@@ -1,10 +1,31 @@
+import argparse
+
 from ase.io import read, write
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--element", required=True, help="Substitution element symbol (e.g., Ru, Fe, Co)")
+args = parser.parse_args()
+
+target_element = args.element
 atoms = read('restart.json')
 
-atoms[45].symbol = 'Ru'
+candidate_indices = [i for i, atom in enumerate(atoms) if atom.symbol not in {target_element, 'O'}]
+if not candidate_indices:
+    raise ValueError(f"No atoms found with symbols other than {target_element} and O.")
+
+target_idx = max(candidate_indices, key=lambda i: atoms.positions[i][2])
+
+atoms[target_idx].symbol = target_element
 
 magmoms = atoms.get_initial_magnetic_moments()
-magmoms[45] = magmoms[44]
+if target_idx == 45:
+    source_idx = 44
+elif target_idx == 19:
+    source_idx = 16
+else:
+    raise ValueError(f"Unsupported target_idx for magmom mapping: {target_idx}")
+
+magmoms[target_idx] = magmoms[source_idx]
 
 atoms.set_initial_magnetic_moments(magmoms)
 
